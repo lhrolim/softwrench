a26b404c32bdeb8ed3388d3ddcfe8b4acb8897a5
lhrolim <luizhenrique.rolim@gmail.com>
2014-10-06 14:42:17 -0300

    switch machines

---
+++ softWrench.sW4.Web/DB_Migration/4.0/20141001_SWWEB-505.cs
@@ -0,0 +1,22 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using FluentMigrator;
+using softWrench.sW4.Util;
+using softWrench.sW4.Web.Util;
+
+namespace softWrench.sW4.Web.DB_Migration._4._0 {
+    [Migration(201410011621)]
+    public class Migration20141001Swweb505 : FluentMigrator.Migration {
+        public override void Up() {
+            Create.Table("PREF_USERFILTER")
+                .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("alias").AsString(MigrationUtil.StringSmall).NotNullable()
+                .WithColumn("application").AsString(MigrationUtil.StringSmall).NotNullable()
+                .WithColumn("schema").AsString(MigrationUtil.StringSmall).Nullable()
+                .WithColumn("whereclause").AsString(MigrationUtil.StringSmall).NotNullable();
+        }
+
+        public override void Down() {
+
+        }
+    }
+}
---
+++ softWrench.sW4/Preferences/UserFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Mapping.Attributes;
+using softWrench.sW4.Security.Interfaces;
+
+namespace softWrench.sW4.Preferences {
+
+    [Class(Table = "PREF_USERFILTER", Lazy = false)]
+    public class UserFilter : IBaseEntity {
+        public int? Id { get; set; }
+
+        public string Alias { get; set; }
+
+        [Property]
+        public string Application{ get; set; }
+
+        [Property]
+        public string Schema { get; set; }
+
+        [Property]
+        public string QueryString { get; set; }
+
+    }
+}
