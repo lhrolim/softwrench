2b23becaf2e35a2daf394b5cacac1af6465d007c
lhrolim <luizhenrique.rolim@gmail.com>
2014-10-09 15:34:32 -0300

    adding utility method on parser

---
+++ softwrench.sW4.batches/com/cts/softwrench/sw4/batches/entities/Batch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NHibernate.Mapping.Attributes;
+using softWrench.sW4.Security.Interfaces;
+
+namespace softwrench.sW4.batches.com.cts.softwrench.sw4.batches.entities {
+
+    [Class(Table = "BATCH", Lazy = false)]
+    public class Batch : IBaseEntity {
+        public int? Id { get; set; }
+
+        [Property]
+        public String Application { get; set; }
+
+        [Property]
+        public String Schema { get; set; }
+
+        [Property]
+        public int? UserId { get; set; }
+
+        [Property]
+        public string Status { get; set; }
+        
+        /// <summary>
+        /// this will hold only the editable fields, as the others should be fetched from the database
+        /// </summary>
+        [Property]
+        public string DataMapJson { get; set; }
+
+        
+
+    }
+}
