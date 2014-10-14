a830c5d64b097948b3ef5b257e03103ced923bf0
Scott <scott@designsbysm.com>
2014-10-14 10:40:00 -0700

    Stop combobox autofocus on detail pages

--- softWrench.sW4.Web/Content/Scripts/bootstrap-combobox.js
+++ softWrench.sW4.Web/Content/Scripts/bootstrap-combobox.js
@@ -298,10 +298,12 @@
     , refresh: function () {
         this.source = this.parse();
         this.options.items = this.source.length;
-        var val = this.$element.val();
-        if (!this.source || $.inArray(val, this.source) == -1) {
-            this.clearElement();
-        }
+
+        //SM - 09/24 - Stop autofocus on detail page load (update to v1.1.6)
+        //var val = this.$element.val();
+        //if (!this.source || $.inArray(val, this.source) == -1) {
+        //    this.clearElement();
+        //}
     }
 
     , listen: function () {
