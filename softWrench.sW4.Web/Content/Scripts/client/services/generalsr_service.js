2e9c15f52fd91520e101236a9295f4c198fb1878
rrpranav <rraja@controltechnologysolutions.com>
2014-11-03 12:34:04 -0700

    Addressing Comments-2

--- softWrench.sW4.Web/Content/Scripts/client/services/generalsr_service.js
+++ softWrench.sW4.Web/Content/Scripts/client/services/generalsr_service.js
@@ -60,6 +60,7 @@ app.factory('generalsrService', function (alertService, $rootScope,associationSe
             if (event.fields['status'] == 'NEW') {
                 event.fields['status'] = 'QUEUED';
                 //Removing the alert for Kongsberg because Kongsberg uses generalsr_service and they can select both owner and ownergroup
+                //TODO: make a custom service for KOGT
                 if (!contextService.isClient('kongsberg')) {
                     alertService.alert("Owner Group Field will be disabled if the owner is selected");
                 }
