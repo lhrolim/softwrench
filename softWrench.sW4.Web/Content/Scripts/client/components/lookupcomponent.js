43ed6d3331a6df82b73f757ff2a6bd7c28286bbd
rrpranav <rraja@controltechnologysolutions.com>
2014-11-06 14:17:04 -0700

    revert this

---
+++ softWrench.sW4.Web/Content/Scripts/client/components/lookupcomponent.js
@@ -0,0 +1,50 @@
+﻿var app = angular.module('sw_layout');
+
+app.factory('cmplookup', function ($rootScope, $timeout) {
+
+    return {
+
+        unblock: function (displayable) {
+
+        },
+
+        block: function (displayable) {
+
+        },
+
+        refreshFromAttribute: function (fieldMetadata, scope) {
+            if (scope.associationOptions == null) {
+                //this scenario happens when a composition has lookup-associations on its details, 
+                //but the option list has not been fetched yet
+                scope.lookupAssociationsDescription[fieldMetadata.attribute] = null;
+                scope.lookupAssociationsCode[fieldMetadata.attribute] = null;
+                return;
+            }
+
+            var options = scope.associationOptions[fieldMetadata.associationKey];
+            var optionValue = scope.datamap[fieldMetadata.target];
+            scope.lookupAssociationsCode[fieldMetadata.attribute] = optionValue;
+            if (options == null || options.length <= 0) {
+                //it should always be lazy loaded... why is this code even needed?
+                return;
+            }
+
+            var optionSearch = $.grep(options, function (e) {
+                return e.value == optionValue;
+            });
+
+            var valueToSet = optionSearch != null && optionSearch.length > 0 ? optionSearch[0].label : null;
+            scope.lookupAssociationsDescription[fieldMetadata.attribute] = valueToSet;
+        },
+
+        init: function (bodyElement, scope) {
+
+        }
+
+
+
+    }
+
+});
+
+
