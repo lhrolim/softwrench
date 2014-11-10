b7c9e99450e14becb5e4aff3cfd01838d20c45a6
rrpranav <rraja@controltechnologysolutions.com>
2014-11-10 16:48:50 -0700

    PO Receiving-Partial

---
+++ softWrench.sW4.Web/Content/Scripts/client/services/poreceiving_service.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('sw_layout');
+
+app.factory('poreceivingService', function($http, redirectService) {
+
+    return {
+        poreceivelistclick: function(datamap, schema) {
+            var param = {};
+            param.id = datamap['matrectrans_.matrectransid'];
+            var application = 'matrectrans';
+            var detail = 'matrectransdetail';
+            var mode = 'input';
+            redirectService.goToApplicationView(application, detail, mode, null, param, null);
+
+        }
+    };
+});
---
+++ softWrench.sW4/Data/Persistence/WS/Commons/BasePurchaseOrderCrudConnector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using softWrench.sW4.Data.Persistence.Operation;
+using softWrench.sW4.Data.Persistence.WS.API;
+using softWrench.sW4.Data.Persistence.WS.Internal;
+using softWrench.sW4.Security.Services;
+using softWrench.sW4.Util;
+using w = softWrench.sW4.Data.Persistence.WS.Internal.WsUtil;
+
+namespace softWrench.sW4.Data.Persistence.WS.Commons {
+    class BasePurchaseOrderCrudConnector : CrudConnectorDecorator {
+        public override void BeforeUpdate(MaximoOperationExecutionContext maximoTemplateData) {
+            var user = SecurityFacade.CurrentUser();
+            var po = maximoTemplateData.IntegrationObject;
+
+            w.SetValueIfNull(po, "ACTLABHRS", 0.0);
+            w.SetValueIfNull(po, "ACTLABCOST", 0.0);
+            w.SetValueIfNull(po, "CHANGEDATE", DateTime.Now.FromServerToRightKind(), true);
+            w.SetValueIfNull(po, "CHANGEBY", user.Login);
+            w.SetValueIfNull(po, "REPORTDATE", DateTime.Now.FromServerToRightKind());
+
+
+            var crudData = ((CrudOperationData)maximoTemplateData.OperationData);
+            LongDescriptionHandler.HandleLongDescription(po, crudData);
+            HandlePolines(maximoTemplateData, crudData, po);
+            base.BeforeUpdate(maximoTemplateData);
+        }
+
+        private void HandlePolines(MaximoOperationExecutionContext maximoTemplateData, CrudOperationData crudData, object po)
+        {
+            if (crudData != null)
+            {
+                var polines = (IEnumerable<CrudOperationData>)crudData.GetRelationship("poline");
+                var recordKey = crudData.Id;
+            }
+            var user = SecurityFacade.CurrentUser();
+
+        }
+    }
+}
