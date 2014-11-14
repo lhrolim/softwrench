29116524f43c227865369f3273c956fed284490b
jmvogt <jvogt@controltechnologysolutions.com>
2014-11-14 06:04:21 -0700

    Inventory Transfer - Added PostFilter to Sort Bins
    - Added a postfilterfunction to sort bins so that a 'null' bin with
    inventory will show up first in the bin selection list.

--- softWrench.sW4/Metadata/Applications/DataSet/BaseInvuseDataSet.cs
+++ softWrench.sW4/Metadata/Applications/DataSet/BaseInvuseDataSet.cs
@@ -1,22 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using DocumentFormat.OpenXml.Drawing.Diagrams;
 using DocumentFormat.OpenXml.Spreadsheet;
 using softWrench.sW4.Data.Persistence.Dataset.Commons;
 using softWrench.sW4.Data.Search;
+using softwrench.sW4.Shared2.Data;
 using softwrench.sw4.Shared2.Data.Association;
 
 
 namespace softWrench.sW4.Metadata.Applications.DataSet {
 
     class BaseInvuseDataSet : MaximoApplicationDataSet {
-        public SearchRequestDto FilterLocation(AssociationPreFilterFunctionParameters parameters) {
-            return LocationFilterByType(parameters);
-        }
-
-        public SearchRequestDto LocationFilterByType(AssociationPreFilterFunctionParameters parameters) {
+        
+        public SearchRequestDto FilterToStoreLoc(AssociationPreFilterFunctionParameters parameters) {
             var filter = parameters.BASEDto;
             const string type = "STOREROOM";
             filter.AppendSearchEntry("location.type", type.ToUpper());
+
+            var siteid = parameters.OriginalEntity.GetAttribute("siteid");
+
+            if (siteid != null)
+            {
+                filter.AppendSearchEntry("location.siteid", siteid.ToString().ToUpper());
+            }
+
             return filter;
         }
 
@@ -48,15 +56,50 @@ namespace softWrench.sW4.Metadata.Applications.DataSet {
             var filter = parameters.BASEDto;
             var siteid = parameters.OriginalEntity.GetAttribute("siteid");
             var location = parameters.OriginalEntity.GetAttribute("fromstoreloc");
-            if (siteid != null && location != null) {
-                filter.AppendSearchEntry("inventory_.siteid", siteid.ToString().ToUpper());
-                filter.AppendSearchEntry("invbalances.location", location.ToString().ToUpper());
-                filter.ProjectionFields.Clear();
-                filter.ProjectionFields.Add(new ProjectionField("binnum", "ISNULL(invbalances.binnum, '[No Bin]')"));
-                filter.SearchSort = "CASE WHEN invbalances.binnum = '[No Bin]' THEN '1' ELSE invbalances.binnum END";
-                filter.SearchAscending = true;
+            return FilterBin(filter, siteid, location);
+        }
+
+        public SearchRequestDto FilterToBin(AssociationPreFilterFunctionParameters parameters) {
+            var filter = parameters.BASEDto;
+            var siteid = parameters.OriginalEntity.GetAttribute("siteid");
+            var location = parameters.OriginalEntity.GetAttribute("invuseline_.tostoreloc");
+            return FilterBin(filter, siteid, location);
+        }
+
+        public SearchRequestDto FilterBin(SearchRequestDto filter, object siteid, object location) {
+            if (siteid == null || location == null) {
+                return filter;
             }
+            filter.AppendSearchEntry("inventory_.siteid", siteid.ToString().ToUpper());
+            filter.AppendSearchEntry("invbalances.location", location.ToString().ToUpper());
+            filter.ProjectionFields.Clear();
+            filter.ProjectionFields.Add(new ProjectionField("binnum", "ISNULL(invbalances.binnum, '[No Bin]')"));
+            filter.SearchSort = "CASE WHEN invbalances.binnum = '[No Bin]' THEN '1' ELSE invbalances.binnum END";
+            filter.SearchAscending = true;
             return filter;
+        }
+
+        public IEnumerable<IAssociationOption> ParseBins(AssociationPostFilterFunctionParameters postParams)
+        {
+            ISet<IAssociationOption> resultBins = new HashSet<IAssociationOption>();
+
+            var nullBinWithInventory = (from bin in postParams.Options
+                                        where bin.Value == "[No Bin]"
+                                        select bin).SingleOrDefault();
+
+            if (nullBinWithInventory != null) {
+                resultBins.Add(nullBinWithInventory);
+            } 
+            
+            var binsWithInventory = (from bin in postParams.Options
+                                          where bin.Value != "[No Bin]"
+                                          select bin).ToList();
+
+            foreach (var bin in binsWithInventory) {
+                resultBins.Add(bin);
+            }
+
+            return resultBins;
         }
 
         public override string ApplicationName() {
