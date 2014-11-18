66cba1d969b3cb36f8c47222b321c2bd309c473b
rrpranav <rraja@controltechnologysolutions.com>
2014-11-18 15:07:26 -0700

    Commit_11/18/14

---
+++ softWrench.sW4.Web/Content/Scripts/customers/shared/receivingactions.js
@@ -0,0 +1,9 @@
+﻿function ReceivingController($scope,contextService,alertService) {
+    $scope.receive(compositionitem){
+        if (compositionItemData['orderqty'] <= compositionItemData['receivedqty']) {
+            alertService.alert("Cannot receive more than you ordered");
+            return;
+        }
+        return;
+    }
+};
