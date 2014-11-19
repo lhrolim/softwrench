f597c4b9550990b14d6159ee62c311ccd3182c63
rrpranav <rraja@controltechnologysolutions.com>
2014-11-19 16:48:48 -0700

    Commit_11/19/14

--- softWrench.sW4.Web/Content/Scripts/customers/shared/receiving.js
+++ softWrench.sW4.Web/Content/Scripts/customers/shared/receiving.js
@@ -5,8 +5,20 @@
             return;
         }
         if (compositionitem['receivedqty'] == null) {
-            //need to perform the query to get the total quantity due
-
+            //need to perform the query to get the total quantity due -- Extract everything into a function of poreceiving service later
+            var searchData = {
+                ponum: compositionitem['ponum'],
+                polinenum: String(compositionitem['polinenum'])
+            };
+            searchService.searchWithData("materialrecords", searchData).success(function(data) {
+                var resultObject = data.resultObject;
+                var totalquantityreceived = 0;
+                var i = 0;
+                for (i = 0;i< resultObject.length;i++) {
+                    totalquantityreceived = totalquantityreceived + resultObject[i]['fields']['quantity'];
+                }
+                var qtydue = compositionitem['orderqty'] - totalquantityreceived;
+            });
             // prepopulate the values for the matrectrans record
 
             //open a schema to submit a matrectrans record
