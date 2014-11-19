467682a951bc0557ca79d5cf9fe7ba2cb767f211
rrpranav <rraja@controltechnologysolutions.com>
2014-11-19 15:33:45 -0700

    fix
    Changes to Controller

---
+++ softWrench.sW4.Web/Content/Scripts/customers/shared/receiving.js
@@ -0,0 +1,16 @@
+﻿function ReceivingController($scope,contextService,alertService,searchService) {
+    $scope.receive = function(compositionitem){
+        if (compositionitem['orderqty'] <= compositionitem['receivedqty']) {
+            alertService.alert("Cannot receive more than you ordered !! Receipts Completed");
+            return;
+        }
+        if (compositionitem['receivedqty'] == null) {
+            //need to perform the query to get the total quantity due
+
+            // prepopulate the values for the matrectrans record
+
+            //open a schema to submit a matrectrans record
+        }
+        return;
+    };
+}
--- softWrench.sW4.Web/Content/Scripts/customers/shared/receivingactions.js
+++
@@ -1,9 +0,0 @@
-﻿function ReceivingController($scope,contextService,alertService) {
-    $scope.receive(compositionitem){
-        if (compositionItemData['orderqty'] <= compositionItemData['receivedqty']) {
-            alertService.alert("Cannot receive more than you ordered");
-            return;
-        }
-        return;
-    }
-};
