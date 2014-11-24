072feb173d6d78c303ae118f29cd6849417be1fd
lhrolim <luizhenrique.rolim@gmail.com>
2014-11-24 16:08:47 -0200

    HAP-717

--- softwrench.sw4.Shared2/Metadata/Applications/UI/FieldRendererConstants.cs
+++ softwrench.sw4.Shared2/Metadata/Applications/UI/FieldRendererConstants.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using softwrench.sW4.Shared2.Util;
 
-namespace softwrench.sw4.Shared2.Metadata.Applications.UI
-{
+namespace softwrench.sw4.Shared2.Metadata.Applications.UI {
     /// <summary>
     /// Class to handle the paramerters used in the Renderer in metadata.xml
     /// </summary>
-    public class FieldRendererConstants
-    {
+    public class FieldRendererConstants {
         //Export to excel in case the field is hidden.
-        public const string EXPORTTOEXCEL = "exportToExcel";
+        public const string Exporttoexcel = "exporttoexcel";
+
     }
 }
