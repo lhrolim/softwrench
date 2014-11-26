019dbd4fe384dc62ecd9ff17e36af884434ae255
Scott A. Matthews <scott@designsbysm.com>
2014-11-26 09:47:53 -0700

    SWWEB-723 update button & disabled styles

--- softWrench.sW4.Web/Content/Scripts/customers/shared/invissuereturnaction.js
+++ softWrench.sW4.Web/Content/Scripts/customers/shared/invissuereturnaction.js
@@ -1,17 +1,9 @@
-﻿function InvIssueReturnActionController($scope, contextService, alertService, modalService, restService, redirectService) {
+function InvIssueReturnActionController($scope, contextService, alertService, modalService, restService, redirectService) {
 
     $scope.hasBeenReturned = function (matusetransitem) {
         var data = matusetransitem['fields'];
         var deltaQty = data['quantity'] + data['qtyreturned'] >= 0;
         return deltaQty;
-    };
-
-    $scope.updateOpacity = function (matusetransitem) {
-        if ($scope.hasBeenReturned(matusetransitem)) {
-            return "low-opacity gray";
-        }
-
-        return "black";
     };
 
     $scope.isReturnHidden = function (matusetransitem) {
