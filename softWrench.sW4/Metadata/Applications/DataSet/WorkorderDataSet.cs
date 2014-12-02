6245f7810638366306beb92c1fbdc3cc19dae8f6
ckcheng83 <ccheng@controltechnologysolutions.com>
2014-12-01 23:59:58 -0700

    COMSW-2
    Added classification to work orders.   Also, updated classification on
    SR - part of SWWEB-539.
    
    **Code change in associatedService for the priority list issue.

--- softWrench.sW4/Metadata/Applications/DataSet/WorkorderDataSet.cs
+++ softWrench.sW4/Metadata/Applications/DataSet/WorkorderDataSet.cs
@@ -9,6 +9,7 @@ using softwrench.sw4.Shared2.Data.Association;
 using softWrench.sW4.Data.Search;
 using softwrench.sw4.Shared2.Util;
 using softWrench.sW4.Util;
+using System;
 
 namespace softWrench.sW4.Metadata.Applications.DataSet {
 
@@ -57,7 +58,6 @@ namespace softWrench.sW4.Metadata.Applications.DataSet {
             return AssetFilterBySiteFunction(parameters);
         }
 
-
         public SearchRequestDto AssetFilterBySiteFunction(AssociationPreFilterFunctionParameters parameters) {
             var filter = parameters.BASEDto;
             var location = (string)parameters.OriginalEntity.GetAttribute("location");
@@ -66,6 +66,79 @@ namespace softWrench.sW4.Metadata.Applications.DataSet {
             }
             filter.AppendSearchEntry("asset.location", location.ToUpper());
             return filter;
+        }
+
+        public IEnumerable<IAssociationOption> GetWOPriorityType(OptionFieldProviderParameters parameters)
+        {
+            var query = @"SELECT [description] AS LABEL,
+	                             CAST(value AS INT) AS VALUE 
+                          FROM numericdomain
+                          WHERE domainid = 'WO PRIORITY'";
+
+            var result = MaxDAO.FindByNativeQuery(query, null);
+            var list = new List<AssociationOption>();
+
+            if (result != null && result.Any())
+            {
+                foreach (var record in result)
+                    list.Add(new AssociationOption(record["VALUE"].ToString(), string.Format("{0} - {1}", record["VALUE"], record["LABEL"])));
+            }
+            else
+            {
+                // If no values are found, then default to numeric selection 1-5
+                list.Add(new AssociationOption("1", "1"));
+                list.Add(new AssociationOption("2", "2"));
+                list.Add(new AssociationOption("3", "3"));
+                list.Add(new AssociationOption("4", "4"));
+                list.Add(new AssociationOption("5", "5"));
+            }
+
+            return list;
+        }
+
+        public IEnumerable<IAssociationOption> GetWOClassStructureType(OptionFieldProviderParameters parameters)
+        {
+
+            // TODO: Change the design to use a tree view component
+            var query = string.Format(@"SELECT c.classstructureid AS ID,
+                                               p3.classificationid AS CLASS_5,
+                                               p2.classificationid AS CLASS_4,
+                                               p1.classificationid AS CLASS_3,
+                                               p.classificationid AS CLASS_2,
+                                               c.classificationid AS CLASS_1
+                                        from classstructure as c
+                                        left join classstructure as p on p.classstructureid = c.parent
+                                        left join classstructure as p1 on p1.classstructureid = p.parent
+                                        left join classstructure as p2 on p2.classstructureid = p1.parent
+                                        left join classstructure as p3 on p3.classificationid = p2.parent
+                                        where
+                                        c.haschildren = 0
+                                        and (c.orgid is null or (c.orgid is not null and c.orgid = '{0}' ))
+                                        and (c.siteid is null or (c.siteid is not null and c.siteid = '{1}' ))
+                                        and c.classstructureid in (select classusewith.classstructureid
+                                        from classusewith
+                                        where classusewith.classstructureid=c.classstructureid
+                                        and objectname= '{2}')",
+                                        parameters.OriginalEntity.Attributes["orgid"],
+                                        parameters.OriginalEntity.Attributes["siteid"],
+                                        parameters.OriginalEntity.Attributes["woclass"] == "" ? parameters.OriginalEntity.Attributes["woclass"] : "WORKORDER");
+
+            var result = MaxDAO.FindByNativeQuery(query, null);
+            var list = new List<AssociationOption>();
+
+            foreach (var record in result)
+            {
+                list.Add(new AssociationOption(record["ID"],
+                String.Format("{0}{1}{2}{3}{4}",
+                            record["CLASS_5"] == null ? "" : record["CLASS_5"] + "/",
+                            record["CLASS_4"] == null ? "" : record["CLASS_4"] + "/",
+                            record["CLASS_3"] == null ? "" : record["CLASS_3"] + "/",
+                            record["CLASS_2"] == null ? "" : record["CLASS_2"] + "/",
+                            record["CLASS_1"] == null ? "" : record["CLASS_1"]
+                            )));
+            }
+
+            return list;
         }
 
         public override string ApplicationName() {
