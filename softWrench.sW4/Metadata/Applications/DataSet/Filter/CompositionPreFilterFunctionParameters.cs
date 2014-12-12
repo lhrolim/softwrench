3dbe6b82bb4228cf320acc8765b5efb5ec2ad11f
lhrolim <luizhenrique.rolim@gmail.com>
2014-12-12 18:14:35 -0200

    adding extra parameter to compositionprefilterparam

--- softWrench.sW4/Metadata/Applications/DataSet/Filter/CompositionPreFilterFunctionParameters.cs
+++ softWrench.sW4/Metadata/Applications/DataSet/Filter/CompositionPreFilterFunctionParameters.cs
@@ -1,15 +1,20 @@
 ﻿using softWrench.sW4.Data.Search;
+using softWrench.sW4.Metadata.Entities.Schema;
 using softwrench.sW4.Shared2.Data;
 using softwrench.sW4.Shared2.Metadata.Applications.Relationships.Compositions;
 
 namespace softWrench.sW4.Metadata.Applications.DataSet.Filter {
     public class CompositionPreFilterFunctionParameters : BasePreFilterParameters<ApplicationCompositionCollectionSchema> {
+        private readonly EntitySchema _schema;
 
 
-        public CompositionPreFilterFunctionParameters(SearchRequestDto baseDto,
-            AttributeHolder originalEntity, ApplicationCompositionCollectionSchema composition)
+        public CompositionPreFilterFunctionParameters(EntitySchema schema, SearchRequestDto baseDto, AttributeHolder originalEntity, ApplicationCompositionCollectionSchema composition)
             : base(baseDto, composition, originalEntity) {
+            _schema = schema;
         }
 
+        public EntitySchema Schema {
+            get { return _schema; }
+        }
     }
 }
