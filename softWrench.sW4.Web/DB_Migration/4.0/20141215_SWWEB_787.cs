b80579591594b727391a40558e96f5d4e62d46a2
ckcheng83 <ccheng@controltechnologysolutions.com>
2014-12-15 12:09:50 -0700

    Update softWrench DB
    Updated ApplicationItemId to nvarchar inside MAX_COMMREADFLAG.

---
+++ softWrench.sW4.Web/DB_Migration/4.0/20141215_SWWEB_787.cs
@@ -0,0 +1,22 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using FluentMigrator;
+using softWrench.sW4.Util;
+using softWrench.sW4.Web.Util;
+
+namespace softWrench.sW4.Web.DB_Migration._4._0
+{
+    [Migration(201412151205)]
+    public class Migration201410021630SWWEB787 : FluentMigrator.Migration
+    {
+        public override void Up()
+        {
+            // Updated the column to nvarchar without dropping the content
+            Alter.Table("MAX_COMMREADFLAG").AlterColumn("ApplicationItemId").AsString(MigrationUtil.StringMedium).Nullable();
+        }
+
+        public override void Down()
+        {
+
+        }
+    }
+}
