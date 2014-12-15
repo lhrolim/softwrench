d447817cd740ea91004cafca3284cad9340f9a4f
Ken <kpederson@controltechnologysolutions.com>
2014-12-15 12:29:10 -0700

    Implement formatter and filterfields on lookupmodal
    Added the formatter and different filter field types to the lookupModal.
    
    Added kit and rotating columns to the item lookup.

--- softWrench.sW4.Web/Content/Scripts/client/lookupModal.js
+++ softWrench.sW4.Web/Content/Scripts/client/lookupModal.js
@@ -41,7 +41,8 @@ app.directive('lookupModal', function (contextService) {
             datamap: '='
         },
 
-        controller: function ($scope, $http, $element, searchService, i18NService, associationService) {
+        controller: function ($scope, $http, $element, searchService, i18NService, associationService,
+                              formatService, expressionService) {
 
             $scope.lookupModalSearch = function (pageNumber) {
                 var schema = $scope.schema;
@@ -177,6 +178,73 @@ app.directive('lookupModal', function (contextService) {
                     $scope.lookupModalSearch();
                 }
             });
+
+            $scope.getFormattedValue = function (value, column, datamap) {
+                var formattedValue = formatService.format(value, column, datamap);
+                if (formattedValue == "-666") {
+                    //this magic number should never be displayed! 
+                    //hack to make the grid sortable on unions, where we return this -666 instead of null, but then remove this from screen!
+                    return null;
+                }
+                return formattedValue;
+            };
+
+            $scope.handleDefaultValue = function(data, column) {
+                var key = column.target ? column.target : column.attribute;
+
+                if (column.defaultValue != null && data[key] == null) {
+                    if (column.enableDefault != null && expressionService.evaluate(column.enableDefault, data)) {
+                        data[key] = column.defaultValue;
+                    }
+                }
+            };
+
+            $scope.getOperator = function (columnName) {
+                var searchOperator = $scope.searchOperator;
+                if (searchOperator != null && searchOperator[columnName] != null) {
+                    return searchOperator[columnName];
+                }
+                return searchService.getSearchOperation(0);
+            };
+
+            $scope.getSearchIcon = function(columnName) {
+                var showSearchIcon = $scope.schema.properties["list.advancedfilter.showsearchicon"] != "false";
+                var operator = $scope.getOperator(columnName);
+                return showSearchIcon ? operator.symbol : "";
+            };
+
+            $scope.GetAssociationOptions = function(fieldMetadata, forfilter) {
+                if (fieldMetadata.type == "OptionField") {
+                    return $scope.GetOptionFieldOptions(fieldMetadata, forfilter);
+                }
+                $scope.$parent.associationOptions = instantiateIfUndefined($scope.$parent.associationOptions);
+                return $scope.$parent.associationOptions[fieldMetadata.associationKey];
+            };
+
+            $scope.GetOptionFieldOptions = function(optionField, forfilter) {
+                if (optionField.providerAttribute == null) {
+                    return optionField.options;
+                }
+                optionField.jscache = instantiateIfUndefined(optionField.jscache);
+                if (optionField.jscache.providerOptions) {
+                    return optionField.jscache.providerOptions;
+                }
+                $scope.$parent.associationOptions = instantiateIfUndefined($scope.$parent.associationOptions);
+                var associationOptions = $scope.$parent.associationOptions[optionField.providerAttribute];
+                if (forfilter || optionField.addBlankOption) {
+                    associationOptions.unshift({
+                        label: "",
+                        value: ""
+                    });
+                }
+                optionField.jscache.providerOptions = associationOptions;
+                return associationOptions;
+            };
+
+            $scope.shouldShowHeaderLabel = function(column) {
+                return (column.type == "ApplicationFieldDefinition" || column.type == "OptionField") && column.rendererType != "color" && column.rendererType != "icon";
+            };
+
         }
     };
 });
