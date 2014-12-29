1cd018f515fd03ab8ae1e9db82ebedfb11cbc14c
lhrolim <luizhenrique.rolim@gmail.com>
2014-12-29 21:47:26 -0200

    refactoring

--- softWrench.sW4.Web/Content/Scripts/client/directives/fileread.js
+++ softWrench.sW4.Web/Content/Scripts/client/directives/fileread.js
@@ -6,67 +6,67 @@
         },
 
 
-        link: function(scope, element, attributes) {
-            element.bind("change", function(changeEvent) {
-                var validFileTypes = ["pdf", "zip", "txt", "doc", "docx", "dwg", "gif", "jpg", "csv", "xls", "xlsx", "ppt", "xml", "xsl", "bmp", "html", "png"];
-                var fileName = [];
-                var flag = -1;
-                if (!isIe9()) {
-                    var reader = new FileReader();
-                    if (changeEvent.target.files.length > 0) {
-                        //Getting the File extension.
-                        for (i = 0; i < changeEvent.target.files.length; i++) {
-                            var temp = changeEvent.target.files[i].name.split(".").pop().toLowerCase();
-                            if (validFileTypes.indexOf(temp) == -1) {
-                                changeEvent.currentTarget.value = "";
-                                alertService.alert("Invalid file type. Allowed file types are:" + validFileTypes.join(', '));
-                                //Updating the model
-                                scope.$apply(function() {
-                                    scope.fileread = undefined;
-                                    scope.path = undefined;
-                                });
-                                return;
-                            }
-                        }
-                        flag = 1;
-                        var file;
-                        var fileRead = [];
-                        var current = 0;
-                        var readLoad = function(flag, loadEvent) {
-                            scope.$apply(function () {
-                                if (flag == 1) {
-                                    fileRead.push(loadEvent.target.result);
-                                    readFiles();
-                                }
-                            });
-                        };
-                        var readFiles = function() {
-                            if (isIe9() || changeEvent.target.files.length == 0) { // Using an opposite if to return when there aren't any files 
-                                return;
-                            }
-                            var fileNew = changeEvent.target.files[current]; // get the first from queue and store in file
-                            current++;
-                            if (current == changeEvent.target.files.length + 1) {
-                                scope.fileread = fileRead;
-                                return;
-                            }
-                            reader.onloadend = function(loadEvent) { // when finished reading file, call recursive readFiles function
-                                readLoad(flag, loadEvent);
-                            };
-                            reader.readAsDataURL(fileNew);
-                        };
-                        readFiles();
-                        for (i = 0; i < changeEvent.target.files.length; i++) {
-                            file = changeEvent.target.files[i];
-                            fileName.push(file.name);
-                        }
 
-                    }
-                };
-                if (flag == 1) {
-                    scope.path = fileName;
-                    $('[data-upload=files]')[0].value = fileName;
+
+        link: function (scope, element, attributes) {
+
+            var validFileTypes = ["pdf", "zip", "txt", "doc", "docx", "dwg", "gif", "jpg", "csv", "xls", "xlsx", "ppt", "xml", "xsl", "bmp", "html", "png"];
+
+            var readFiles = function (changeEvent,fileRead,reader,current) {
+                if (isIe9() || changeEvent.target.files.length == 0) {
+                    return;
                 }
+                var fileNew = changeEvent.target.files[current]; // get the first from queue and store in file
+                current++;
+                if (current == changeEvent.target.files.length + 1) {
+                    scope.fileread = fileRead;
+                    return;
+                }
+                reader.onloadend = function (loadEvent) { // when finished reading file, call recursive readFiles function
+                    scope.$apply(function () {
+                        fileRead.push(loadEvent.target.result);
+                        readFiles(changeEvent, fileRead, reader, current);
+                    });
+                };
+                reader.readAsDataURL(fileNew);
+            };
+
+            element.bind("change", function (changeEvent) {
+
+                if (isIe9() || changeEvent.target.files.length == 0) {
+                    // if we´re on ie9 we need to submit the files using a form since it doesnt have the FileReader object present (HTML5+)
+                    return;
+                }
+
+                var fileName = [];
+                var reader = new FileReader();
+
+                //Getting the File extension.
+                for (var i = 0; i < changeEvent.target.files.length; i++) {
+                    var temp = changeEvent.target.files[i].name.split(".").pop().toLowerCase();
+                    if (validFileTypes.indexOf(temp) == -1) {
+                        changeEvent.currentTarget.value = "";
+                        alertService.alert("Invalid file type. Allowed file types are:" + validFileTypes.join(', '));
+                        //Updating the model
+                        scope.$apply(function () {
+                            scope.fileread = undefined;
+                            scope.path = undefined;
+                        });
+                        return;
+                    }
+                }
+                var file;
+                var fileRead = [];
+             
+                readFiles(changeEvent, fileRead, reader,0);
+                for (i = 0; i < changeEvent.target.files.length; i++) {
+                    file = changeEvent.target.files[i];
+                    fileName.push(file.name);
+                }
+
+                scope.path = fileName;
+                $('[data-upload=files]')[0].value = fileName;
             });;
         }
-    }; });
+    };
+});
