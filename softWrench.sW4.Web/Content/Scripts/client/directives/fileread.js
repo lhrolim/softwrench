24d08bb41c09b67f8b1346e0131565909abe0199
rrpranav <rraja@controltechnologysolutions.com>
2014-12-29 15:16:44 -0700

    Addressing Changes - 2

--- softWrench.sW4.Web/Content/Scripts/client/directives/fileread.js
+++ softWrench.sW4.Web/Content/Scripts/client/directives/fileread.js
@@ -5,10 +5,10 @@
             path: "="
         },
 
-        
-        link: function (scope, element, attributes) {
-            element.bind("change", function (changeEvent) {
-                var validFileTypes = ["pdf", "zip", "txt", "doc", "docx", "dwg", "gif", "jpg", "csv", "xls", "xlsx", "ppt", "xml", "xsl", "bmp", "html" , "png"];
+
+        link: function(scope, element, attributes) {
+            element.bind("change", function(changeEvent) {
+                var validFileTypes = ["pdf", "zip", "txt", "doc", "docx", "dwg", "gif", "jpg", "csv", "xls", "xlsx", "ppt", "xml", "xsl", "bmp", "html", "png"];
                 var fileName = [];
                 var flag = -1;
                 if (!isIe9()) {
@@ -32,8 +32,16 @@
                         var file;
                         var fileRead = [];
                         var current = 0;
-                        var readFiles= function () {
-                            if (isIe9() || changeEvent.target.files.length ==0){ // Using an opposite if to return when there aren't any files 
+                        var readLoad = function(flag, loadEvent) {
+                            scope.$apply(function () {
+                                if (flag == 1) {
+                                    fileRead.push(loadEvent.target.result);
+                                    readFiles();
+                                }
+                            });
+                        };
+                        var readFiles = function() {
+                            if (isIe9() || changeEvent.target.files.length == 0) { // Using an opposite if to return when there aren't any files 
                                 return;
                             }
                             var fileNew = changeEvent.target.files[current]; // get the first from queue and store in file
@@ -42,13 +50,8 @@
                                 scope.fileread = fileRead;
                                 return;
                             }
-                            reader.onloadend = function (loadEvent) { // when finished reading file, call recursive readFiles function
-                                scope.$apply(function() {
-                                    if (flag == 1) {
-                                        fileRead.push(loadEvent.target.result);
-                                        readFiles();
-                                    }
-                                });
+                            reader.onloadend = function(loadEvent) { // when finished reading file, call recursive readFiles function
+                                readLoad(flag, loadEvent);
                             };
                             reader.readAsDataURL(fileNew);
                         };
@@ -60,10 +63,10 @@
 
                     }
                 };
-            if (flag == 1) {
+                if (flag == 1) {
                     scope.path = fileName;
-                    changeEvent.currentTarget.parentNode.parentNode.children[0].value = fileName;
+                    $('[data-upload=files]')[0].value = fileName;
                 }
             });;
-        },; };
-});
+        }
+    }; });
