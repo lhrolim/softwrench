5d18e758d0b97a4cdd31f9b0bbab472428f78081
lhrolim <luizhenrique.rolim@gmail.com>
2015-01-13 11:44:17 -0700

    HAP-811

---
+++ softWrench.sW4.Web/Content/Scripts/customers/hapag/attachment_service.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module('sw_layout');
+
+app.factory('attachmentservice', function () {
+
+    return {
+
+        validateNewForm: function (schema, datamap) {
+            var arr = [];
+            if (isIe9()) {
+                return arr;
+            }
+            var newAttachment = datamap['newattachment'];
+            var newScreenshot = datamap['newscreenshot'];
+            if (newScreenshot == "" && !newAttachment) {
+                arr.push('Either an attachment or a screenshot should be selected');
+            }
+            return arr;
+
+        }
+    };
+
+
+});
