78f970b4a7cb88f76cbf6b1ccd6ff329779d6a54
ckcheng83 <ccheng@controltechnologysolutions.com>
2015-01-19 16:13:54 -0700

    COMSW-39
    Update SR to retain service address.

--- softWrench.sW4/Data/Persistence/WS/Commons/BaseServiceRequestCrudConnector.cs
+++ softWrench.sW4/Data/Persistence/WS/Commons/BaseServiceRequestCrudConnector.cs
@@ -13,21 +13,45 @@ using System.Net;
 using softWrench.sW4.Configuration.Services.Api;
 using softWrench.sW4.SimpleInjector;
 using softWrench.sW4.Email;
+using softWrench.sW4.Data.Persistence.WS.Internal;
+using softWrench.sW4.Data.Persistence.Engine;
+
 
 namespace softWrench.sW4.Data.Persistence.WS.Commons {
 
-    class BaseServiceRequestCrudConnector : CrudConnectorDecorator {
+    class BaseServiceRequestCrudConnector : CrudConnectorDecorator
+    {
 
         protected AttachmentHandler _attachmentHandler;
 
         private readonly EmailService _emailService;
 
-        public BaseServiceRequestCrudConnector() {
+        public BaseServiceRequestCrudConnector()
+        {
             _attachmentHandler = new AttachmentHandler();
             _emailService = SimpleInjectorGenericFactory.Instance.GetObject<EmailService>(typeof(EmailService));
         }
+        
+        public override void BeforeCreation(MaximoOperationExecutionContext maximoTemplateData)
+        {
+            // Update common fields or transactions prior to maximo operation exection
+            CommonTransaction(maximoTemplateData);
 
-        public override void BeforeUpdate(MaximoOperationExecutionContext maximoTemplateData) {
+            base.BeforeCreation(maximoTemplateData);
+        }
+
+        public override void AfterCreation(MaximoOperationExecutionContext maximoTemplateData)
+        {
+            base.AfterUpdate(maximoTemplateData);
+            maximoTemplateData.OperationData.Id = maximoTemplateData.ResultObject.Id;
+            maximoTemplateData.OperationData.OperationType = Internal.OperationType.AddChange; 
+
+            // Resubmitting MIF for ServiceAddress Update
+            ConnectorEngine.Update((CrudOperationData)maximoTemplateData.OperationData);
+        }
+
+        public override void BeforeUpdate(MaximoOperationExecutionContext maximoTemplateData)
+        {
             // Update common fields or transactions prior to maximo operation exection
             CommonTransaction(maximoTemplateData);
 
@@ -35,21 +59,17 @@ namespace softWrench.sW4.Data.Persistence.WS.Commons {
             var crudData = ((CrudOperationData)maximoTemplateData.OperationData);
 
             var mailObject = maximoTemplateData.Properties;
-            
+
             WorkLogHandler.HandleWorkLogs(crudData, sr);
             CommLogHandler.HandleCommLogs(maximoTemplateData, crudData, sr);
+
+            HandleServiceAddress(maximoTemplateData);
 
             base.BeforeUpdate(maximoTemplateData);
         }
 
-        public override void BeforeCreation(MaximoOperationExecutionContext maximoTemplateData) {
-            // Update common fields or transactions prior to maximo operation exection
-            CommonTransaction(maximoTemplateData);
-
-            base.BeforeCreation(maximoTemplateData);
-        }
-
-        public override void AfterUpdate(MaximoOperationExecutionContext maximoTemplateData) {
+        public override void AfterUpdate(MaximoOperationExecutionContext maximoTemplateData)
+        {
             if (maximoTemplateData.Properties.ContainsKey("mailObject")) {
                 _emailService.SendEmail((EmailService.EmailData)maximoTemplateData.Properties["mailObject"]);
             }
@@ -59,7 +79,8 @@ namespace softWrench.sW4.Data.Persistence.WS.Commons {
         }
 
 
-        private void CommonTransaction(MaximoOperationExecutionContext maximoTemplateData) {
+        private void CommonTransaction(MaximoOperationExecutionContext maximoTemplateData)
+        {
             // Get current username that trigger the transaction
             var user = SecurityFacade.CurrentUser();
 
@@ -76,18 +97,47 @@ namespace softWrench.sW4.Data.Persistence.WS.Commons {
 
             // Update or create attachments
             var attachments = crudData.GetRelationship("attachment");
-            foreach (var attachment in (IEnumerable<CrudOperationData>)attachments) {
+            foreach (var attachment in (IEnumerable<CrudOperationData>)attachments)
+            {
                 HandleAttachmentAndScreenshot(attachment, sr, maximoTemplateData.ApplicationMetadata);
             }
         }
 
-        private void HandleAttachmentAndScreenshot(CrudOperationData data, object maximoObj, ApplicationMetadata applicationMetadata) {
+        private bool HandleServiceAddress(MaximoOperationExecutionContext maximoTemplateData)
+        {
+            var data = (CrudOperationData)maximoTemplateData.OperationData; 
+            var user = SecurityFacade.CurrentUser();
+            var saddresscode = data.GetUnMappedAttribute("saddresscode");
 
+            if (saddresscode == null) return false;
+
+            var description = data.GetUnMappedAttribute("#tkdesc");
+            var formattedaddr = data.GetUnMappedAttribute("#tkformattedaddress");
+            var streetnumber = data.GetUnMappedAttribute("#tkstaddrnumber");
+            var streetaddr = data.GetUnMappedAttribute("#tkstaddrstreet");
+            var streettype = data.GetUnMappedAttribute("#tkstaddrsttype");
+
+            var tkserviceaddress = ReflectionUtil.InstantiateSingleElementFromArray(maximoTemplateData.IntegrationObject, "TKSERVICEADDRESS");
+            w.SetValueIfNull(tkserviceaddress, "TKSERVICEADDRESSID", -1);
+            w.SetValue(tkserviceaddress, "ORGID", user.OrgId);
+            w.SetValue(tkserviceaddress, "SADDRESSCODE", saddresscode);
+            w.SetValue(tkserviceaddress, "DESCRIPTION", description);
+            w.SetValue(tkserviceaddress, "FORMATTEDADDRESS", formattedaddr);
+            w.SetValue(tkserviceaddress, "STADDRNUMBER", streetnumber);
+            w.SetValue(tkserviceaddress, "STADDRSTREET", streetaddr);
+            w.SetValue(tkserviceaddress, "STADDRSTTYPE", streettype);
+
+            return true; 
+        }
+
+        private void HandleAttachmentAndScreenshot(CrudOperationData data, object maximoObj, ApplicationMetadata applicationMetadata)
+        {
             // Check if Attachment is present
             var attachmentString = data.GetUnMappedAttribute("newattachment");
             var attachmentPath = data.GetUnMappedAttribute("newattachment_path");
 
-            if (!String.IsNullOrWhiteSpace(attachmentString) && !String.IsNullOrWhiteSpace(attachmentPath)) {
+            if (!String.IsNullOrWhiteSpace(attachmentString) && !String.IsNullOrWhiteSpace(attachmentPath))
+            {
                 _attachmentHandler.HandleAttachments(maximoObj, attachmentString, attachmentPath, applicationMetadata);
             }
 
@@ -95,9 +145,11 @@ namespace softWrench.sW4.Data.Persistence.WS.Commons {
             var screenshotString = data.GetUnMappedAttribute("newscreenshot");
             var screenshotName = data.GetUnMappedAttribute("newscreenshot_path");
 
-            if (!String.IsNullOrWhiteSpace(screenshotString) && !String.IsNullOrWhiteSpace(screenshotName)) {
+            if (!String.IsNullOrWhiteSpace(screenshotString) && !String.IsNullOrWhiteSpace(screenshotName))
+            {
 
-                if (screenshotName.ToLower().EndsWith("rtf")) {
+                if (screenshotName.ToLower().EndsWith("rtf"))
+                {
                     var bytes = Convert.FromBase64String(screenshotString);
                     var decodedString = Encoding.UTF8.GetString(bytes);
                     var compressedScreenshot = CompressionUtil.CompressRtf(decodedString);
--- softWrench.sW4/Data/Persistence/WS/Manchester/ComServiceRequestCrudConnector.cs
+++ softWrench.sW4/Data/Persistence/WS/Manchester/ComServiceRequestCrudConnector.cs
@@ -15,38 +15,18 @@ namespace softWrench.sW4.Data.Persistence.WS.Manchester
 {
     class ComServiceRequestCrudConnector : BaseServiceRequestCrudConnector
     {
-        public override void BeforeUpdate(MaximoOperationExecutionContext maximoTemplateData)
-        {
+        public override void BeforeUpdate(MaximoOperationExecutionContext maximoTemplateData) {
             base.BeforeUpdate(maximoTemplateData);
+            
             var sr = maximoTemplateData.IntegrationObject;
             var crudData = ((CrudOperationData)maximoTemplateData.OperationData);
-            HandleTKServiceAddress(crudData, sr);
         }
-        public override void BeforeCreation(MaximoOperationExecutionContext maximoTemplateData)
-        {
+
+        public override void BeforeCreation(MaximoOperationExecutionContext maximoTemplateData) {
             base.BeforeCreation(maximoTemplateData);
+            
             var sr = maximoTemplateData.IntegrationObject;
             var crudData = ((CrudOperationData)maximoTemplateData.OperationData);
-        }
-        public static T HandleTKServiceAddress<T>(CrudOperationData entity, T rootObject)
-        {
-            var user = SecurityFacade.CurrentUser();
-            var saddresscode = entity.GetUnMappedAttribute("saddresscode");
-            var description = entity.GetUnMappedAttribute("#tkdesc");
-            var formattedaddr = entity.GetUnMappedAttribute("#tkformattedaddress");
-            var streetnumber = entity.GetUnMappedAttribute("#tkstaddrnumber");
-            var streetaddr = entity.GetUnMappedAttribute("#tkstaddrstreet");
-            var streettype = entity.GetUnMappedAttribute("#tkstaddrsttype");
-            var tkserviceaddress = ReflectionUtil.InstantiateSingleElementFromArray(rootObject, "TKSERVICEADDRESS");
-            w.SetValueIfNull(tkserviceaddress, "TKSERVICEADDRESSID", -1);
-            w.SetValue(tkserviceaddress, "ORGID", user.OrgId);
-            w.SetValue(tkserviceaddress, "SADDRESSCODE", saddresscode);
-            w.SetValue(tkserviceaddress, "DESCRIPTION", description);
-            w.SetValue(tkserviceaddress, "FORMATTEDADDRESS", formattedaddr);
-            w.SetValue(tkserviceaddress, "STADDRNUMBER", streetnumber);
-            w.SetValue(tkserviceaddress, "STADDRSTREET", streetaddr);
-            w.SetValue(tkserviceaddress, "STADDRSTTYPE", streettype);
-            return rootObject;
         }
     }
 }
