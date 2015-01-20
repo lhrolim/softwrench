d115af22f308f4ffa029ceb394ccc1c70d179894
ckcheng83 <ccheng@controltechnologysolutions.com>
2015-01-20 16:08:23 -0700

    Added GL Lookup for Bering
    Please review - Jordan.

--- softWrench.sW4.Web/Content/Scripts/client/services/glcomponent_service.js
+++ softWrench.sW4.Web/Content/Scripts/client/services/glcomponent_service.js
@@ -2,18 +2,24 @@
 
 app.factory('glcomponentService', function ($http, modalService, validationService) {
     return {
+
         // A new getGLAccount() is needed for the client profile to get their customization.
         // Have the metadata.xml redirect to that service and function.
         getGLAccount: function (datamap, schema, selecteditem, attribute) {
+            var validationErrors = validationService.validate(schema, schema.displayables, datamap);
+            if (validationErrors.length > 0) {
+                //interrupting here, can´t be done inside service
+                return;
+            }
 
             // refer to glconfigure table to program this section
             // this will be different for each account
-            var result = selecteditem["glpart-0"] == undefined ? "????" : selecteditem["glpart-0"];
-            result += selecteditem["glpart-1"] == undefined ? "-???" : "-" + selecteditem["glpart-1"];
-            result += selecteditem["glpart-2"] == undefined ? "-???" : '-' + selecteditem["glpart-2"];
-            result += selecteditem["glpart-3"] == undefined ? "" : '-' + selecteditem["glpart-3"];
-            // Output results to the parent schema
+            var result = selecteditem["glpart-0"];
+            result += selecteditem["glpart-1"];
+            result += selecteditem["glpart-2"];
+            result += selecteditem["glpart-3"];
 
+            // Output results to the parent schema
             datamap[attribute.target] = result;
 
             // close the modal
