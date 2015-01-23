5e25a148558a7a5cf03d2541d5b0c8aa21488008
ckcheng83 <ccheng@controltechnologysolutions.com>
2015-01-23 09:44:30 -0700

    COMSW-8
    Added code for recording actual tools.

--- softWrench.sW4/Data/Persistence/WS/Commons/BaseWorkOrderCrudConnector.cs
+++ softWrench.sW4/Data/Persistence/WS/Commons/BaseWorkOrderCrudConnector.cs
@@ -72,6 +72,7 @@ namespace softWrench.sW4.Data.Persistence.WS.Commons {
             WorkLogHandler.HandleWorkLogs((CrudOperationData)maximoTemplateData.OperationData, wo);
             HandleMaterials((CrudOperationData)maximoTemplateData.OperationData, wo);
             HandleLabors((CrudOperationData)maximoTemplateData.OperationData, wo);
+            HandleTools((CrudOperationData)maximoTemplateData.OperationData, wo);
             HandleAttachments((CrudOperationData)maximoTemplateData.OperationData, wo, maximoTemplateData.ApplicationMetadata);
         }
 
@@ -143,6 +144,39 @@ namespace softWrench.sW4.Data.Persistence.WS.Commons {
 
                 WsUtil.SetValueIfNull(integrationObject, "ISSUETYPE", "ISSUE");
                 WsUtil.SetValue(integrationObject, "MATUSETRANSID", -1);
+
+                ReflectionUtil.SetProperty(integrationObject, "action", OperationType.Add.ToString());
+            });
+        }
+
+        protected virtual void HandleTools(CrudOperationData entity, object wo) {
+            // Use to obtain security information from current user
+            var user = SecurityFacade.CurrentUser();
+
+            // Workorder id used for data association
+            var recordKey = entity.UserId;
+
+            // Filter work order materials for any new entries where matusetransid is null
+            var Tools = (IEnumerable<CrudOperationData>)entity.GetRelationship("tooltrans");
+            var newTools = Tools.Where(r => r.GetAttribute("tooltransid") == null);
+
+            // Convert collection into array, if any are available
+            var crudOperationData = newTools as CrudOperationData[] ?? newTools.ToArray();
+
+            WsUtil.CloneArray(crudOperationData, wo, "TOOLTRANS", delegate(object integrationObject, CrudOperationData crudData) {
+                WsUtil.SetValueIfNull(integrationObject, "TOOLRATE", 0.00);
+                WsUtil.SetValueIfNull(integrationObject, "TOOLQTY", 0);
+                WsUtil.SetValueIfNull(integrationObject, "TOOLHRS", 0); 
+                
+                WsUtil.SetValue(integrationObject, "ORGID", user.OrgId);
+                WsUtil.SetValue(integrationObject, "SITEID", user.SiteId);
+                WsUtil.SetValue(integrationObject, "REFWO", recordKey);
+
+                WsUtil.SetValue(integrationObject, "ENTERBY", user.Login);
+                WsUtil.SetValue(integrationObject, "ENTERDATE", DateTime.Now.FromServerToRightKind(), true);
+
+                WsUtil.SetValue(integrationObject, "TOOLTRANSID", -1);
+                WsUtil.SetValue(integrationObject, "TRANSDATE", DateTime.Now.FromServerToRightKind(), true);
 
                 ReflectionUtil.SetProperty(integrationObject, "action", OperationType.Add.ToString());
             });
