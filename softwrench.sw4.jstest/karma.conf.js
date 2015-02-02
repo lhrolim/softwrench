67b33008210cb78afe5a78c474c3743375349906
luiz <luizhenrique.rolim@gmail.com>
2015-02-02 13:21:36 -0200

    increasing timeout

--- softwrench.sw4.jstest/karma.conf.js
+++ softwrench.sw4.jstest/karma.conf.js
@@ -55,7 +55,7 @@ module.exports = function(config) {
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-    browserNoActivityTimeout: 20000,
+    browserNoActivityTimeout: 40000,
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
