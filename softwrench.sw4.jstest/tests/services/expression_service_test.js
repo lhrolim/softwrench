39defbacad52c80d76eca934632c6f9a0f2f66b2
luiz <luizhenrique.rolim@gmail.com>
2015-02-02 18:00:09 -0200

    improving test

--- softwrench.sw4.jstest/tests/services/expression_service_test.js
+++ softwrench.sw4.jstest/tests/services/expression_service_test.js
@@ -4,10 +4,20 @@
     beforeEach(inject(function (_expressionService_) {
         expressionService = _expressionService_;
     }));
+
     it('should match variables by @', function () {
+        var variables = expressionService.getVariables("@status == 'test'");
+        expect(variables.length).toBe(1);
+        expect(variables[0]).toBe('status');
+    });
+
+    it('should match variables by @ 2', function () {
         var variables = expressionService.getVariables("@status == 'test' and @ticketid!=null");
         expect(variables.length).toBe(2);
         expect(variables[0]).toBe('status');
         expect(variables[1]).toBe('ticketid');
     });
+
+    
+
 });
