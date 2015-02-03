26e173dbbee66c92245bdc2eabf6abcfc42fab38
jmvogt <jvogt@controltechnologysolutions.com>
2015-02-03 04:26:45 -0700

    Merge with Develop

---
+++ softwrench.sw4.jstest/tests/HelloWorldKarma.js
@@ -0,0 +1,5 @@
+﻿describe("a test", function() {
+     it("passes", function() {
+          expect(true).toBe(true);
+     });
+});
---
+++ softwrench.sw4.jstest/tests/services/expression_service_test.js
@@ -0,0 +1,23 @@
+﻿describe('expressionService', function () {
+    var expressionService;
+    beforeEach(module('sw_layout'));
+    beforeEach(inject(function (_expressionService_) {
+        expressionService = _expressionService_;
+    }));
+
+    it('should match variables by @', function () {
+        var variables = expressionService.getVariables("@status == 'test'");
+        expect(variables.length).toBe(1);
+        expect(variables[0]).toBe('status');
+    });
+
+    it('should match variables by @ 2', function () {
+        var variables = expressionService.getVariables("@status == 'test' and @ticketid!=null");
+        expect(variables.length).toBe(2);
+        expect(variables[0]).toBe('status');
+        expect(variables[1]).toBe('ticketid');
+    });
+
+    
+
+});
