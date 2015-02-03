ef93f7225e26842a382cf4fd0ae4f3174fd2e51e
jmvogt <jvogt@controltechnologysolutions.com>
2015-02-03 03:57:44 -0700

    Expression Service Updates

--- softWrench.sW4.Web/Content/Scripts/client/services/expression_service.js
+++ softWrench.sW4.Web/Content/Scripts/client/services/expression_service.js
@@ -1,26 +1,24 @@
 ﻿var app = angular.module('sw_layout');
 
 app.factory('expressionService', function ($rootScope, contextService, dispatcherService) {
-        
-//                   Example Regex Tester W/ Examples URL:
-//                    https://www.regex101.com/r/fB6kI9/12               */
+
 
     var compiledDatamapReplaceRegex = /(\@\#?)(\w+(\.?\w?)*)/g;
+    //var datamapReplaceRegexString = "(\@\#?)(\w+(\.?\w?)*)";
 
-  //var datamapReplaceRegexString = "(\@\#?)(\w+(\.?\w?)*)";
     var datamapReplaceRegexString = "(\@\#?)" +
   //                                Looks leading @ or @#
                                     "(\w+(\.?\w?)*)";
   //                                At least one word, followed by unlimited number of .word
   //    
-  //                        Examples:
+    //                      Examples:      https://www.regex101.com/r/fB6kI9/24
   //                                @inventory_.item_.itemnum                  
   //                                @assetnum                                  
   //                                @#customfield   
 
     var compiledScopeReplaceRegex = /\$\.(\w+)((\.\w+)|(\[.*?\]+)|(\(.*?\)))*/g;
+    //var scopeReplaceRegexString = "\$\.(\w+)((\.\w+)|(\[.*?\]+)|(\(.*?\)))*";
 
-  //var scopeReplaceRegexString = "\$\.(\w+)((\.\w+)|(\[.*?\]+)|(\(.*?\)))*";
     var scopeReplaceRegexString = "\$\.(\w+)" +
 
   //                               Looks for leading $.word (will translate into scope.word)
@@ -28,57 +26,102 @@ app.factory('expressionService', function ($rootScope, contextService, dispatche
   //                               The following three conditions are OR'd together 
   //                               and can be repeated 0 or more times
         
-                                   "((\.\w+)|" +
+                                    "(" +
+                                                "(\.\w+)"                  + "|" +
   //                                Condition 1: Looks for .word
-                                    "(\[.*?\]+)|" +
+
+                                                "(\[.*?\]+)"                + "|" +
   //                                Condition 2: Looks for an open bracket [ and will accept
   //                                any characters until the first closing bracket ] is found.
   //                                There can be multiple closing brackets back to back. This
   //                                is sort of a hackey way of supporting nested dictionaries
-                                    "(\(.*?\)))*";
+
+                                                "(\(.*?\))" +
   //                                Condition 3: Looks for an open parenthesis ( and will accept
   //                                any characters until the first closing parenthesis ) is found
 
-  //                          Examples:
-  //                                    $.createFlag
-  //                                    $.selectedValue()
-  //                                    $.previousdata[@assetnum]
-  //                                    $.saveItem($.datamap, $.schema, @assetnum)
-  //                                    $.previousdata.fields['wonum'].list[@assetnum]
-  //                                    $.previousdata.lookupAssociationCode[@assetnum].[$.previousdata.fields()]
-  //                                    $.previousdata.fields('CAT', var, @assetnum).list[@assetnum]
-  //                                    $.previousdata.fields(@assetnum, 'CAT', var).update(@currentVariable)
+                                    ")*";
 
-    var compiledServiceReplaceRegex = /fn\:\w+\.\w+(\(.*?\)(\s?\,\s?.*?\))*)((\.\w+)|(\[.*?\])|\)|(\(.*?\)(\s?\,\s?.*?\))*))*/g;
+    //                          Examples:     https://www.regex101.com/r/fB6kI9/22
+                 /*                       $.previousdata.fields['wonum'].list[@assetnum]
+                                          $.previousdata.fields[wonum].list[key]
+                                          $.previousdata.fields('CAT')
+                                          $.previousdata.fields(@#assetnum)
+                                          $.previousdata.fields(var)
+                                          $.previousdata.fields(var,'CAT',@assetnum)
+                                          $.previousdata.fields('CAT', var, @assetnum).list[@assetnum]
+                                          $.previousdata.fields(@assetnum, 'CAT', var).update(@currentVariable)
+                                          $.previousdata.fields(var)
+                                          $.previousdata['@wonum'].fields
+                                          $.previousdata.fields.test
+                                          $.previousdata.fields[@assetnum]
+                                          $.lookupAssociationCode[@#lookupCode]
+                                          $.currentfunction($.datamap[@assetnum])
+                                          $.scope.datamap[@assetnum]                  */
 
-  //var serviceReplaceRegexString = "fn\:\w+\.\w+(\(.*?\)(\s?\,\s?.*?\))*)((\.\w+)|(\[.*?\])|\)|(\(.*?\)(\s?\,\s?.*?\))*))*";
-    var serviceReplaceRegexString = "fn\:\w+\.\w+(\(.*?\)(\s?\,\s?.*?\))*)((\.\w+)|(\[.*?\])|\)|(\(.*?\)(\s?\,\s?.*?\))*))*";
+
+    var compiledServiceReplaceRegex = /fn\:\w+\.\w+(\(.*?\)((\s?\,\s?.*?)*\))*)((\.\w+)|(\[.*?\])|\)|(\(.*?\)((\s?\,\s?.*?)*\))*))*/g;
+    //var serviceReplaceRegexString = "fn\:\w+\.\w+(\(.*?\)((\s?\,\s?.*?)*\))*)((\.\w+)|(\[.*?\])|\)|(\(.*?\)((\s?\,\s?.*?)*\))*))*";
+
+    var serviceReplaceRegexString = "fn\:\w+\.\w+" + 
   //                                Looks for leading fn:word.word (will translate into service.method call)
 
-  //                                Looks for an open parenthesis ( and will accept
-  //                                any characters until the first closing parenthesis ) is found
-
-
-  //                                The following three conditions are OR'd together 
+                                    "(\(.*?\)" + "((\s?\,\s?.*?)*\))*)" +
+  //                                - Looks for an open parenthesis ( and will accept
+  //                                  any characters until the first closing parenthesis ) is found
+  //                                - Next, the above pattern could be followed with an unlimited number of
+  //                                  commas that are followed by a word. Finally, this condition would expect
+  //                                  a closed parenthesis. This part of the condition can occur 0 to unlimited number of times
+  //                                  This case will allow for identifying variables nested within ( ) i.e. $.test($.test(), $.test())
+    
+  //                                The following four conditions are OR'd together 
   //                                and can be repeated 0 or more times
-                                    //"((\.\w+)|" +
+
+                                    "(" +
+
+                                               "(\.\w+)"                        + "|" +
   //                                Condition 1: Looks for .word
-                                    //"(\[.*?(?=\])\])|" +
+
+
+                                               "(\[.*?\])"                      + "|" +
   //                                Condition 2: Looks for an open bracket [ and will accept
-  //                                any characters until the first closing bracket ] is found
+  //                                any characters until the first closing bracket ] is found.
+  //                                This allows for you to call a function, and then refer to a 
+  //                                key in its result. For example, 
+  //                                     fn:contextService.getCurrentDatamap()[@assetnum]
+  
+
+                                               "\)"                             + "|" +
+  //                                Condition 3: This OR with a close parenthesis ) will help when
+  //                                capturing variables nested within parenthesis.
+
+                                               "(\(.*?\)" + "(\s?\,\s?.*?\))*)" +
+  //                                Condition 4:
+  //                                - Looks for an open parenthesis ( and will accept
+  //                                  any characters until the first closing parenthesis ) is found
+  //                                - Next, the above pattern could be followed with an unlimited number of
+  //                                  commas that are followed by a word. Finally, this condition would expect
+  //                                  a closed parenthesis. This part of the condition can occur 0 to unlimited number of times
+  //                                  This case will allow for identifying variables nested within ( ) i.e. $.test($.test(), $.test())
+
+                                    ")*";
+
+                                    //"((\.\w+)|" +
+  //                                
+                                    //"(\[.*?(?=\])\])|" +
+  //                                
                                     //"(\(.*?(?=\))\)))*";
   //                                Condition 3: Looks for an open parenthesis ( and will accept
   //                                any characters until the first closing parenthesis ) is found
 
-  //                            Examples:
-  //                                    fn:inventoryService.addition(@#refsiteid, fn:inventoryService.getint('test, test'))
-  //                                    fn:inventoryService.addition(@#refsiteid, fn:inventoryService.getint('test, test')) + fn:test.function()
-  //                                    fn:inventoryService.retrieveCost($.datamap[@assetnum]).parseDouble()
-  //                                    $.saveItem($.datamap, $.schema, @assetnum)
-  //                                    $.previousdata.fields['wonum'].list[@assetnum]
-  //                                    $.previousdata.lookupAssociationCode[@assetnum].[$.previousdata.fields()]
-  //                                    $.previousdata.fields('CAT', var, @assetnum).list[@assetnum]
-  //                                    $.previousdata.fields(@assetnum, 'CAT', var).update(@currentVariable)
+    /*            Examples:                 https://www.regex101.com/r/fB6kI9/21                
+                    fn:customService.getTransformation(@assetnum) + fn:customService.getTransformation(@binnum)
+                    fn:inventoryService.retrieveCost($.datamap[@assetnum], $.test(@assetnum), $.test()).parseDouble(fn:test.method(@assetnum))
+                    fn:inventoryService.addition(@#refsiteid, fn:inventoryService.getint('test, test')) + fn:test.function()
+                    fn:inventoryService.addition($.test(), fn:inventoryService.getint('test, test'))
+                    fn:inventoryService.retrieveCost($.datamap[@assetnum]).parseDouble()
+                    fn:inventoryService.retrieveCost($.datamap[@assetnum], $.test()).parseDouble()
+                    fn:contextService.getCurrentDatamap()[@assetnum]                                    */
         
 
 
@@ -96,15 +139,15 @@ app.factory('expressionService', function ($rootScope, contextService, dispatche
         },
 
         getExpression: function (expression, datamap, scope) {
-            /*The third parameter (boolean flag) will force the getVariables
+        /*  The third parameter (boolean flag) will force the getVariables
               method to evaluate sub variables and only return a mapping for
-              the highest nodes. This means that nested variables will not have
-              their own key/value pair in the resulting dictionary*/
+              the root nodes. This means that nested variables will not have
+              their own key/value pair in the resulting dictionary          */
             var variables = this.getVariables(expression, datamap, true, scope);
 
-            /*Each dictionary key is used to quickly update an expression with its
+        /*  Each dictionary key is used to quickly update an expression with its
             true value. We loop through each variable, replacing any instance of the
-            key (original reference in metadata) with an expression we can evaluate*/
+            key (original reference in metadata) with an expression we can evaluate   */
             if (variables != null) {
                 $.each(variables, function (key, value) {
                         expression = expression.replace(key, value);
@@ -116,7 +159,14 @@ app.factory('expressionService', function ($rootScope, contextService, dispatche
             return expression;
         },
 
-        getVariables: function (expression, datamap, variableValueFlag, scope) {
+    /*  Returns a dictionary containing variables that are identified within the expression.
+        The dictionary key for the variable is the expression's original value, where the 
+        dictionary value is the evaluated/"mapped" value.
+
+        The onlyReturnRootNode would be set to true when trying to evaluate an expression.
+        When the flag is false, the resulting list will have all variables, including nested
+        variables.                                                                     */
+        getVariables: function (expression, datamap, onlyReturnRootNode, scope) {
             var variables = {};
 
             var scopeVariables = expression.match(compiledScopeReplaceRegex);
@@ -126,18 +176,15 @@ app.factory('expressionService', function ($rootScope, contextService, dispatche
                     var referenceVariable = scopeVariables[i];
                     var realVariable = referenceVariable.replace(/\$\./, 'scope.');
 
-
                     var declarations = this.extractSignaturesFromExpression(realVariable);
 
                     //Handles any extra functions that were picked up by the variableRegex
                     for (var j = 0; j < declarations.length; j++) {
                         var declaration = declarations[j].trim();
                         //Tests whether or not the realVariable has a subVariable within it
-                        //For example, the reference variable $.lookupAssociatonCode[@assetnum]
-                        //will translate into a real variable of scope.lookupAssociationCode[@assetnum].
-                        //Because the match has @assetnum as a subvariable, the below function will return true.
                         var subVariable = compiledScopeReplaceRegex.test(declaration) || compiledServiceReplaceRegex.test(declaration);
                         if (subVariable == true) {
+                            //Updates the realValue of current with the evaluation of its child nodes
                             var subVariables = this.getVariables(declaration, datamap);
 
                             //For each sub variable, updated the real variable reference
@@ -145,8 +192,7 @@ app.factory('expressionService', function ($rootScope, contextService, dispatche
                             // the variable sub variable to our current variables list.
                             $.each(subVariables, function (key, value) {
                                 realVariable = realVariable.replace(key, value);
-                                declarations[0] = declarations[0].replace(key, value);
-
+                                
                                 if (variables[key] != undefined && variableValueFlag == true) {
                                     delete variables[key];
                                 }
@@ -180,15 +226,24 @@ app.factory('expressionService', function ($rootScope, contextService, dispatche
                             //(the variable's true reference upon being evaluated) and add
                             // the variable sub variable to our current variables list.
                             $.each(subVariables, function (key, value) {
+                                //Updates the realValue of current with the evaluation of its child nodes
                                 realVariable = realVariable.replace(key, value);
+
+                                //Updates the current declaration with evaluation of its child nodes.
+                                //This will be used to identify/parse the parameters when injecting
+                                //the custom service
                                 declarations[0] = declarations[0].replace(key, eval(value));
 
-                                if (variables[key] != undefined && variableValueFlag == true) {
+                                //Deletes subVariable keys from resulting Dictionary if
+                                //onlyReturnRootNode flag is true
+                                if (variables[key] != undefined && onlyReturnRootNode == true) {
                                     delete variables[key];
                                 }
                             });
                         }
                     }
+                    
+                    //Captures character up to the first open parenthises (
                     var functionCallStr = realVariable.substring(0, realVariable.indexOf('('));
 
                     var functionCall = functionCallStr.split('.');
@@ -201,13 +256,12 @@ app.factory('expressionService', function ($rootScope, contextService, dispatche
 
                     var parameters = declarations[0].split(functionParameterRegex);
 
+                    //Calls dispatcherService to load the custom service
                     realVariable = dispatcherService.loadService(service, method, parameters);
 
                 }
-
-
+                //Updates the variables Dictionary with the evaluated value
                 variables[referenceVariable] = realVariable;
-
             }
 
             var datamapVariables = expression.match(compiledDatamapReplaceRegex);
