734949271afaf015790e431a8a16034a9475ee8f
luiz <luizhenrique.rolim@gmail.com>
2015-02-03 17:44:51 -0200

    refactoring

--- softWrench.sW4.Web/Content/Scripts/client/services/dispatcher_service.js
+++ softWrench.sW4.Web/Content/Scripts/client/services/dispatcher_service.js
@@ -16,15 +16,6 @@ app.factory('dispatcherService', function ($injector, $log) {
             return null;
         }
         var fn = dispatcher[method];
-
-        if (parameters != null) {
-            var args = [];
-            for (var i = 0; i < parameters.length; i++) {
-                args.push(parameters[i]);
-            }
-            fn = fn.apply(this, args);
-        }
-
         if (!fn) {
             log.warn('Method {0} not found on service {1}'.format(method, service));
             return null;
@@ -34,6 +25,20 @@ app.factory('dispatcherService', function ($injector, $log) {
     return {
         loadService: function(service, method, parameters) {
             return loadService(service, method, parameters);
+        },
+
+        invokeService: function(service, method, parameters) {
+            var fn = this.loadService(service, method, parameters);
+            if (fn == null) {
+                return null;
+            }
+            if (parameters != null) {
+                var args = [];
+                for (var i = 0; i < parameters.length; i++) {
+                    args.push(parameters[i]);
+                }
+                return fn.apply(this, args);
+            }
         }
     };
 
