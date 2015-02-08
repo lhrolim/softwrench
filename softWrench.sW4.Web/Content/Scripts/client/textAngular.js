3f92632598f2f9eac29d3069c46f4155139e4456
ckcheng83 <ccheng@controltechnologysolutions.com>
2015-02-08 11:28:58 -0700

    Enable Custom RTE Toolbar
    Removed the youtube, picture, and custom link from the rich textbox.

--- softWrench.sW4.Web/Content/Scripts/client/textAngular.js
+++ softWrench.sW4.Web/Content/Scripts/client/textAngular.js
@@ -1,15 +1,26 @@
-﻿//var app = angular.module('sw_layout');
+﻿var app = angular.module('sw_layout');
 
-//app.config(['$provide', function($provide) {
-//    $provide.decorator('taOptions', ['taRegisterTool', '$modal', '$delegate',
-//      function(taRegisterTool, $modal, taOptions) {
-//          // $delegate is the taOptions we are decorating
-//          // here we override the default toolbars specified in taOptions.
-//          taOptions.toolbar = [
-//                ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
-//                ['bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo', 'clear'],
-//                ['justifyLeft', 'justifyCenter', 'justifyRight'],
-//                ['html']
-//          ];
-//      }]);
-//}]);
+app.config(['$provide', function ($provide) {
+    // this demonstrates how to register a new tool and add it to the default toolbar
+    $provide.decorator('taOptions', ['$delegate', function (taOptions) {
+        // $delegate is the taOptions we are decorating
+        // here we override the default toolbars and classes specified in taOptions.
+        taOptions.toolbar = [
+            ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
+            ['bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo', 'clear'],
+            ['justifyLeft', 'justifyCenter', 'justifyRight'],
+            ['html']
+        ];
+        taOptions.classes = {
+            focussed: 'focussed',
+            toolbar: 'btn-toolbar',
+            toolbarGroup: 'btn-group',
+            toolbarButton: 'btn btn-default',
+            toolbarButtonActive: 'active',
+            disabled: 'disabled',
+            textEditor: 'form-control',
+            htmlEditor: 'form-control'
+        };
+        return taOptions; // whatever you return will be the taOptions
+    }]);
+}]);
