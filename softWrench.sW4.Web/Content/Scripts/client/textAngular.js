85f2bf3508347b54e0fcf3ff2642b8094cead59c
ckcheng83 <ccheng@controltechnologysolutions.com>
2015-02-08 10:01:00 -0700

    CustomToolbar
    Remove unwanted toolitem.   (Part of GRICC's request...)

---
+++ softWrench.sW4.Web/Content/Scripts/client/textAngular.js
@@ -0,0 +1,15 @@
+﻿//var app = angular.module('sw_layout');
+
+//app.config(['$provide', function($provide) {
+//    $provide.decorator('taOptions', ['taRegisterTool', '$modal', '$delegate',
+//      function(taRegisterTool, $modal, taOptions) {
+//          // $delegate is the taOptions we are decorating
+//          // here we override the default toolbars specified in taOptions.
+//          taOptions.toolbar = [
+//                ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
+//                ['bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo', 'clear'],
+//                ['justifyLeft', 'justifyCenter', 'justifyRight'],
+//                ['html']
+//          ];
+//      }]);
+//}]);
