5ee0d186b32e010e5f2af27164b289d4c5d9fb39
rrpranav <rraja@controltechnologysolutions.com>
2015-02-10 14:48:24 -0700

    Change the parameters.....

--- softWrench.sW4/Metadata/Applications/DataSet/BaseIncidentDataSet.cs
+++ softWrench.sW4/Metadata/Applications/DataSet/BaseIncidentDataSet.cs
@@ -53,7 +53,7 @@ namespace softWrench.sW4.Metadata.Applications.DataSet{
                                                                     and objectname= '{2}')",
                                         parameters.OriginalEntity.Attributes["orgid"],
                                         parameters.OriginalEntity.Attributes["siteid"],
-                                        parameters.OriginalEntity.Attributes["class"] == "" ? parameters.OriginalEntity.Attributes["class"] : "INCIDENT");
+                                        "INCIDENT");
 
             var result = MaxDAO.FindByNativeQuery(query, null);
             var list = new List<AssociationOption>();
