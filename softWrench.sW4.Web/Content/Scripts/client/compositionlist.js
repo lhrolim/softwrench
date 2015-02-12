e3e25e790f2bc32276c22e2d97984375ae780c8c
ckcheng83 <ccheng@controltechnologysolutions.com>
2015-02-12 14:51:13 -0700

    SWWEB-958
    Updated a few things to address the expand all and collapse all button.
    
    Also, include some fix for attachment error.  Description field should
    be optional.

--- softWrench.sW4.Web/Content/Scripts/client/compositionlist.js
+++ softWrench.sW4.Web/Content/Scripts/client/compositionlist.js
@@ -504,11 +504,11 @@ app.directive('compositionList', function (contextService, formatService) {
             /*API Methods*/
             this.showExpansionCommands = function () {
                 // if schema is not present, then it should default back normal expansion commands
-                if ($scope.schema != null) {
+                if ($scope.compositionlistschema.properties != null) {
                     //this is fix for GRIC-98. Don't remove it
-                    var isExpansible = $scope.schema.properties.expansible;
-                    if (isExpansible != undefined && !isExpansible) {
-                        return isExpansible;
+                    var isExpansible = $scope.compositionlistschema.properties.expansible;
+                    if (isExpansible != undefined && isExpansible == "false") {
+                        return false;
                     }
                 }
 
