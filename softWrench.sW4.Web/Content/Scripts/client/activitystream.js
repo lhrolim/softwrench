c11431a161f551ac523b8703de826fa0c0c7c5cc
Scott A. Matthews <scott@designsbysm.com>
2015-02-18 11:59:03 -0700

    progress

---
+++ softWrench.sW4.Web/Content/Scripts/client/activitystream.js
@@ -0,0 +1,29 @@
+﻿function ActivityStream($scope, $http, $log) {
+
+    var log = $log.getInstance('sw4.activityStream');
+    //xszlog.debug('ActivityStream Controller');
+
+    var controllerToUse = "Notification";
+    var actionToUse = "GetNotifications";
+
+    var parameters = {};
+    parameters.role = 'allRole';
+
+    var rawUrl = url("/api/generic/" + controllerToUse + "/" + actionToUse + "?" + $.param(parameters));
+    $http.get(rawUrl).success(
+        function (data) {
+            //var test = data;
+            //$rootScope.$broadcast("sw_redirectactionsuccess", data);
+
+            $scope.activities = data;
+            log.debug($scope.activities);
+        }).error(
+        function (data) {
+            var errordata = {
+                errorMessage: "error opening action {0} of controller {1} ".format(actionToUse, controllerToUse),
+                errorStack: data.message
+            }
+            $rootScope.$broadcast("sw_ajaxerror", errordata);
+        }
+    );
+}
