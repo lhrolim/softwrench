c44c96a3fb27ed54405361dd4142685b5a036a17
Scott A. Matthews <scott@designsbysm.com>
2015-02-18 09:07:03 -0700

    progress

---
+++ softWrench.sW4.Web/Content/Scripts/client/directives/activityStream.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module('sw_layout');
+
+app.directive('activityStream', function ($log, $timeout) {
+    var log = $log.getInstance('sw4.activityStream');
+
+    return {
+        link: function (scope, element, attr) {
+            log.debug('activityStream');
+
+
+        }
+    }
+});
