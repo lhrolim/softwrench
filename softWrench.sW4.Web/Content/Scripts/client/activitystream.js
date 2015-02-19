75e2b5d3f8ed669c5d4957a04561f6986574aaf4
Scott A. Matthews <scott@designsbysm.com>
2015-02-19 11:03:29 -0700

    SWWEB-615 front-end progress

--- softWrench.sW4.Web/Content/Scripts/client/activitystream.js
+++ softWrench.sW4.Web/Content/Scripts/client/activitystream.js
@@ -1,29 +1,57 @@
 ﻿function ActivityStream($scope, $http, $log) {
 
     var log = $log.getInstance('sw4.activityStream');
-    //xszlog.debug('ActivityStream Controller');
 
-    var controllerToUse = "Notification";
-    var actionToUse = "GetNotifications";
 
-    var parameters = {};
-    parameters.role = 'allRole';
+    $scope.formatDate = function (notificationDate) {
+        //console.log('format date count');
 
-    var rawUrl = url("/api/generic/" + controllerToUse + "/" + actionToUse + "?" + $.param(parameters));
-    $http.get(rawUrl).success(
-        function (data) {
-            //var test = data;
-            //$rootScope.$broadcast("sw_redirectactionsuccess", data);
+        var currentDate = new Date();
+        var nowMils = currentDate.getTime() - (currentDate.getTimezoneOffset() * 60000);
+        var notificationMils = new Date(notificationDate).getTime();
 
-            $scope.activities = data;
-            log.debug($scope.activities);
-        }).error(
-        function (data) {
-            var errordata = {
-                errorMessage: "error opening action {0} of controller {1} ".format(actionToUse, controllerToUse),
-                errorStack: data.message
+        var differenceMils = nowMils - notificationMils;
+        var dateMessage = moment.duration(differenceMils, "milliseconds").humanize();
+
+        return 'About ' + dateMessage + ' ago.';
+    };
+
+    $scope.markAllRead = function (notificationDate) {
+        log.debug('markAllRead');
+
+        //TODO: mark all notifications read (confirmation alert?)
+    }
+
+    $scope.openLink = function (notificationDate) {
+        log.debug('openLink');
+
+        //TODO: mark current notification as read and open link
+    }
+
+    $scope.refreshStream = function () {
+        log.debug('refreshStream');
+
+        var controllerToUse = "Notification";
+        var actionToUse = "GetNotifications";
+
+        var parameters = {};
+        parameters.role = 'allRole';
+
+        var rawUrl = url("/api/generic/" + controllerToUse + "/" + actionToUse + "?" + $.param(parameters));
+        $http.get(rawUrl).success(
+            function (data) {
+                $scope.activities = data;
+                log.debug($scope.activities);
+            }).error(
+            function (data) {
+                var errordata = {
+                    errorMessage: "error opening action {0} of controller {1} ".format(actionToUse, controllerToUse),
+                    errorStack: data.message
+                }
+                $rootScope.$broadcast("sw_ajaxerror", errordata);
             }
-            $rootScope.$broadcast("sw_ajaxerror", errordata);
-        }
-    );
+        );
+    }
+
+    $scope.refreshStream();
 }
