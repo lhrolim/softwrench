9c729ad78e1ab40cfc18f239093511b15b6cf783
luiz <luizhenrique.rolim@gmail.com>
2015-02-19 11:22:07 -0200

    removing unused imports

--- softWrench.sW4/Data/Persistence/Dataset/Commons/Maximo/AttachmentDTO.cs
+++ softWrench.sW4/Data/Persistence/Dataset/Commons/Maximo/AttachmentDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace softWrench.sW4.Data.Persistence.Dataset.Commons.Maximo
+﻿namespace softWrench.sW4.Data.Persistence.Dataset.Commons.Maximo
 {
     public sealed class AttachmentDTO {
 
