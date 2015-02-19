1516b5e3658498ec7d56a220ef4d9cad95a1570e
jmvogt <jvogt@controltechnologysolutions.com>
2015-02-19 12:23:11 -0700

    Notification Build Fix

--- softWrench.sW4/Metadata/Parsing/XmlApplicationMetadataParser.cs
+++ softWrench.sW4/Metadata/Parsing/XmlApplicationMetadataParser.cs
@@ -489,10 +489,10 @@ namespace softWrench.sW4.Metadata.Parsing {
             return commandsSchemaEl.Elements().Select(XmlCommandBarMetadataParser.GetCommandDisplayable).ToList();
         }
 
-        private ApplicationNotificationSchema ParseNotificationSchema(XElement xElement) {
-            //var commandsSchemaEl = xElement.Elements().FirstOrDefault(f => f.IsNamed(XmlCommandSchema.CommandToolBarElements));
-            //return new ApplicationCommandSchema(XmlCommandBarMetadataParser.DoParse(commandsSchemaEl), _commandBars);
-        }
+        /*private ApplicationNotificationSchema ParseNotificationSchema(XElement xElement) {
+            var commandsSchemaEl = xElement.Elements().FirstOrDefault(f => f.IsNamed(XmlCommandSchema.CommandToolBarElements));
+            return new ApplicationCommandSchema(XmlCommandBarMetadataParser.DoParse(commandsSchemaEl), _commandBars);
+        }*/
 
         /// <summary>
         ///     Deseriliazes the specified XML element to its corresponding
---
+++ softwrench.sw4.Shared2/Metadata/Applications/Notification/ApplicationNotificationSchema.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace softwrench.sw4.Shared2.Metadata.Applications.Notification
+{
+    class ApplicationNotificationSchema
+    {
+    }
+}
