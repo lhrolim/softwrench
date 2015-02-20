99ff80493dd863b00548ec24e2f91a59abe1bfbf
luiz <luizhenrique.rolim@gmail.com>
2015-02-20 13:37:07 -0200

    HAP-894

--- softWrench.sW4.Web/Content/Scripts/client/directives/fileread.js
+++ softWrench.sW4.Web/Content/Scripts/client/directives/fileread.js
@@ -12,7 +12,7 @@
                 var extensionIdx = this.value.lastIndexOf(".");
                 var extension = this.value.substring(extensionIdx+1);
                 if ($.inArray(extension, validFileTypes) == -1) {
-                    alertService.alert("Invalid file. Please choose another one.");
+                    alert("Invalid file. Please choose another one.");
                     return;
                 }
 
