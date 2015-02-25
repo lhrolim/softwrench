4f8cdd358aa203b9c7af84fa2852a49ebd2be9c3
ckcheng83 <ccheng@controltechnologysolutions.com>
2015-02-25 08:23:56 -0700

    SWWEB-952
    Reapplied the latest fix on SWWEB-952

---
+++ softwrench.sw4.webcommons/web_content/webcommons/scripts/softwrench/util/responsive.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    $(window).resize(function () {
+        //if the header is fixed to the top of the page, set the location of the content, context menu, grid header and filter bar
+        if ($('.site-header').css('position') == 'fixed') {
+            var headerHeight = $('.site-header').height();
+            var messageHeight = $('messagesection .alerts').height();
+            var paginationHeight = $('.affix-pagination').height();
+            var theaderHeight = $('.listgrid-thead').height();
+            $('.content').css('margin-top', headerHeight + messageHeight);
+            $('messagesection .alerts').css('margin-top', 0 - messageHeight);
+            $('.affix-pagination').css('top', headerHeight + messageHeight);
+            //move fixed listgrid header up in IE9
+            var adjustment = 0;
+            if (isIe9()) {
+                adjustment = 135;
+            }
+            $('.listgrid-thead').css('top', headerHeight + messageHeight + paginationHeight - adjustment);
+            $('.listgrid-table').css('margin-top', paginationHeight + theaderHeight - 1);
+        }
+            //reset the lcoation of the content, context menu, grid header and filter bar
+        else {
+            $('.content').css('margin-top', 'auto');
+            $('messagesection .alerts').css('margin-top', 'auto');
+            $('.affix-pagination').css('top', 'auto');
+            $('.listgrid-thead').css('top', 'auto');
+            $('.listgrid-table').css('margin-top', 'auto');
+        }
+    });
+});
