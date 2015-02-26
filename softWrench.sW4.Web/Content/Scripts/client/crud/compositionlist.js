bedbcdc9016532455c5a301864c9ff81396c2753
ckcheng83 <ccheng@controltechnologysolutions.com>
2015-02-26 15:09:56 -0700

    SWWEB-1015
    Update Worklog to make it not required in the OTB template.  However, it
    is required in manchester.  Also, needed to convert all numeric value to
    string for the combo list to work.

--- softWrench.sW4.Web/Content/Scripts/client/crud/compositionlist.js
+++ softWrench.sW4.Web/Content/Scripts/client/crud/compositionlist.js
@@ -330,7 +330,8 @@ app.directive('compositionList', function (contextService, formatService) {
                     $scope.detailData[id] = {};
                     $scope.detailData[id].expanded = false;
                 }
-                $scope.detailData[id].data = item;
+                $scope.detailData[id].data = formatService.doContentStringConversion(item);
+                //$scope.detailData[id].data = item;
                 var newState = forcedState != undefined ? forcedState : !$scope.detailData[id].expanded;
                 $scope.detailData[id].expanded = newState;
 
