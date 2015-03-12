e71de117ba945cc0a7f5372f3d899dd1a22c6318
Ken <kpederson@controltechnologysolutions.com>
2015-03-12 14:23:16 -0700

    undo datetime changes

--- softWrench.sW4.Web/Formatting/JsonDateTimeConverter.cs
+++ softWrench.sW4.Web/Formatting/JsonDateTimeConverter.cs
@@ -13,13 +13,14 @@ namespace softWrench.sW4.Web.Formatting {
                 var user = SecurityFacade.CurrentUser();
                 // Chrome converts times without offsets as if they are UTC. Instead of sending the time 
                 // without an offset, send the time with the users current off set so no conversion occurs.
-                var userOffsetVal = 0;
-                if (user.TimezoneOffset != null)
-                {
-                    userOffsetVal = user.TimezoneOffset.Value;
-                }
-                var userOffset = TimeSpan.FromMinutes(userOffsetVal * -1);
-                datetime = new DateTimeOffset(((DateTime)value).FromMaximoToUser(user), userOffset).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFFK");
+                //var userOffsetVal = 0;
+                //if (user.TimezoneOffset != null)
+                //{
+                //    userOffsetVal = user.TimezoneOffset.Value;
+                //}
+                //var userOffset = TimeSpan.FromMinutes(userOffsetVal * -1);
+                //datetime = new DateTimeOffset(((DateTime)value).FromMaximoToUser(user), userOffset).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFFK");
+                datetime = ((DateTime)value).FromMaximoToUser(user).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFFK");
             }
             writer.WriteValue(datetime);
         }
