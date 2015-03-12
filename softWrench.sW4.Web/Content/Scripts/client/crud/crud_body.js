10d8321d2fa29d073485b98f21c776acb6c0c57e
rrpranav <rraja@controltechnologysolutions.com>
2015-03-12 16:57:19 -0700

    Revert "Merge pull request #422 from controltechnologysolutions/bug/KSW-106"
    This reverts commit cbe7bf8ff8beff8db9f7926cfa937bb60648c6ae, reversing
    changes made to f5dd5d87ee69c85e7991bb2508d612fa37a9539b.

--- softWrench.sW4.Web/Content/Scripts/client/crud/crud_body.js
+++ softWrench.sW4.Web/Content/Scripts/client/crud/crud_body.js
@@ -413,12 +413,6 @@ app.directive('crudBody', function (contextService) {
                 var urlToUse = url("/api/data/" + applicationName + "/?" + $.param(submissionParameters));
                 var command = id == null ? $http.post : $http.put;
 
-                if ("true"==sessionStorage.logJSON) {
-                    $log.info(jsonString);
-                }
-
-                $log.getInstance("crud_body#submit").debug(jsonString);
-
                 command(urlToUse, jsonString)
                     .success(function (data) {
                         //datamap should always be updated
