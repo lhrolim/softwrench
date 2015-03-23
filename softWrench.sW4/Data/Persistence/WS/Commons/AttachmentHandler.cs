8934a3d845adb3e9ecf2caf6104f94904b43d6c3
ckcheng83 <ccheng@controltechnologysolutions.com>
2015-03-23 15:09:39 -0700

    KSW-163 Initial Work

--- softWrench.sW4/Data/Persistence/WS/Commons/AttachmentHandler.cs
+++ softWrench.sW4/Data/Persistence/WS/Commons/AttachmentHandler.cs
@@ -202,6 +202,7 @@ namespace softWrench.sW4.Data.Persistence.WS.Commons {
             var fileName = (string)file.GetAttribute("document");
             var docinfoURL = (string)file.GetAttribute("urlname");
             var finalURL = GetFileUrl(docinfoURL);
+
             if (finalURL == null) {
                 return null;
             }
@@ -253,6 +254,8 @@ namespace softWrench.sW4.Data.Persistence.WS.Commons {
             if (_baseMaximoURL == null) {
                 BuildMaximoURL();
             }
+
+
             if (docInfoURL.StartsWith("\\")) {
                 docInfoURL = "C:" + docInfoURL;
             }
