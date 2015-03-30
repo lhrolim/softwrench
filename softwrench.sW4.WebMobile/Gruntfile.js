5eb0ff1026290fa3f7c523d1b7885de1b97b7d94
lhrolim <luizhenrique.rolim@gmail.com>
2015-03-30 16:00:10 -0300

    adding cordova dep

--- softwrench.sW4.WebMobile/Gruntfile.js
+++ softwrench.sW4.WebMobile/Gruntfile.js
@@ -5,89 +5,78 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     
      clean: {
-	    folder: "src/main/webapp/resources/styles/vendor/",
-	    folder2: "src/main/webapp/resources/scripts/vendor/"
+	    folder: "Content/Vendor/scripts/",
+	    folder2: "Content/Vendor/styles/"
     },
     
     
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/main/webapp/scripts/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
-  
   	bowercopy:{
   		
   		options:{
-  			 destPrefix: 'src/main/webapp/resources/scripts/vendor'
+  			 destPrefix: 'Content/Vendor/scripts'
   		},
   		
   		
   		 jquery: {
              files: {
-                 'jquery.js': 'jquery/dist/jquery.js',
-                 'spin.js': 'spin.js/spin.js'
+                 'jquery.min.js': 'jquery/dist/jquery.min.js',
+//                 'jquery.js': 'jquery/dist/jquery.js',
+//                 'jquery.min.map': 'jquery/dist/jquery.min.map',
              }
-         },
+  		 },
+
+  		 angular: {
+  		     files: {
+  		         'angular.min.js': 'angular/angular.min.js',
+  		     }
+  		 },
+
+  		 ionic: {
+  		     files: {
+  		         'ionin.min.js': 'ionic/release/js/ionic.bundle.min.js',
+  		     }
+  		 },
+
+  		 ionic_css: {
+
+  		     options: {
+  		         destPrefix: 'Content/Vendor/styles'
+  		     },
+
+  		     files: {
+  		         'ionin.min.css': 'ionic/release/css/ionic.min.css',
+  		     }
+  		 },
          
-         angular: {
-             files: {
-                 'angular.js': 'angular/angular.js',
-                 'angular-sanitize.js': 'angular-sanitize/angular-sanitize.min.js',
-                 'angularsmarttable.js': 'angular-smart-table/dist/smart-table.js',
-             }
-         },
-         
-       
-         
-         bootstrap: {
-             files: {
-                 'bootstrap.js': 'bootstrap/dist/js/bootstrap.js',
-             }
-         },
-         
-         bootbox: {
-             files: {
-                 'bootbox.js': 'bootbox/bootbox.js',
-             }
-         },
-         
-         bootstrap_css: {
-        	 options:{
-        		 destPrefix: 'src/main/webapp/resources/styles/vendor'
-        	 },
-        	 
-             files: {
-                 'bootstrap.min.css': 'bootstrap/dist/css/bootstrap.min.css',
-                 'bootstrap-theme.min.css': 'bootstrap/dist/css/bootstrap-theme.min.css',
-             }
-         },
-         
-         fontawesome: {
-        	 options:{
-        		 destPrefix: 'src/main/webapp/resources/styles/vendor'
-        	 },
-        	 
-             files: {
-                 'fontawesome.min.css': 'components-font-awesome/css/font-awesome.min.css'
-             }
-         },
-         
-         folders: {
-        	 
-        	 options:{
-        		 destPrefix: 'src/main/webapp/resources/'
-        	 },
-        	 
-             files: {
-                 // Note: when copying folders, the destination (key) will be used as the location for the folder 
-                 'fonts': 'components-font-awesome/fonts/',
-             }
-         },
+//         angular: {
+//             files: {
+//                 'angular.js': 'angular/angular.min.js',
+//             }
+//         },
+//         
+//         
+//         fontawesome: {
+//        	 options:{
+//        		 destPrefix: 'Content/Vendor/styles/'
+//        	 },
+//        	 
+//             files: {
+//                 'fontawesome.min.css': 'components-font-awesome/css/font-awesome.min.css'
+//             }
+//         },
+
+//         folders: {
+//
+//             options: {
+//                 destPrefix: 'src/main/webapp/resources/'
+//             },
+//
+//             files: {
+//                 // Note: when copying folders, the destination (key) will be used as the location for the folder 
+//                 'fonts': 'components-font-awesome/fonts/',
+//             }
+//         },
+      
          
   	}
   
