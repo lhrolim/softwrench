b1c4bd3de04f4fe2933be87ae9377dd8a244fbb2
ckcheng83 <ccheng@controltechnologysolutions.com>
2015-04-01 14:04:39 -0700

    COMSW-74 SQL Script

--- softwrench.sw4.manchester/classes/com/cts/manchester/migration/Migration20150331Comsw74.cs
+++ softwrench.sw4.manchester/classes/com/cts/manchester/migration/Migration20150331Comsw74.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using FluentMigrator;
 using FluentMigrator.Runner.Extensions;
+using System.Configuration;
 
 
 namespace softwrench.sw4.manchester.classes.com.cts.manchester.migration {
@@ -8,163 +9,14 @@ namespace softwrench.sw4.manchester.classes.com.cts.manchester.migration {
     public class Migration20150331Comsw74 : FluentMigrator.Migration
     {
         public override void Up() {
-            // Insert swdashboard into user table
-            Insert.IntoTable("SW_USER2")
-                .WithIdentityInsert()
-                .Row(
-                    new {
-                        id = 3,
-                        username = "swdashboard",
-                        firstname = "dashboard",
-                        lastname = "dashboard",
-                        isactive = 1
-                    });
+            var key = ConfigurationManager.AppSettings["clientkey"];
 
-            // Insert user profile laborers
-            Insert.IntoTable("SW_USERPROFILE")
-                .WithIdentityInsert()
-                .Row(
-                    new {
-                        id = 16,
-                        name = "Laborers",
-                        deletable = "0",
-                        description = "Laborers"
-                    });
-
-            // Insert a new dashboard
-            Insert.IntoTable("DASH_DASHBOARD")
-                .WithIdentityInsert()
-                .Row(
-                    new {
-                        id = 1,
-                        layout = "1,1",
-                        title = "SRs and WOs",
-                        createdby = 3,
-                        creationdate = DateTime.Now,
-                        updatedate = DateTime.Now
-                    });
-
-            // Insert active SR and WO widgets
-            Insert.IntoTable("DASH_BASEPANEL")
-                .WithIdentityInsert()
-                .Row(
-                    new {
-                        id = 1,
-                        alias_ = "ActiveSRs",
-                        title = "Active Service Requests",
-                        createdby = 3,
-                        creationdate = DateTime.Now,
-                        updatedate = DateTime.Now
-                    })
-                .Row(
-                    new {
-                        id = 2,
-                        alias_ = "ActiveWOs",
-                        title = "Active Work Orders",
-                        createdby = 3,
-                        creationdate = DateTime.Now,
-                        updatedate = DateTime.Now
-                    });
-
-            Insert.IntoTable("DASH_GRIDPANEL")
-                .Row(
-                    new {
-                        gpid = 1,
-                        application = "servicerequest",
-                        schemaref = "list",
-                        defaultsortfield = "ticketid"
-                    })
-                .Row(
-                    new {
-                        gpid = 2,
-                        application = "workorder",
-                        schemaref = "list",
-                        defaultsortfield = "wonum"
-                    });
-
-            // Define layout
-            Insert.IntoTable("DASH_DASHBOARDREL")
-                .Row(
-                    new {
-                        position = 0,
-                        panel_id = 2,
-                        dashboard_id = 1
-                    })
-                .Row(
-                    new {
-                        position = 0,
-                        panel_id = 1,
-                        dashboard_id = 1
-                    });
-
-            // Define where clause
-            Insert.IntoTable("CONF_CONDITION")
-                .WithIdentityInsert()
-                .Row(
-                    new {
-                        id = 2,
-                        alias_ = "active_sr_dashboard",
-                        fullkey = @"/_whereclauses/servicerequest/",
-                        global = 0
-                    })
-                .Row(
-                    new {
-                        id = 3,
-                        alias_ = "active_wo_dashboard",
-                        fullkey = @"/_whereclauses/workorder/",
-                        global = 0
-                    });
-
-            Insert.IntoTable("CONF_WCCONDITION")
-                .Row(
-                    new {
-                        wcwcid = 2,
-                        metadataid = "ActiveSRs",
-                    })
-                .Row(
-                    new {
-                        wcwcid = 3,
-                        schema_ = "list",
-                        metadataid = "ActiveWOs"
-                    });
-
-            Insert.IntoTable("CONF_PROPERTYDEFINITION")
-                .Row(
-                    new {
-                        fullkey = @"/_whereclauses/servicerequest/whereclause",
-                        key_ = "whereclause",
-                        defaultvalue = "",
-                        datatype = "String",
-                        renderer = "whereclause",
-                        visible = 1,
-                        contextualized = 1,
-                        alias_ = ""
-                    })
-                .Row(
-                    new {
-                        fullkey = @"/_whereclauses/workorder/whereclause",
-                        key_ = "whereclause",
-                        defaultvalue = "",
-                        datatype = "String",
-                        renderer = "whereclause",
-                        visible = 1,
-                        contextualized = 1,
-                        alias_ = ""
-                    }); 
-
-            Insert.IntoTable("CONF_PROPERTYVALUE")
-                .Row(
-                    new {
-                      value = @"((status in ('INPROG','SR WO COMP','WO CREATED' ,'NEW','PENDING','QUEUED'))) and ((owner = @username  or upper(reportedby) = @username ) or ((ownergroup is not null) and ownergroup  in (select persongroup from persongroupteam where respparty = @username ))) and siteid = 'DPW'",  
-                      definition_id = @"/_whereclauses/servicerequest/whereclause",
-                      condition_id = 2
-                    })
-                .Row(
-                    new {
-                      value = @"((workorder.status in ('APPR','BASE','FIELD WORK COMP','NEW','QUEUED','INPRG','INV','POSTPONED','RFI','SAW','WAPPR','WMATL','WPCOND','WSCH')) and ((owner = 'swadmin' or upper(reportedby) = 'swadmin') or ((ownergroup is not null) and ownergroup  in (select persongroup from persongroupteam where respparty = 'swadmin'))) and historyflag = 0 and istask = 0 and workorder.siteid = 'DPW')",  
-                      definition_id = @"/_whereclauses/workorder/whereclause",
-                      condition_id = 3
-                    });
+            if (key.Equals("manchester")) {
+                // Derive path for the SQL script
+                var filepath = String.Format("{0}Content\\Customers\\manchester\\scripts\\{1}", AppDomain.CurrentDomain.BaseDirectory, "com_dataimport_com74.sql");
+                // Execute script using FluentMigration
+                Execute.Script(filepath);
+            }
         }
 
         public override void Down() {
---
+++ softwrench.sw4.manchester/web_content/manchester/scripts/com_dataimport_com74.sql
@@ -0,0 +1,112 @@
+﻿DECLARE @ORGID varchar
+DECLARE @SITEID varchar
+
+SET @ORGID  = 'DPWORG'
+SET @SITEID = 'DPW'
+
+-- Create user profile 'laborers'
+IF NOT EXISTS (SELECT 1 FROM dbo.SW_USERPROFILE WHERE NAME = 'Laborers')
+BEGIN
+INSERT INTO [dbo].[SW_USERPROFILE] ([name], [deletable], [description]) VALUES (N'Laborers', 0, N'Laborers')
+END
+
+-- Create user 'swadmin'
+IF NOT EXISTS (SELECT 1 FROM dbo.SW_USER2 WHERE USERNAME = 'swadmin')
+BEGIN
+INSERT INTO [dbo].[SW_USER2] (username, password, firstname, lastname, isactive, orgid, siteid, phone, language)
+VALUES ('swadmin', '16193128416889160822141461911951451401273111117118200', 'admin', 'admin', 1, @ORGID, @SITEID, '1-800-433-7300', 'EN')
+END
+
+DECLARE @USERID int
+SELECT  @USERID = id FROM dbo.SW_USER2 WHERE USERNAME = 'swadmin'
+
+-- Define Dashboard if not exist
+IF NOT EXISTS (SELECT 1 FROM dbo.DASH_DASHBOARD WHERE TITLE = 'SRs and WOs')
+BEGIN
+
+-- Assign all user with default orgid and siteid
+UPDATE SW_USER2
+SET ORGID = @ORGID, SITEID = @SITEID
+
+DECLARE @DASHBOARDID int
+DECLARE @WO int
+DECLARE @SR int
+
+INSERT INTO [dbo].[DASH_DASHBOARD] (layout, title, createdby, creationdate, updatedate)
+VALUES ('1,1', 'SRs and WOs', @USERID, GETDATE(), GETDATE());
+
+SELECT @DASHBOARDID = @@IDENTITY;
+
+INSERT INTO [dbo].[DASH_BASEPANEL] (alias_, title, createdby, creationdate, updatedate)
+VALUES ('ActiveSRs', 'Active Service Requests', @USERID, GETDATE(), GETDATE())
+
+SELECT @SR = @@IDENTITY;
+
+INSERT INTO [dbo].[DASH_BASEPANEL] (alias_, title, createdby, creationdate, updatedate)
+VALUES ('ActiveWOs', 'Active Work Orders', @USERID, GETDATE(), GETDATE())
+
+SELECT @WO = @@IDENTITY;
+
+INSERT INTO [dbo].[DASH_GRIDPANEL] (gpid, application, schemaref, defaultsortfield)
+VALUES (@SR, 'servicerequest', 'list', 'ticketid')
+
+INSERT INTO [dbo].[DASH_GRIDPANEL] (gpid, application, schemaref, defaultsortfield)
+VALUES (@WO, 'workorder', 'list', 'wonum')
+
+INSERT INTO [dbo].[DASH_DASHBOARDREL] (position, panel_id, dashboard_id)
+VALUES(0, @WO, @DASHBOARDID)
+
+INSERT INTO [dbo].[DASH_DASHBOARDREL] (position, panel_id, dashboard_id)
+VALUES(1, @SR, @DASHBOARDID)
+
+END
+
+-- Define Whereclause if not exist
+IF NOT EXISTS (SELECT 1 FROM [dbo].[CONF_CONDITION] WHERE ALIAS_ = 'active_sr_dashboard')
+BEGIN
+
+DECLARE @CONDITIONID int
+
+INSERT INTO [dbo].[CONF_CONDITION] (alias_, fullkey, global)
+VALUES ('active_sr_dashboard', '/_whereclauses/servicerequest/', 0)
+
+SELECT @CONDITIONID = @@IDENTITY;
+
+INSERT INTO [dbo].[CONF_WCCONDITION] (wcwcid, metadataid)
+VALUES (@CONDITIONID, 'ActiveSRs')
+
+	IF NOT EXISTS (SELECT 1 FROM [dbo].[CONF_PROPERTYDEFINITION] WHERE FULLKEY LIKE '/_whereclauses/servicerequest/whereclause')
+	BEGIN
+		INSERT INTO [dbo].[CONF_PROPERTYDEFINITION] (fullkey, key_, defaultvalue, datatype, renderer, visible, contextualized, alias_)
+		VALUES ('/_whereclauses/servicerequest/whereclause', 'whereclause', '', 'String', 'whereclause', 1, 1, '')
+	END
+
+INSERT INTO [dbo].[CONF_PROPERTYVALUE] (value, definition_id, condition_id)
+VALUES ('((status in (''INPROG'',''SR WO COMP'',''WO CREATED'' ,''NEW'',''PENDING'',''QUEUED''))) and ((owner = @username  or upper(reportedby) = @username ) or ((ownergroup is not null) and ownergroup  in (select persongroup from persongroupteam where respparty = @username ))) and siteid = ''DPW''', '/_whereclauses/servicerequest/whereclause', @CONDITIONID)
+
+END 
+GO
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[CONF_CONDITION] WHERE ALIAS_ = 'active_wo_dashboard')
+BEGIN
+
+DECLARE @CONDITIONID int
+
+INSERT INTO [dbo].[CONF_CONDITION] (alias_, fullkey, global)
+VALUES ('active_wo_dashboard', '/_whereclauses/workorder/', 0)
+
+SELECT @CONDITIONID = @@IDENTITY;
+
+INSERT INTO [dbo].[CONF_WCCONDITION] (wcwcid, metadataid)
+VALUES (@CONDITIONID, 'ActiveWOs')
+
+	IF NOT EXISTS (SELECT 1 FROM [dbo].[CONF_PROPERTYDEFINITION] WHERE FULLKEY LIKE '/_whereclauses/workorder/whereclause')
+	BEGIN
+		INSERT INTO [dbo].[CONF_PROPERTYDEFINITION] (fullkey, key_, defaultvalue, datatype, renderer, visible, contextualized, alias_)
+		VALUES ('/_whereclauses/workorder/whereclause', 'whereclause', '', 'String', 'whereclause', 1, 1, '')
+	END
+
+INSERT INTO [dbo].[CONF_PROPERTYVALUE] (value, definition_id, condition_id)
+VALUES ('((workorder.status in (''APPR'',''BASE'',''FIELD WORK COMP'',''NEW'',''QUEUED'',''INPRG'',''INV'',''POSTPONED'',''RFI'',''SAW'',''WAPPR'',''WMATL'',''WPCOND'',''WSCH'')) and ((owner = ''swadmin'' or upper(reportedby) = ''swadmin'') or ((ownergroup is not null) and ownergroup  in (select persongroup from persongroupteam where respparty = ''swadmin''))) and historyflag = 0 and istask = 0 and workorder.siteid = ''DPW'')', '/_whereclauses/workorder/whereclause', @CONDITIONID)
+END 
+GO
