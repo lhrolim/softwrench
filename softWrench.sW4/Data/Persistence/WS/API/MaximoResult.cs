ae9b17006785937322374c4511802537ad16ff24
ckcheng83 <ccheng@controltechnologysolutions.com>
2015-04-04 14:10:03 -0700

    YGSI-11
    Updated the code to add back Id.  There's a distinct difference between
    Id and UserId.

--- softWrench.sW4/Data/Persistence/WS/API/MaximoResult.cs
+++ softWrench.sW4/Data/Persistence/WS/API/MaximoResult.cs
@@ -1,10 +1,13 @@
 ﻿namespace softWrench.sW4.Data.Persistence.WS.API {
  
     public class TargetResult {
+        // Unique Id use to identify the record; it might be different than the userid
+        public string Id { get; set; }
         public string UserId { get; set; }
         public object ResultObject { get; set; }
 
-        public TargetResult(string userId, object resultObject) {
+        public TargetResult(string id, string userId, object resultObject) {
+            Id = id;
             UserId = userId;
             ResultObject = resultObject;
         }
