4748d7080b99fd93137b34505cf69a34bc2c4975
jmvogt <jvogt@controltechnologysolutions.com>
2015-04-10 14:45:44 -0700

    Partial PAE Metadata Commit

---
+++ softWrench.sW4/Metadata/Applications/DataSet/BeringKeyInventoryIssueDataSet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Windows.Controls;
+using DocumentFormat.OpenXml.Spreadsheet;
+using NHibernate.Hql.Ast.ANTLR;
+using NHibernate.Linq;
+using softWrench.sW4.Data;
+using softWrench.sW4.Data.API;
+using softWrench.sW4.Data.Entities;
+using softWrench.sW4.Data.Pagination;
+using softWrench.sW4.Data.Persistence;
+using softWrench.sW4.Data.Persistence.SWDB;
+using softWrench.sW4.Data.Relationship.Composition;
+using softWrench.sW4.Data.Search;
+using softWrench.sW4.Data.Persistence.Dataset.Commons;
+using softWrench.sW4.Metadata.Applications.DataSet.Filter;
+using softWrench.sW4.Metadata.Security;
+using softWrench.sW4.Security.Services;
+using softwrench.sW4.Shared2.Data;
+using softwrench.sw4.Shared2.Data.Association;
+using cts.commons.simpleinjector;
+
+namespace softWrench.sW4.Metadata.Applications.DataSet {
+    class BeringKeyInventoryIssueDataSet : BaseInventoryIssueDataSet {
+
+        public override string ApplicationName() {
+            return "keyinvissue";
+        }
+
+        public override string ClientFilter() {
+            return "bering_inv";
+        }
+    }
+}
