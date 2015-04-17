9f260419d89a66854b3e1c0e2b71077bf8fff1c3
jmvogt <jvogt@controltechnologysolutions.com>
2015-04-17 10:07:22 -0700

    Initial DB Migration for Audit

---
+++ softWrench.sW4.Web/DB_Migration/4.0/20150417_PAE_001.cs
@@ -0,0 +1,40 @@
+﻿using FluentMigrator;
+using softWrench.sW4.Web.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace softWrench.sW4.Web.DB_Migration._4._0
+{
+    [Migration(201504170900)]
+    public class Migration201500417PAE001 : FluentMigrator.Migration
+    {
+        public override void Up()
+        {
+            Create.Table("SW_AUDITENTRY")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Action").AsString().NotNullable()
+                .WithColumn("RefEntity").AsString().NotNullable()
+                .WithColumn("RefId").AsInt64().NotNullable()
+                .WithColumn("Data").AsString().NotNullable()
+                .WithColumn("CreatedBy").AsString().NotNullable()
+                .WithColumn("CreatedDate").AsDateTime().NotNullable();
+
+            //Create.Table("SW_AUDITEVENT")
+            //    .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+            //    .WithColumn("Action").AsString().NotNullable()
+            //    .WithColumn("RefEntity").AsString().NotNullable()
+            //    .WithColumn("RefId").AsInt64().NotNullable()
+            //    .WithColumn("CreatedBy").AsString().NotNullable()
+            //    .WithColumn("CreatedDate").AsDateTime().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("SW_AUDITENTRY");
+            //Delete.Table("SW_AUDITEVENT");
+        }
+    }
+}
