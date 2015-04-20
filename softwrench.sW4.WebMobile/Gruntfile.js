c985437e2d13121ca1b1b53f7e003470c3dd4870
lhrolim <luizhenrique.rolim@gmail.com>
2015-04-20 10:51:35 -0700

    fix build

--- softwrench.sW4.WebMobile/Gruntfile.js
+++ softwrench.sW4.WebMobile/Gruntfile.js
@@ -20,65 +20,35 @@ module.exports = function (grunt) {
             },
 
 
-            jquery: {
+            dev: {
                 files: {
-                    'jquery.min.js': 'jquery/dist/jquery.min.js',
-                    //                 'jquery.js': 'jquery/dist/jquery.js',
-                    //                 'jquery.min.map': 'jquery/dist/jquery.min.map',
-                }
-            },
+                    'angular.js': 'angular/angular.min.js',
+                    'angular-sanitize.js': 'angular-sanitize/angular-sanitize.min.js',
+                    'angular-ui-router.js': 'angular-ui-router/release/angular-ui-router.min.js',
+                    'angular-animate.js': 'angular-animate/angular-animate.min.js',
 
-            angular: {
-                files: {
-                    'angular.min.js': 'angular/angular.min.js',
-                    'angular.min.js.map': 'angular/angular.min.js.map',
-                    'angular-sanitize.min.js': 'angular-sanitize/angular-sanitize.min.js',
-                    'angular-sanitize.min.js.map': 'angular-sanitize/angular-sanitize.min.js.map',
-                    'angular-ui-router.min.js': 'angular-ui-router/release/angular-ui-router.js',
-                    'angular-animate.min.js': 'angular-animate/angular-animate.min.js',
-                    'angular-animate.min.js.map': 'angular-animate/angular-animate.min.js.map',
-                    'angular.js': 'angular/angular.js',
-                }
-            },
-
-            ngCordova: {
-                files: {
-                    //  		         'ng-cordova.min.js': 'ngCordova/dist/ng-cordova.min.js',
-                    'ng-cordova.min.js': 'ngCordova/dist/ng-cordova.js',
-                }
-            },
-
-
-            persistence: {
-                files: {
+                    'jquery.js': 'jquery/dist/jquery.min.js',
+                    'ng-cordova.js': 'ngCordova/dist/ng-cordova.js',
                     'persistence.js': 'persistence/lib/persistence.js',
                     'persistence.store.sql.js': 'persistence/lib/persistence.store.sql.js',
                     'persistence.store.websql.js': 'persistence/lib/persistence.store.websql.js',
                 }
             },
 
-            //ionic_css: {
+            prod: {
+                files: {
+                    'angular-sanitize.js': 'angular-sanitize/angular-sanitize.js',
+                    'angular-ui-router.js': 'angular-ui-router/release/angular-ui-router.js',
+                    'angular-animate.js': 'angular-animate/angular-animate.js',
+                    'angular.js': 'angular/angular.js',
 
-            //    options: {
-            //        destPrefix: 'Content/Vendor/styles'
-            //    },
-
-            //    files: {
-            //        'ionic.min.css': 'ionic/release/css/ionic.min.css',
-            //    }
-            //},
-
-            //ionic_fonts: {
-
-            //    options: {
-            //        destPrefix: 'Content/Vendor/fonts'
-            //    },
-
-            //    files: {
-            //        'ionicons.ttf': 'ionic/release/fonts/ionicons.ttf',
-            //        'ionicons.woff': 'ionic/release/fonts/ionicons.woff',
-            //    }
-            //},
+                    'jquery.js': 'jquery/dist/jquery.js',
+                    'ng-cordova.js': 'ngCordova/dist/ng-cordova.js',
+                    'persistence.js': 'persistence/lib/persistence.js',
+                    'persistence.store.sql.js': 'persistence/lib/persistence.store.sql.js',
+                    'persistence.store.websql.js': 'persistence/lib/persistence.store.websql.js',
+                }
+            }
 
 
     
@@ -121,16 +91,11 @@ module.exports = function (grunt) {
     
 
     // Default task(s).
-    grunt.registerTask('defaultdev', ['clean', 'bowercopy', 'tags']);
-    grunt.registerTask('default', ['clean', 'bowercopy', 'uglify']);
+    
+    grunt.registerTask('prod', ['clean', 'bowercopy:prod', 'uglify']);
 
-    grunt.registerTask('quick_dev_wrapper', ['quick_dev']);
+    grunt.registerTask('fulldev', ['clean', 'bowercopy:dev', 'tags']);
+    grunt.registerTask('quick_dev', ['bowercopy:dev']);
 
-    grunt.registerTask('quick_dev', 'Build with production options', function () {
-        //grunt.global.jssuffix = "min.js";
-        grunt.task.run('bowercopy');
-    });
-
-    grunt.registerTask('quick_dev_wrapper', ['quick_dev']);
 
 };
