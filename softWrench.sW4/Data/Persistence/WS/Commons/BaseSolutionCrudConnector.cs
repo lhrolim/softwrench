707442f8e4dbc317d5c214ba4dea82f5c2a3729c
ckcheng83 <ccheng@controltechnologysolutions.com>
2015-04-24 14:04:43 -0700

    KSW-173 Add Crud Connector

---
+++ softWrench.sW4/Data/Persistence/WS/Commons/BaseSolutionCrudConnector.cs
@@ -0,0 +1,24 @@
+﻿using softWrench.sW4.Data.Persistence.Dataset.Commons.Maximo;
+using softWrench.sW4.Data.Persistence.Operation;
+using softWrench.sW4.Data.Persistence.WS.API;
+using softWrench.sW4.Data.Persistence.WS.Internal;
+using softWrench.sW4.Metadata.Applications;
+using softWrench.sW4.Security.Services;
+using softWrench.sW4.Util;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using w = softWrench.sW4.Data.Persistence.WS.Internal.WsUtil;
+
+namespace softWrench.sW4.Data.Persistence.WS.Commons {
+    class BaseSolutionCrudConnector : CrudConnectorDecorator {
+        public override void BeforeUpdate(MaximoOperationExecutionContext maximoTemplateData)
+        {
+        }
+
+        public override void BeforeCreation(MaximoOperationExecutionContext maximoTemplateData)
+        {
+        }
+    }
+}
