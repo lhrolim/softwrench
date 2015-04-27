a8c1c351dd9fdffae56dc48427a5f90ecad788cc
ckcheng83 <ccheng@controltechnologysolutions.com>
2015-04-27 11:45:18 -0700

    KSW-173 Update Common Transaction in CRUD Connector
    Save is now working for solution.

--- softWrench.sW4/Data/Persistence/WS/Commons/BaseSolutionCrudConnector.cs
+++ softWrench.sW4/Data/Persistence/WS/Commons/BaseSolutionCrudConnector.cs
@@ -9,16 +9,23 @@ using System.Linq;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using DocumentFormat.OpenXml.Packaging;
 using w = softWrench.sW4.Data.Persistence.WS.Internal.WsUtil;
 
 namespace softWrench.sW4.Data.Persistence.WS.Commons {
     class BaseSolutionCrudConnector : CrudConnectorDecorator {
-        public override void BeforeUpdate(MaximoOperationExecutionContext maximoTemplateData)
-        {
+        public override void BeforeUpdate(MaximoOperationExecutionContext maximoTemplateData) {
+            CommonTransaction(maximoTemplateData);
         }
 
-        public override void BeforeCreation(MaximoOperationExecutionContext maximoTemplateData)
-        {
+        public override void BeforeCreation(MaximoOperationExecutionContext maximoTemplateData) {
+            CommonTransaction(maximoTemplateData);
+        }
+
+        public void CommonTransaction(MaximoOperationExecutionContext maximoTemplateData) {
+            LongDescriptionHandler.HandleLongDescription(maximoTemplateData.IntegrationObject, (CrudOperationData)maximoTemplateData.OperationData, "PROBLEMCODE_LONGDESCRIPTION", "symptom");
+            LongDescriptionHandler.HandleLongDescription(maximoTemplateData.IntegrationObject, (CrudOperationData)maximoTemplateData.OperationData, "FR1CODE_LONGDESCRIPTION", "cause");
+            LongDescriptionHandler.HandleLongDescription(maximoTemplateData.IntegrationObject, (CrudOperationData)maximoTemplateData.OperationData, "FR2CODE_LONGDESCRIPTION", "resolution");
         }
     }
 }
