38ef8a070a2d106885fbb0c08de58ea119eb98a0
lhrolim <luizhenrique.rolim@gmail.com>
2015-04-28 12:19:45 -0700

    HAP-935

--- softwrench.sw4.Hapag/Data/DataSet/HapagIncidentDataSet.cs
+++ softwrench.sw4.Hapag/Data/DataSet/HapagIncidentDataSet.cs
@@ -14,10 +14,8 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
-namespace softwrench.sw4.Hapag.Data.DataSet
-{
-    class HapagIncidentDataSet : HapagBaseApplicationDataSet
-    {
+namespace softwrench.sw4.Hapag.Data.DataSet {
+    class HapagIncidentDataSet : HapagBaseApplicationDataSet {
         private const string MISSING = "";
         private const string SITEID = "pluspcustomer";
         private const string COMMODITYGROUP = "#commoditygroup";
@@ -32,16 +30,14 @@ namespace softwrench.sw4.Hapag.Data.DataSet
 
         #region Filter functions
 
-        public SearchRequestDto FilterComponent(AssociationPreFilterFunctionParameters parameters)
-        {
+        public SearchRequestDto FilterComponent(AssociationPreFilterFunctionParameters parameters) {
             var searchDTO = parameters.BASEDto;
             var system = parameters.OriginalEntity.GetAttribute("system") as String;
             searchDTO.AppendSearchEntry("description", system + "%");
             return searchDTO;
         }
 
-        public SearchRequestDto FilterItem(AssociationPreFilterFunctionParameters parameters)
-        {
+        public SearchRequestDto FilterItem(AssociationPreFilterFunctionParameters parameters) {
             var searchDTO = parameters.BASEDto;
             var system = parameters.OriginalEntity.GetAttribute("system") as String;
             var component = parameters.OriginalEntity.GetAttribute("component") as String;
@@ -49,8 +45,7 @@ namespace softwrench.sw4.Hapag.Data.DataSet
             return searchDTO;
         }
 
-        public SearchRequestDto FilterModule(AssociationPreFilterFunctionParameters parameters)
-        {
+        public SearchRequestDto FilterModule(AssociationPreFilterFunctionParameters parameters) {
             var searchDTO = parameters.BASEDto;
             var system = parameters.OriginalEntity.GetAttribute("system") as String;
             var component = parameters.OriginalEntity.GetAttribute("component") as String;
@@ -59,79 +54,58 @@ namespace softwrench.sw4.Hapag.Data.DataSet
             return searchDTO;
         }
 
-        public IEnumerable<IAssociationOption> GetHlagUserLocations(OptionFieldProviderParameters parameters)
-        {
+        public IEnumerable<IAssociationOption> GetHlagUserLocations(OptionFieldProviderParameters parameters) {
             var options = base.GetHlagUserLocations(parameters);
             var multivaluedOptions = new List<IAssociationOption>();
             var selected = new Dictionary<string, object> { { "isSelected", true } };
 
-            foreach (var option in options)
-            {
+            foreach (var option in options) {
                 multivaluedOptions.Add(new MultiValueAssociationOption(option.Value, option.Label, selected));
             }
             return multivaluedOptions;
         }
-        public IEnumerable<IAssociationOption> GetIncidentStatus(OptionFieldProviderParameters parameters)
-        {
+        public IEnumerable<IAssociationOption> GetIncidentStatus(OptionFieldProviderParameters parameters) {
             var options = parameters.OptionField.Options;
             var multivaluedOptions = new List<IAssociationOption>();
             var selected = new Dictionary<string, object> { { "isSelected", true } };
             var selectedStatus = new string[] { "CANCELLED", "HISTEDIT", "INPROG", "NEW", "PENDING", "REJECTED", "SLAHOLD", "QUEUED" };
 
-            foreach (var option in options)
-            {
-                if (selectedStatus.Contains(option.Value))
-                {
-                    multivaluedOptions.Add(new MultiValueAssociationOption(option.Value, option.Label, selected));
-                }
-                else
-                {
-                    multivaluedOptions.Add(option);
-                }
+            foreach (var option in options) {
+
+                multivaluedOptions.Add(new MultiValueAssociationOption(option.Value, option.Label, selected));
+
             }
             return multivaluedOptions;
         }
 
-        public IEnumerable<IAssociationOption> ParseCommoditiesList(AssociationPostFilterFunctionParameters postParams)
-        {
+        public IEnumerable<IAssociationOption> ParseCommoditiesList(AssociationPostFilterFunctionParameters postParams) {
             var comparer = new OptionComparer();
             ISet<IAssociationOption> commodity = new SortedSet<IAssociationOption>();
 
-            foreach (var resultset in postParams.Options)
-            {
+            foreach (var resultset in postParams.Options) {
                 AssociationOption result = new AssociationOption(resultset.Value, resultset.Label);
                 result.Value = resultset.Value;
-                if (resultset.Label == null || resultset.Label.IndexOf('-') == -1)
-                {
+                if (resultset.Label == null || resultset.Label.IndexOf('-') == -1) {
                     result.Label = MISSING;
                     //commodity.Add(result);
-                }
-                else
-                {
+                } else {
                     string[] arrTemp = resultset.Label.Split('-');
-                    if (postParams.Association.Target == "component")
-                    {
+                    if (postParams.Association.Target == "component") {
                         result.Label = (arrTemp.Length > 2) ? arrTemp[2] : MISSING;
                         //Please Confirm Luiz
                         result.Value = result.Label;
                         commodity.Add(result);
-                    }
-                    else if (postParams.Association.Target == "system")
-                    {
+                    } else if (postParams.Association.Target == "system") {
                         result.Label = arrTemp[0] + '-' + arrTemp[1];
                         //Please Confirm Luiz
                         result.Value = result.Label;
                         commodity.Add(result);
-                    }
-                    else if (postParams.Association.Target == "module")
-                    {
+                    } else if (postParams.Association.Target == "module") {
                         result.Label = (arrTemp.Length > 4) ? ModuleFieldValue(arrTemp) : MISSING;
                         //Please Confirm Luiz
                         result.Value = result.Label;
                         commodity.Add(result);
-                    }
-                    else if (postParams.Association.Target == "item")
-                    {
+                    } else if (postParams.Association.Target == "item") {
                         result.Label = (arrTemp.Length > 3) ? arrTemp[3] : MISSING;
                         //Please Confirm Luiz
                         result.Value = result.Label;
@@ -148,11 +122,9 @@ namespace softwrench.sw4.Hapag.Data.DataSet
         /// </summary>
         /// <param name="arrTemp">The description field, with System/Component/Module/Item</param>
         /// <returns>Module Filed Value and any trailing values.</returns>
-        private static string ModuleFieldValue(string[] arrTemp)
-        {
+        private static string ModuleFieldValue(string[] arrTemp) {
             string strTemp = string.Empty;
-            for (int i = 4; i < arrTemp.Length; i++)
-            {
+            for (int i = 4; i < arrTemp.Length; i++) {
                 strTemp += arrTemp[i] + '-';
             }
             return strTemp.Substring(0, strTemp.Length - 1);
@@ -160,120 +132,87 @@ namespace softwrench.sw4.Hapag.Data.DataSet
 
         #endregion Filter functions
 
-        protected override ApplicationDetailResult GetApplicationDetail(ApplicationMetadata application, InMemoryUser user, DetailRequest request)
-        {
+        protected override ApplicationDetailResult GetApplicationDetail(ApplicationMetadata application, InMemoryUser user, DetailRequest request) {
             var dbDetail = base.GetApplicationDetail(application, user, request);
             var resultObject = dbDetail.ResultObject;
 
-            if (application.Schema.Mode == SchemaMode.output)
-            {
+            if (application.Schema.Mode == SchemaMode.output) {
                 HandleClosedDate(resultObject, application);
             }
             return dbDetail;
         }
 
-        protected override ApplicationListResult GetList(ApplicationMetadata application, PaginatedSearchRequestDto searchDto)
-        {
+        protected override ApplicationListResult GetList(ApplicationMetadata application, PaginatedSearchRequestDto searchDto) {
             var dbList = base.GetList(application, searchDto);
             var resultObject = dbList.ResultObject;
-            if ((application.Schema.SchemaId == "hardwarerepair"))
-            {
+            if ((application.Schema.SchemaId == "hardwarerepair")) {
                 HandleHardwareRepairFields(resultObject);
-            }
-            else if (application.Schema.SchemaId == "incidentdetail")
-            {
+            } else if (application.Schema.SchemaId == "incidentdetail") {
                 HandleIncidentDetailsFields(resultObject);
-            }
-            else if (application.Schema.SchemaId == "incidentperlocation")
-            {
+            } else if (application.Schema.SchemaId == "incidentperlocation") {
                 HandleIncidentPerLocationFields(resultObject);
             }
             return dbList;
         }
 
-        private void HandleClosedDate(DataMap resultObject, ApplicationMetadata application)
-        {
+        private void HandleClosedDate(DataMap resultObject, ApplicationMetadata application) {
             var status = resultObject.GetAttribute("status");
             var statusDate = resultObject.GetAttribute("statusdate");
-            if ("CLOSED".Equals(status))
-            {
+            if ("CLOSED".Equals(status)) {
                 resultObject.Attributes["#closeddate"] = statusDate;
-            }
-            else
-            {
+            } else {
                 resultObject.Attributes["#closeddate"] = "";
             }
         }
 
-        public static void HandleIncidentPerLocationFields(IEnumerable<AttributeHolder> resultObject)
-        {
-            foreach (var attributeHolder in resultObject)
-            {
+        public static void HandleIncidentPerLocationFields(IEnumerable<AttributeHolder> resultObject) {
+            foreach (var attributeHolder in resultObject) {
                 //Displaying the last 3 characters of pluspcustomer.
-                try
-                {
+                try {
                     attributeHolder.Attributes["#currentdatetime"] = DateTime.Now;
                     var splitedSiteId = attributeHolder.Attributes[SITEID].ToString().Split('-');
-                    if (splitedSiteId.Length == 3 && splitedSiteId[2].Length == 3)
-                    {
+                    if (splitedSiteId.Length == 3 && splitedSiteId[2].Length == 3) {
                         attributeHolder.Attributes[SITEID] = splitedSiteId[2];
-                    }
-                    else
-                    {
+                    } else {
                         attributeHolder.Attributes[SITEID] = attributeHolder.Attributes[SITEID].ToString();
                     }
 
                     var description = attributeHolder.GetAttribute("commodities_.description") as String;
-                    if (description == null || description.IndexOf('-') == -1)
-                    {
+                    if (description == null || description.IndexOf('-') == -1) {
                         attributeHolder.Attributes[COMMODITY] = MISSING;
                         attributeHolder.Attributes[COMMODITYGROUP] = MISSING;
-                    }
-                    else
-                    {
+                    } else {
                         string[] arrTemp = description.Split('-');
                         var commodityGroup = arrTemp[0] + '-' + arrTemp[1];
                         attributeHolder.Attributes[COMMODITYGROUP] = commodityGroup;
                         attributeHolder.Attributes[COMMODITY] = description.Substring(commodityGroup.Length + 1, description.Length - commodityGroup.Length - 1);
                     }
-                }
-                catch (ArgumentOutOfRangeException ae)
-                {
+                } catch (ArgumentOutOfRangeException ae) {
                 }
             }
         }
 
-        public static void HandleIncidentDetailsFields(IEnumerable<AttributeHolder> resultObject)
-        {
-            foreach (var attributeHolder in resultObject)
-            {
+        public static void HandleIncidentDetailsFields(IEnumerable<AttributeHolder> resultObject) {
+            foreach (var attributeHolder in resultObject) {
                 //Displaying the last 3 chareacters of pluspcustomer.
-                try
-                {
+                try {
                     attributeHolder.Attributes["#currentdatetime"] = DateTime.Now;
                     attributeHolder.Attributes[SITEID] = attributeHolder.Attributes[SITEID].ToString().Split('-').Last().Substring(0, 3);
-                }
-                catch (ArgumentOutOfRangeException ae)
-                {
+                } catch (ArgumentOutOfRangeException ae) {
                 }
             }
         }
 
-        public static void HandleHardwareRepairFields(IEnumerable<AttributeHolder> resultObject)
-        {
-            foreach (var attributeHolder in resultObject)
-            {
+        public static void HandleHardwareRepairFields(IEnumerable<AttributeHolder> resultObject) {
+            foreach (var attributeHolder in resultObject) {
                 attributeHolder.Attributes["#currentdatetime"] = DateTime.Now;
                 var description = attributeHolder.GetAttribute("commodities_.description");
-                if (description == null || description.ToString().IndexOf('-') == -1)
-                {
+                if (description == null || description.ToString().IndexOf('-') == -1) {
                     attributeHolder.Attributes[SystemField] = MISSING;
                     attributeHolder.Attributes[ItemField] = MISSING;
                     attributeHolder.Attributes[ModuleField] = MISSING;
                     attributeHolder.Attributes[ComponentField] = MISSING;
-                }
-                else
-                {
+                } else {
                     string[] arrTemp = description.ToString().Split('-');
                     attributeHolder.Attributes[SystemField] = arrTemp[0] + '-' + arrTemp[1];
                     attributeHolder.Attributes[ComponentField] = (arrTemp.Length > 2) ? arrTemp[2] : MISSING;
@@ -283,8 +222,7 @@ namespace softwrench.sw4.Hapag.Data.DataSet
             }
         }
 
-        public override string ApplicationName()
-        {
+        public override string ApplicationName() {
             return "incident";
         }
     }
