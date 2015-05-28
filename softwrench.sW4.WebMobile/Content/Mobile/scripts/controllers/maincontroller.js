c404b235e5a6c0b387562ff718f45ba8b93273e8
lhrolim <luizhenrique.rolim@gmail.com>
2015-05-28 12:52:01 -0300

    changing menu

--- softwrench.sW4.WebMobile/Content/Mobile/scripts/controllers/maincontroller.js
+++ softwrench.sW4.WebMobile/Content/Mobile/scripts/controllers/maincontroller.js
@@ -1,4 +1,4 @@
-﻿softwrench.controller('MainController', function ($scope,$state, $ionicSideMenuDelegate,menuModelService,crudContextService) {
+﻿softwrench.controller('MainController', function ($scope, routeService, $ionicSideMenuDelegate, menuModelService, crudContextService) {
     $scope.data = {};
 
     function init() {
@@ -15,6 +15,11 @@
 
     $scope.loadApplication = function (menuleaf) {
         crudContextService.loadApplicationGrid(menuleaf.application,menuleaf.title, menuleaf.schema);
+        $ionicSideMenuDelegate.toggleLeft();
+    }
+
+    $scope.loadAction = function (action) {
+        routeService.go(action);
         $ionicSideMenuDelegate.toggleLeft();
     }
 
