26201862a397a025ee4647b308a16a8730c044d6
lhrolim <luizhenrique.rolim@gmail.com>
2015-06-01 12:42:24 -0300

    crud refactoring ++

--- softwrench.sW4.WebMobile/Content/Mobile/scripts/controllers/crud_detail.js
+++ softwrench.sW4.WebMobile/Content/Mobile/scripts/controllers/crud_detail.js
@@ -20,21 +20,6 @@
         return crudContextService.currentTitle();
     }
 
-    $scope.isFieldHidden = function (fieldMetadata) {
-        return fieldService.isFieldHidden(crudContextService.currentDetailItem(), crudContextService.currentDetailSchema(), fieldMetadata);
-    }
-
-    $scope.associationSearch = function (queryparameters) {
-        return offlineAssociationService.filterPromise(crudContextService.currentDetailSchema(), $scope.datamap, queryparameters.identifier, queryparameters.query);
-    }
-
-    $scope.getAssociationLabelField = function (fieldMetadata) {
-        return 'datamap.' + fieldMetadata.labelFields[0];
-    }
-
-    $scope.getAssociationValueField = function (fieldMetadata) {
-        return 'datamap.' + fieldMetadata.valueField;
-    }
 
     $scope.detailSummary = function () {
         return schemaService.getTitle(crudContextService.currentDetailSchema(), $scope.datamap, true);
@@ -50,17 +35,15 @@
         crudContextService.navigatePrevious();
         $scope.datamap = crudContextService.currentDetailItem();
     }
-
     $rootScope.$on('$stateChangeSuccess',
-        function (event, toState, toParams, fromState, fromParams) {
-            if (toState.name == "main.cruddetail") {
-                //needs to refresh the displayables and datamap everytime the detail page is loaded.
-                init();
-            }
-        });
+          function (event, toState, toParams, fromState, fromParams) {
+              if (toState.name.startsWith("main.cruddetail")) {
+                  //needs to refresh the displayables and datamap everytime the detail page is loaded.
+                  init();
+              }
+          });
 
     init();
-
 
 }
 );
--- softwrench.sW4.WebMobile/Content/Mobile/scripts/controllers/crud_input.js
+++ softwrench.sW4.WebMobile/Content/Mobile/scripts/controllers/crud_input.js
@@ -1,9 +1,5 @@
 ﻿softwrench.controller('CrudInputController', function ($log, $scope, $rootScope, schemaService, crudContextService, fieldService, offlineAssociationService, $ionicPopover) {
 
-    function init() {
-        $scope.displayables = crudContextService.mainDisplayables();
-        $scope.datamap = crudContextService.currentDetailItem();
-    }
 
     $ionicPopover.fromTemplateUrl('Content/Mobile/templates/compositionmenu.html', {
         scope: $scope,
@@ -27,19 +23,7 @@
         return 'datamap.' + fieldMetadata.valueField;
     }
 
-    $scope.detailSummary = function () {
-        return schemaService.getTitle(crudContextService.currentDetailSchema(), $scope.datamap, true);
-    }
-
-    $rootScope.$on('$stateChangeSuccess',
-        function (event, toState, toParams, fromState, fromParams) {
-            if (toState.name == "main.cruddetail") {
-                //needs to refresh the displayables and datamap everytime the detail page is loaded.
-                init();
-            }
-        });
-
-    init();
+  
 
 
 }
