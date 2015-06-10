98b7a9ae902448dcb9baebd09e650604a06830cb
Ken <kpederson@controltechnologysolutions.com>
2015-06-10 16:18:44 -0700

    Fix aliasing and longdescription
    Remove as from table name aliasing when using oracle.
    Update longdescription relationships to use a shorter qualifier.

--- softWrench.sW4/Metadata/Applications/DataSet/IncidentDataSet.cs
+++ softWrench.sW4/Metadata/Applications/DataSet/IncidentDataSet.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using softWrench.sW4.Data;
 using System.Threading.Tasks;
+using cts.commons.persistence;
 using softwrench.sw4.Shared2.Data.Association;
 using softWrench.sW4.Data.Persistence.Dataset.Commons;
 using softWrench.sW4.Metadata.Applications.DataSet.Filter;
 using softwrench.sw4.Shared2.Data.Association;
 using softwrench.sw4.Shared2.Data.Association;
 using softWrench.sW4.Data.Search;
+using softWrench.sW4.Util;
 
 namespace softWrench.sW4.Metadata.Applications.DataSet {
     class IncidentDataSet : MaximoApplicationDataSet {
@@ -72,6 +74,32 @@ namespace softWrench.sW4.Metadata.Applications.DataSet {
                                         parameters.OriginalEntity.Attributes["orgid"],
                                         parameters.OriginalEntity.Attributes["siteid"],
                                         parameters.OriginalEntity.Attributes["woclass"] == "" ? parameters.OriginalEntity.Attributes["woclass"] : "ASSET");
+
+            if (ApplicationConfiguration.IsOracle(DBType.Maximo))
+            {
+                query = string.Format(@"SELECT c.classstructureid AS ID,
+                                               p3.classificationid AS CLASS_5,
+                                               p2.classificationid AS CLASS_4,
+                                               p1.classificationid AS CLASS_3,
+                                               p.classificationid AS CLASS_2,
+                                               c.classificationid AS CLASS_1
+                                        from classstructure  c
+                                        left join classstructure  p on p.classstructureid = c.parent
+                                        left join classstructure  p1 on p1.classstructureid = p.parent
+                                        left join classstructure  p2 on p2.classstructureid = p1.parent
+                                        left join classstructure  p3 on p3.classificationid = p2.parent
+                                        where
+                                        c.haschildren = 0
+                                        and (c.orgid is null or (c.orgid is not null and c.orgid = '{0}' ))
+                                        and (c.siteid is null or (c.siteid is not null and c.siteid = '{1}' ))
+                                        and c.classstructureid in (select classusewith.classstructureid
+                                        from classusewith
+                                        where classusewith.classstructureid=c.classstructureid
+                                        and objectname= '{2}')",
+                                        parameters.OriginalEntity.Attributes["orgid"],
+                                        parameters.OriginalEntity.Attributes["siteid"],
+                                        parameters.OriginalEntity.Attributes["woclass"] == "" ? parameters.OriginalEntity.Attributes["woclass"] : "ASSET");
+            }
 
             var result = MaxDAO.FindByNativeQuery(query, null);
             var list = new List<AssociationOption>();
