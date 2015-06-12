3ed4c0f880b4bafd5eddf34a5de56d83869b7a8e
lhrolim <luizhenrique.rolim@gmail.com>
2015-06-12 16:07:13 -0300

    fix syncoperation/batches

--- softwrench.sW4.WebMobile/Content/Mobile/scripts/services/synchronization/synchronizationOperationService.js
+++ softwrench.sW4.WebMobile/Content/Mobile/scripts/services/synchronization/synchronizationOperationService.js
@@ -22,31 +22,20 @@ modules.webcommons.factory("synchronizationOperationService", ['$log', '$q', 'sw
                 };
                 // return promise chain initiated with the creation of SyncOperation
                 return swdbDAO.instantiate("SyncOperation", operation)
-                    .then(function (operationEntity) {
-                        var dbPromises = [$q.when(operationEntity)];
-                        relatedBatches.forEach(function(batch) {
-                            dbPromises.push(swdbDAO.instantiate("Batch", batch));
+                    .then(function(operationEntity) {
+                        relatedBatches.forEach(function (batch) {
+                            operationEntity.batches.add(batch);
+                            batch.syncoperation = operationEntity;
                         });
-                        return $q.all(dbPromises);
-                    })
-                    .then(function (results) {
                         var deferred = $q.defer();
-                        var syncOperation = results[0];
-                        var batches = results.slice(1, results.length);
-                        // two-way relational mapping
-                        batches.forEach(function(batch) {
-                            syncOperation.batches.add(batch);
-                            batch.syncoperation = syncOperation;
-                        });
-                        // saving entities in single transaction
                         persistence.transaction(function(tx) {
                             try {
-                                swdbDAO.bulkSave(batches, tx);
-                                swdbDAO.save(syncOperation, tx);
+                                swdbDAO.bulkSave(relatedBatches, tx);
+                                swdbDAO.save(operationEntity, tx);
                                 // flushing transaction
                                 persistence.flush(tx, function() {
                                     // resolve promise with the syncOperation
-                                    deferred.resolve(syncOperation);
+                                    deferred.resolve(operationEntity);
                                 });
                             } catch (error) {
                                 // reject promise with DB error
