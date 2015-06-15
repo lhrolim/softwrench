f4d72a0afc634159b0dec7eb45e856e6189192df
Scott A. Matthews <scott@designsbysm.com>
2015-06-15 16:10:12 -0700

    SWWEB-726 breadcrumb progress

--- softWrench.sW4.Web/Content/Scripts/client/directives/breadcrumb.js
+++ softWrench.sW4.Web/Content/Scripts/client/directives/breadcrumb.js
@@ -11,28 +11,34 @@ app.directive('breadcrumb', function ($rootScope, $log, $timeout) {
         },
         link: function (scope, element, attr) {
             scope.$watch('title', function () {
-                //log.debug('current title', scope.title);
                 element.html(getBreadCrumbHTML(log, scope.menu, scope.title));
             });  
         },
     }
 });
 
-var seperator = '&emsp;<span class="seperator">/</span>&emsp;';
+var seperator = '<span class="part seperator">/</span>';
 
 function getBreadCrumbHTML(log, menu, current) {
     log.debug(menu, current);
 
-    var path = '<i class="fa fa-home"></i>&ensp;';
+    var path = '<div class="part"><a data-toggle="dropdown" aria-expanded="false"><i class="fa fa-home"></i>&ensp;';
 
+    //get the title of the home item
     if (menu.leafs[0].controller == 'Dashboard') {
         path += 'Dashboard';
     } else {
         path += 'Home';
     }
+    path += '</a>';
 
+    //add submenu
+    path += getChildMenu(log, menu.leafs, null);
+    path += '</div>';
+
+    //append child parts
     if (current != undefined) {
-        var foundPath = findCurrentPage(log, menu.leafs, current);
+        var foundPath = findCurrentPage(log, menu.leafs, current, null);
 
         if (foundPath) {
             path += seperator;
@@ -43,22 +49,86 @@ function getBreadCrumbHTML(log, menu, current) {
     return path;
 }
 
-function findCurrentPage(log, leafs, current) {
+function findCurrentPage(log, leafs, current, parent) {
     var path = '';
 
     if (leafs != null) {
         for (var id in leafs) {
-            var newPath = findCurrentPage(log, leafs[id].leafs, current);
+            var newPath = findCurrentPage(log, leafs[id].leafs, current, leafs[id]);
 
             var icon = '<i class="' + leafs[id].icon + '"></i>&ensp;'
             if (newPath != undefined && newPath != '') {
-                path = icon + leafs[id].title + seperator + newPath;
 
-                //TODO: call method to get siblings and build menu
+                //build button and dropdown menu
+                path += '<div class="part">';
+                path += '<a data-toggle="dropdown" aria-expanded="false">';
+                path += icon + leafs[id].title;
+                path += '</a>';
+                path += getChildMenu(log, leafs, leafs[id]);
+                path += '</div>';
+                path += seperator + newPath;
+
             } else if (leafs[id].title == current) {
-                path = icon + leafs[id].title;
+
+                //build the button and dropdown menu
+                path += '<div class="part">';
+                path += '<a>';
+                path += icon + leafs[id].title;
+
+                //build the dropdown menu
+                if (leafs[id].leafs != null) {
+                    //TODO: get children menu items
+                    log.debug('get children menu items', leafs[id]);
+                }
+
+                path += '</a>';
+                path += '</div>';
             }
         }
+    }
+
+    return path;
+}
+
+
+function getChildMenu(log, leafs, parent) {
+    var path = '';
+    var searchLeafs = null;
+
+    //if no parent use the get the whole menu, else the child items from the parent
+    if (parent == null) {
+        searchLeafs = leafs
+    } else {
+        if (parent.leafs != null) {
+            searchLeafs = parent.leafs;
+        }
+    }
+
+    if (searchLeafs != null) {
+        path += '<ul class="dropdown-menu" role="menu">';
+        for (var id in searchLeafs) {
+            if (searchLeafs[id].title != null) {
+                var menuItem = getChildMenu(log, searchLeafs[id].leafs, searchLeafs[id]);
+
+                //if child items found, display as submenu
+                if (menuItem) {
+                    path += '<li class="dropdown-submenu">';
+                } else {
+                    path += '<li>';
+                }
+
+                //build the menu item
+                path += '<a data-toggle="dropdown" aria-expanded="false"><i class="' + searchLeafs[id].icon + '"></i>&ensp;' + searchLeafs[id].title + '</a>';
+
+                //append the child menu items
+                if (menuItem) {
+                    path += menuItem;
+                }
+
+                path += '</li>';
+            }
+        }
+        path += '</ul>';
     }
 
     return path;
