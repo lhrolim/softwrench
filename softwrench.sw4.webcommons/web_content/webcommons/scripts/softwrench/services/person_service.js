de4047744699d45067096617bb6d8f9d4fa19363
Ken <kpederson@controltechnologysolutions.com>
2015-06-16 13:19:41 -0700

    Add lookups to person detail
    Add lookups to default person detail
    Add lookups for Delta Dental custom fields on person detail

---
+++ softwrench.sw4.webcommons/web_content/webcommons/scripts/softwrench/services/person_service.js
@@ -0,0 +1,20 @@
+﻿var app = angular.module('sw_layout');
+
+app.factory('personService', function ($http, $timeout) {
+    return {
+        afterChangeAltDisplayname: function (datamap) {
+            if (datamap.fields['altdisplayname'] === null) {
+                datamap.fields['altperson'] = null;
+                datamap.fields['altfirstname'] = null;
+                datamap.fields['altlastname'] = null;
+                datamap.fields['altphone'] = null;
+                datamap.fields['altemail'] = null;
+            } else {
+                datamap.fields['altfirstname'] = datamap.fields['altperson_.firstname'];
+                datamap.fields['altlastname'] = datamap.fields['altperson_.lastname'];
+                datamap.fields['altphone'] = datamap.fields['altperson_.phone_.phonenum'];
+                datamap.fields['altemail'] = datamap.fields['altperson_.email_.emailaddress'];
+            }
+        },
+    }
+});
