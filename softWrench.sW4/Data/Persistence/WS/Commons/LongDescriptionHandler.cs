413ef1af59631671084741279b2398b1901b998d
lhrolim <luizhenrique.rolim@gmail.com>
2015-06-18 13:20:06 -0300

    refactorings on server side

--- softWrench.sW4/Data/Persistence/WS/Commons/LongDescriptionHandler.cs
+++ softWrench.sW4/Data/Persistence/WS/Commons/LongDescriptionHandler.cs
@@ -11,7 +11,7 @@ namespace softWrench.sW4.Data.Persistence.WS.Commons {
             if (ld != null) {
                 w.SetValue(integrationObject, property, ld.GetAttribute("ldtext"));
             } else {
-                var longDescription = entity.GetAttribute("longdescription_.ldtext") ?? entity.GetAttribute("worklogdesc_.ldtext");
+                var longDescription = entity.GetAttribute("ld_.ldtext") ?? entity.GetAttribute("wld_.ldtext");
                 w.SetValue(integrationObject, property, longDescription);
             }
 
