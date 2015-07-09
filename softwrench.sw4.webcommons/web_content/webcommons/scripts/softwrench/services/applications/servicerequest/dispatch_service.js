44bd06949328f3b1af88b8288f37af0c35b21565
Ken <kpederson@controltechnologysolutions.com>
2015-07-09 13:02:41 -0700

    Add dispatch feature to DD SR
    Add a dispatch feature to Delta Dentals SR. Dispatch will create a new
    Work Order from the SR being dispatched.

---
+++ softwrench.sw4.webcommons/web_content/webcommons/scripts/softwrench/services/applications/servicerequest/dispatch_service.js
@@ -0,0 +1,36 @@
+﻿
+(function () {
+    'use strict';
+
+    angular.module('maximo_applications').factory('dispatchService', ['$rootScope', 'applicationService', 'alertService', 'commandService', dispatchService]);
+
+    function dispatchService($rootScope, applicationService, alertService, commandService) {
+
+        var service = {
+            dispatch: dispatch
+        };
+
+        return service;
+
+        function dispatch(schema, datamap) {
+            var localDatamap = datamap;
+            if (datamap.fields) {
+                localDatamap = datamap.fields;
+            }
+
+            if (!localDatamap.owner && !localDatamap.ownergroup) {
+                alertService.alert("Owner or owner group must be filled before dispatch can be performed.");
+                return 0;
+            }
+
+            var crudData = {
+                crud: localDatamap
+            };
+            var extraParameters = {
+                id: localDatamap.ticketid
+            }
+            return applicationService.invokeOperation("servicerequest", "editdetail", "DispatchWO", crudData, extraParameters);
+        }
+
+    }
+})();
