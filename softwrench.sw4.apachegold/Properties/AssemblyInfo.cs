2524823928bbd75fa3365aed0b67fc092763b7ae
Scott A. Matthews <scott@designsbysm.com>
2015-07-21 13:01:48 -0700

    SWWEB-1219 update assemblyinfo

--- softwrench.sw4.apachegold/Properties/AssemblyInfo.cs
+++ softwrench.sw4.apachegold/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("softwrench.sw4.southern_unreg")]
+[assembly: AssemblyTitle("softwrench.sw4.apachegold")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("softwrench.sw4.southern_unreg")]
+[assembly: AssemblyProduct("softwrench.sw4.apachegold")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
