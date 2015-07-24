02cf91a377b2a743b8a0ead35de180a71f3dc08a
Ken <kpederson@controltechnologysolutions.com>
2015-07-24 10:31:50 -0700

    Add email history to softwrench
    Softwrench needs to track entered email addresses that are not a part of
    the maximo database so that users only need to type the first few
    characters of an email address they have used previously.

---
+++ softWrench.sW4/Email/Email.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cts.commons.persistence;
+using NHibernate.Mapping.Attributes;
+
+namespace softWrench.sW4.Email {
+    [Class(Table = "EMAIL_HISTORY", Lazy = false)]
+    class Email : IBaseEntity  {
+
+        [Id(0, Name = "Id")]
+        [Generator(1, Class = "native")]
+        public virtual int? Id { get; set; }
+
+        [Property]
+        public virtual string UserID { get; set; }
+
+        [Property]
+        public virtual string EmailAddress { get; set; }
+
+        public Email() {
+            
+        }
+
+        public Email(int? id, string userid, string emailaddress) {
+            Id = id;
+            UserID = userid;
+            EmailAddress = emailaddress;
+        }
+    }
+}
---
+++ softWrench.sW4/Metadata/Applications/DataSet/BaseCommLogDataSet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using cts.commons.persistence;
+using softWrench.sW4.Data.Persistence.Dataset.Commons;
+using softwrench.sw4.Shared2.Data.Association;
+using softWrench.sW4.Metadata.Security;
+using softWrench.sW4.Security.Services;
+using softWrench.sW4.Metadata.Applications.DataSet.Filter;
+
+namespace softWrench.sW4.Metadata.Applications.DataSet {
+    public class BaseCommLogDataSet : MaximoApplicationDataSet {
+
+        private readonly ISWDBHibernateDAO _swdbDAO;
+
+        public BaseCommLogDataSet(ISWDBHibernateDAO swdbDAO) {
+            _swdbDAO = swdbDAO;
+        }
+
+        public IEnumerable<IAssociationOption> EmailPostFilter(AssociationPostFilterFunctionParameters postParams) {
+
+            InMemoryUser currentUser = SecurityFacade.CurrentUser();
+
+            var addresses = _swdbDAO.FindByQuery<Email.Email>("FROM Email WHERE UserID = {0}", currentUser.MaximoPersonId).ToList();
+
+            return (from item in postParams.Options where item.Label != null && item.Value.Equals(postParams.OriginalEntity.Attributes["itemnum"]) select new AssociationOption(item.Label, item.Label)).Cast<IAssociationOption>().ToList();
+        }
+
+        public override string ApplicationName() {
+            return "commlog";
+        }
+
+        public override string ClientFilter() {
+            return "otb";
+        }
+    }
+}
