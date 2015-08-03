0695ecf47766ba2cb88753641942214ab6a80c9f
lhrolim <luizhenrique.rolim@gmail.com>
2015-08-03 19:01:15 -0300

    bug fix

--- softwrench.sW4.WebMobile/Content/Mobile/scripts/persistence/services/swdbDAO.js
+++ softwrench.sW4.WebMobile/Content/Mobile/scripts/persistence/services/swdbDAO.js
@@ -284,6 +284,7 @@
             },
 
             findSingleByQuery: function (entity, query, options) {
+                options = options || {};
                 var optionsToUse = angular.copy(options);
                 optionsToUse.pagesize = 1;
                 optionsToUse.pageNumber = 1;
