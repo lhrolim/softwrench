88823a929af00a1e826b57b02a3093dac9e33ba6
Scott A. Matthews <scott@designsbysm.com>
2015-08-03 10:23:42 -0700

    SWWEB-1218 start progress

--- softWrench.sW4.Web/Content/Scripts/client/directives/breadcrumb.js
+++ softWrench.sW4.Web/Content/Scripts/client/directives/breadcrumb.js
@@ -1,7 +1,55 @@
 ﻿var app = angular.module('sw_layout');
 
+function mapDOM(element, json) {
+    var treeObject = {};
+
+    // If string convert to document Node
+    if (typeof element === "string") {
+        if (window.DOMParser) {
+            parser = new DOMParser();
+            docNode = parser.parseFromString(element, "text/xml");
+        } else { // Microsoft strikes again
+            docNode = new ActiveXObject("Microsoft.XMLDOM");
+            docNode.async = false;
+            docNode.loadXML(element);
+        }
+        element = docNode.firstChild;
+    }
+
+    //Recursively loop through DOM elements and assign properties to object
+    function treeHTML(element, object) {
+        object["type"] = element.nodeName;
+        var nodeList = element.childNodes;
+        if (nodeList != null) {
+            if (nodeList.length) {
+                object["content"] = [];
+                for (var i = 0; i < nodeList.length; i++) {
+                    if (nodeList[i].nodeType == 3) {
+                        object["content"].push(nodeList[i].nodeValue);
+                    } else {
+                        object["content"].push({});
+                        treeHTML(nodeList[i], object["content"][object["content"].length - 1]);
+                    }
+                }
+            }
+        }
+        if (element.attributes != null) {
+            if (element.attributes.length) {
+                object["attributes"] = {};
+                for (var i = 0; i < element.attributes.length; i++) {
+                    object["attributes"][element.attributes[i].nodeName] = element.attributes[i].nodeValue;
+                }
+            }
+        }
+    }
+    treeHTML(element, treeObject);
+
+    return (json) ? JSON.stringify(treeObject) : treeObject;
+}
+
 app.directive('breadcrumb', function ($rootScope, $log, $compile, menuService) {
     var log = $log.getInstance('sw4.breadcrumb');
+    //log.debug($rootScope);
 
     return {
         scope: {
@@ -10,10 +58,136 @@ app.directive('breadcrumb', function ($rootScope, $log, $compile, menuService) {
             title: '='
         },
         link: function (scope, element, attr) {
-            log.debug(scope.menu);
+
 
             scope.$watch('title', function (newValue, oldValue) {
-                var template = (getBreadCrumbHTML(log, scope.menu, scope.title, menuService));
+                log.debug(scope.menu);
+                //log.debug(scope, scope.schema1);
+                //build breadcrumb based on menu.xml
+
+                var template;
+                //var template = '<div class="part main" bc-menu>';
+                //template += '<a data-toggle="dropdown" aria-expanded="false">';
+                //template += '<i class="fa fa-bars"></i>';
+                //template += '&ensp;<i class="fa fa-caret-down"></i>';
+                //template += '</a>';
+
+
+
+                //template += seperator;
+                //template += '</div>';
+
+                //log.debug('template', template, seperator);
+
+                //if (scope.title) {
+
+                //log.debug(menuItem);
+
+                //var menuIcon;
+                //log.debug(scope.title, currentItem, template != null);
+
+                //if the current page is in the admin menu
+                var currentItem = $('.admin-area .admin-menu .dropdown-menu a:contains("' + scope.title + '")');
+                if (currentItem.hasOwnProperty(length)) {
+                    //menuIcon = menuItem[0].firstChild;
+                    //console.log(menuItem, menuIcon);
+
+                    //
+                    template = getBreadCrumbHTML(log, scope.menu, undefined, menuService);
+                    template += seperator;
+
+                    var mainItem = $('.admin-area .admin-menu > a');
+
+                    if (mainItem.hasOwnProperty(length)) {
+                        log.debug(mainItem, mainItem[0].firstChild);
+
+
+
+                        template += '<span class="part">';
+                        template += '<a>';
+                        template += mainItem[0].firstElementChild.outerHTML;
+                        template += '&ensp;';
+                        template += 'Admin';
+                        template += '&ensp;';
+                        template += '</a>';
+                        template += '</span>';
+                    }
+                    //TODO: parse admin menu
+                    //TODO: build admin menu
+
+                } else {
+                    template = getBreadCrumbHTML(log, scope.menu, scope.title, menuService);
+                }
+                //log.debug(template);
+
+
+
+                var mainMenu = $('.admin-area .admin-menu > .dropdown-menu');
+                //var children = mainMenu[0].children;
+                var leafs = getAdminLeafs(log, mainMenu[0].children);
+
+                var adminMenu = {};
+                adminMenu.leafs = leafs;
+
+                log.debug(mainMenu, adminMenu);
+
+                //if (children.hasOwnProperty(length)) {
+                //    //for (index in children) {
+                //    for (index = 1; index < children.length; index++) {
+                //        //console.log(mainMenu[0].children[index].firstElementChild.firstElementChild);
+
+
+                //        if (children[index].localName != undefined) {
+                //            var iconClass = '';
+
+                //            //log.debug(children[index], children[index].localName);
+                //            if (children[index].localName == 'li') {
+                //                var link = children[index].firstElementChild;
+                //                var icon = link.firstElementChild;
+
+                //                log.debug(link.innerText.trim());
+
+                //                if (icon != null) {
+                //                    if (icon.className != undefined) {
+
+                //                        //for (idx in icon.classList) {
+                //                        //    log.debug(icon.classList[idx]);
+                //                        //}
+
+                //                        for (i = 0; i < icon.classList.length; i++) {
+                //                            //text += cars[i] + "<br>";
+                //                            //log.debug(icon.classList[i]);
+
+                //                            if (icon.classList[i] != 'fa-fw') {
+                //                                iconClass += icon.classList[i] + ' ';
+                //                            }
+                //                        }
+
+                //                       // ,,innerText
+
+                //                        //iconClass
+                //                        log.debug(iconClass);
+                //                    }
+                //                }
+                //            }
+                //        }
+                //    }
+                //}
+
+                // Test with an element.
+                //var initElement = document.getElementsByTagName("html")[0];
+                //log.debug(initElement);
+
+                //var json = mapDOM(mainMenu, true);
+                //log.debug(json);
+
+                // Test with a string.
+                //initElement = "<div><span>text</span>Text2</div>";
+                //json = mapDOM(initElement, true);
+                //console.log(json);
+
+
+
                 if (template != null) {
                     var content = $compile(template)(scope);
                     element.html(content);
@@ -25,18 +199,14 @@ app.directive('breadcrumb', function ($rootScope, $log, $compile, menuService) {
 });
 
 
-app.directive('bcMenuItem', function ($rootScope, $log, $compile,menuService) {
+app.directive('bcMenuItem', function ($rootScope, $log, $compile, menuService) {
     var log = $log.getInstance('sw4.breadcrumb Menu Item');
-
-
 
     return {
         link: function (scope, element, attr) {
             $compile(element.contents())(scope);
         },
         controller: function ($scope, alertService, validationService) {
-
-
 
             $scope.goToApplication = function (title) {
                 var leaf = findleafByTitle(log, $scope.menu.leafs, title);
@@ -73,7 +243,264 @@ app.directive('bcMenuItem', function ($rootScope, $log, $compile,menuService) {
 });
 
 
-function getBreadCrumbHTML(log, menu, current,menuService) {
+function getAdminLeafs(log, kids) {
+    var leafs = null;
+    log.debug(kids, kids.length);
+
+
+    //for (m in kids) {
+    //    log.debug('found', kids[m]);
+    //}
+
+    //for (index = 0; index < kids.length; index++) {
+    //    log.debug('found', kids[index]);
+    //}
+
+
+
+
+    //if (kids != null && kids.length > 0) {
+    //    //for (index in kids) {
+        for (idx = 0; idx < kids.length; idx++) {
+    //        //console.log(mainMenu[0].kids[idx].firstElementChild.firstElementChild);
+
+            //if (kids[idx].localName != undefined) {
+            //    log.debug(idx, kids[idx]);
+            //}
+
+            if (kids[idx].localName != undefined) {
+                var iconClass = '';
+
+                log.debug(idx, kids[idx].localName, kids[idx]);
+
+    //            //log.debug(kids[idx], kids[idx].localName);
+                switch (kids[idx].localName) {
+                    case 'a':
+                        break;
+                    case 'li':
+    //            //if (kids[idx].localName == 'li') {
+    //                    var link = kids[idx].firstElementChild;
+    //                    var icon = link.firstElementChild;
+    //                    var title = link.innerText.trim();
+
+    //                    if (icon != null) {
+    //                        if (icon.className != undefined) {
+
+    //                            //for (idx in icon.classList) {
+    //                            //    log.debug(icon.classList[idx]);
+    //                            //}
+
+    //                            for (i = 0; i < icon.classList.length; i++) {
+    //                                //text += cars[i] + "<br>";
+    //                                //log.debug(icon.classList[i]);
+
+    //                                if (icon.classList[i] != 'fa-fw') {
+    //                                    iconClass += icon.classList[i] + ' ';
+    //                                }
+    //                            }
+
+    //                            // ,,innerText
+
+    //                            //iconClass
+    //                            //log.debug(iconClass);
+    //                        }
+    //                    }
+
+    //                    var newObject = {};
+    //                    newObject.icon = iconClass.trim();
+    //                    newObject.title = title;
+    //                    newObject.type = '';
+
+    //                    //var leafs;
+    //                    //log.debug(kids[idx].children);
+                        if (kids[idx].children != null && kids[idx].children.length > 0) {
+                            log.debug('get kids', kids[idx].children.length, kids[idx].children);
+
+    //                        //for (x = 0; x < children[idx].children.length; x++) {
+    //                        //    log.debug(children[idx].children[x]);
+    //                        //}
+
+    //                        var childLeafs;
+                            //childLeafs = getAdminLeafs(log, kids[idx].children);
+
+    //                        if (childLeafs != null) {
+    //                            newObject.leafs = childLeafs;
+    //                        }
+                        }
+
+    //                    leafs.push(newObject);
+                        break;
+
+                    case 'ul':
+                        break;
+
+                }
+            }
+       }
+    //}
+
+    return leafs;
+}
+
+function getAdminLeafsArchive(log, children) {
+    var leafs = [];
+    log.debug(children, children.length);
+
+
+    //for (m in children) {
+    //    log.debug('found', children[m]);
+    //}
+
+    for (index = 0; index < children.length; index++) {
+        log.debug('found', children[index]);
+    }
+
+
+
+
+    if (children != null && children.length > 0) {
+        //for (index in children) {
+        for (index = 0; index < children.length; index++) {
+            //console.log(mainMenu[0].children[index].firstElementChild.firstElementChild);
+
+            if (children[index].localName != undefined) {
+                log.debug(index, children[index]);
+            }
+
+            if (children[index].localName != undefined) {
+                var iconClass = '';
+
+                log.debug(children[index].localName);
+
+                //log.debug(children[index], children[index].localName);
+                switch (children[index].localName) {
+                    case 'a':
+                        break;
+                    case '~li':
+                        //if (children[index].localName == 'li') {
+                        var link = children[index].firstElementChild;
+                        var icon = link.firstElementChild;
+                        var title = link.innerText.trim();
+
+                        if (icon != null) {
+                            if (icon.className != undefined) {
+
+                                //for (idx in icon.classList) {
+                                //    log.debug(icon.classList[idx]);
+                                //}
+
+                                for (i = 0; i < icon.classList.length; i++) {
+                                    //text += cars[i] + "<br>";
+                                    //log.debug(icon.classList[i]);
+
+                                    if (icon.classList[i] != 'fa-fw') {
+                                        iconClass += icon.classList[i] + ' ';
+                                    }
+                                }
+
+                                // ,,innerText
+
+                                //iconClass
+                                //log.debug(iconClass);
+                            }
+                        }
+
+                        var newObject = {};
+                        newObject.icon = iconClass.trim();
+                        newObject.title = title;
+                        newObject.type = '';
+
+                        //var leafs;
+                        //log.debug(children[index].children);
+                        if (children[index].children != null && children[index].children.length > 0) {
+                            log.debug('get children', children[index].children);
+
+                            //for (x = 0; x < children[index].children.length; x++) {
+                            //    log.debug(children[index].children[x]);
+                            //}
+
+                            var childLeafs;
+                            childLeafs = getAdminLeafs(log, children[index].children);
+
+                            if (childLeafs != null) {
+                                newObject.leafs = childLeafs;
+                            }
+                        }
+
+                        leafs.push(newObject);
+                        break;
+
+
+                    case 'li':
+                        //if (children[index].localName == 'li') {
+                        //var link = children[index].firstElementChild;
+                        //var icon = link.firstElementChild;
+                        //var title = link.innerText.trim();
+
+                        //if (icon != null) {
+                        //if (icon.className != undefined) {
+
+                        //for (idx in icon.classList) {
+                        //    log.debug(icon.classList[idx]);
+                        //}
+
+                        //for (i = 0; i < icon.classList.length; i++) {
+                        //text += cars[i] + "<br>";
+                        //log.debug(icon.classList[i]);
+
+                        //if (icon.classList[i] != 'fa-fw') {
+                        //iconClass += icon.classList[i] + ' ';
+                        //}
+                        //}
+
+                        // ,,innerText
+
+                        //iconClass
+                        //log.debug(iconClass);
+                        //}
+                        //}
+
+                        //var newObject = {};
+                        //newObject.icon = iconClass.trim();
+                        //newObject.title = title;
+                        //newObject.type = '';
+
+                        //var leafs;
+                        //log.debug(children[index].children);
+                        if (children[index].children != null && children[index].children.length >= 5) {
+                            log.debug('get children', children[index].children);
+
+                            //for (x = 0; x < children[index].children.length; x++) {
+                            //    log.debug(children[index].children[x]);
+                            //}
+
+                            var childLeafs;
+                            childLeafs = getAdminLeafs(log, children[index].children);
+
+                            //if (childLeafs != null) {
+                            //    newObject.leafs = childLeafs;
+                            //}
+                        }
+
+                        //leafs.push(newObject);
+                        break;
+
+
+                    case 'ul':
+                        break;
+
+                }
+
+
+
+            }
+        }
+    }
+
+    return leafs;
+}
+
+function getBreadCrumbHTML(log, menu, current, menuService) {
     var path = '<div class="part main" bc-menu>';
     path += '<a data-toggle="dropdown" aria-expanded="false">';
     path += '<i class="fa fa-bars"></i>';
@@ -133,14 +560,12 @@ function findCurrentPage(log, leafs, current, parent) {
                     path += '(\'' + leafs[id].title + '\')">';
                 }
 
-
                 //add the icon and title
                 path += icon + leafs[id].title;
 
                 if (newPath) {
                     path += '&ensp;<i class="fa fa-caret-down"></i>';
                 }
-
 
                 path += '</a>';
 
@@ -163,7 +588,6 @@ function findCurrentPage(log, leafs, current, parent) {
 }
 
 function findleafByTitle(log, leafs, title) {
-
     var found = null;
 
     if (leafs != null) {
@@ -185,7 +609,7 @@ function findleafByTitle(log, leafs, title) {
     return found;
 }
 
-function getChildMenu(log, leafs, parent,menuService) {
+function getChildMenu(log, leafs, parent, menuService) {
     var path = '';
     var searchLeafs = null;
 
@@ -227,7 +651,7 @@ function getChildMenu(log, leafs, parent,menuService) {
                 }
 
                 //build the menu item
-                path += '<i class="' + leaf.icon + '"></i>&ensp;' + leaf.title;
+                path += '<i class="' + leaf.icon + '"></i>&ensp;' + leaf.title.trim();
                 path += '</a>';
 
                 //add the child menu items
@@ -245,4 +669,3 @@ function getChildMenu(log, leafs, parent,menuService) {
 }
 
 var seperator = '<span class="part seperator">/</span>';
-
