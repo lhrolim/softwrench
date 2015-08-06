86814f12b92c26c368870ea12d8bdd56507b12f0
Ken <kpederson@controltechnologysolutions.com>
2015-08-06 12:23:20 -0700

    Adding second migration with default value

---
+++ softwrench.sW4.audit/Migration/MigrationSwweb1322PartTwo.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+
+namespace softwrench.sW4.audit.Migration {
+    [Migration(201508061215)]
+    public class MigrationSwweb1322PartTwo : FluentMigrator.Migration {
+
+        public override void Up() {
+            Alter.Table("AUDIT_ENTRY").AlterColumn("RefUserId").AsString().WithDefaultValue("");
+        }
+
+        public override void Down() {
+            
+        }
+    }
+}
