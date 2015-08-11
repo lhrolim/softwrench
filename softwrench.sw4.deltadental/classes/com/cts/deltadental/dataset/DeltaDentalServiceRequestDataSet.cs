c469b4604284dbd9c36584b6958f3196d2650a40
Ken <kpederson@controltechnologysolutions.com>
2015-08-11 12:47:27 -0700

    Add precautionary formatting
    Adding a space around a hard coded SQL where clause to avoid a possible
    bad query.

--- softwrench.sw4.deltadental/classes/com/cts/deltadental/dataset/DeltaDentalServiceRequestDataSet.cs
+++ softwrench.sw4.deltadental/classes/com/cts/deltadental/dataset/DeltaDentalServiceRequestDataSet.cs
@@ -18,7 +18,7 @@ namespace softwrench.sw4.deltadental.classes.com.cts.deltadental.dataset {
         {
             var siteid = parameters.OriginalEntity.GetAttribute("siteid");
             // Create a where caluse to handle Delta's custom columns used to identify which classifications are valid in which sites
-            var customColumnsWhere = siteid != null ? string.Format("and LOWER(c.{0}) = '1'", siteid) : "";
+            var customColumnsWhere = siteid != null ? string.Format(" and LOWER(c.{0}) = '1' ", siteid) : "";
             var query = base.BuildQuery(parameters, ticketclass);
             return query + customColumnsWhere;
         }
