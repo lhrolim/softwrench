65d429e0bf9f490af7f36d76e61c515b0a239c7e
Scott A. Matthews <scott@designsbysm.com>
2015-08-20 13:15:55 -0700

    SWWEB-1275 remove old files from bundle config

--- softWrench.sW4.Web/App_Start/WebBundleConfig.cs
+++ softWrench.sW4.Web/App_Start/WebBundleConfig.cs
@@ -21,20 +21,6 @@ namespace softWrench.sW4.Web {
                "~/Content/bootstrap/css/textAngular-min.css"
                ));
 
-            //bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-            //    "~/Content/themes/base/jquery.ui.core.css",
-            //    "~/Content/themes/base/jquery.ui.resizable.css",
-            //    "~/Content/themes/base/jquery.ui.selectable.css",
-            //    "~/Content/themes/base/jquery.ui.accordion.css",
-            //    "~/Content/themes/base/jquery.ui.autocomplete.css",
-            //    "~/Content/themes/base/jquery.ui.button.css",
-            //    "~/Content/themes/base/jquery.ui.dialog.css",
-            //    "~/Content/themes/base/jquery.ui.slider.css",
-            //    "~/Content/themes/base/jquery.ui.tabs.css",
-            //    "~/Content/themes/base/jquery.ui.datepicker.css",
-            //    "~/Content/themes/base/jquery.ui.progressbar.css",
-            //    "~/Content/themes/base/jquery.ui.theme.css"));
-
             bundles.Add(new StyleBundle("~/Content/styles/thirdparty").Include(
                 "~/Content/font-awesome/css/font-awesome.css",
                 "~/Content/angular-ui-select/angular-ui-select.css"
