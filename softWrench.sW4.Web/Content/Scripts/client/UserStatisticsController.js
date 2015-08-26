22f46db6c212c07049d3614cd0e56d81fef5062c
lhrolim <luizhenrique.rolim@gmail.com>
2015-08-25 22:14:59 -0300

    success message++

--- softWrench.sW4.Web/Content/Scripts/client/UserStatisticsController.js
+++ softWrench.sW4.Web/Content/Scripts/client/UserStatisticsController.js
@@ -2,33 +2,33 @@
 (function (angular) {
     'use strict';
 
-    function userStatisticsController($scope, restService,alertService,formatService) {
-        
-        var locatePrimaryEmail = function(compositionEmails) {
+    function userStatisticsController($scope, restService, alertService, formatService) {
+
+        var locatePrimaryEmail = function (compositionEmails) {
             if (!compositionEmails) {
                 return null;
             }
-             for (var i = 0; i < compositionEmails.length; i++) {
-                 var email = compositionEmails[i];
-                 if (email.isprimary) {
-                     return email.emailaddress;
-                 }
-             }
+            for (var i = 0; i < compositionEmails.length; i++) {
+                var email = compositionEmails[i];
+                if (email.isprimary) {
+                    return email.emailaddress;
+                }
+            }
             return null;
         };
 
         $scope.activationlink = $scope.datamap["activationlink"];
 
-        $scope.getFormattedDate=function(date) {
+        $scope.getFormattedDate = function (date) {
             return formatService.formatDate(date);
         }
 
 
-        $scope.hasAlreadyLoggedIn = function() {
+        $scope.hasAlreadyLoggedIn = function () {
             return $scope.datamap["statistics"] != null;
         }
 
-        $scope.isLdap = function() {
+        $scope.isLdap = function () {
             return "true" === $scope.datamap["ldapEnabled"];
         }
 
@@ -43,16 +43,17 @@
 
             var params = {
                 userId: userid,
-                email:email
+                email: email
             }
 
-            restService.postPromise("UserSetupWebApi", "SendActivationEmail", params);
-            $scope.successMessage = "An email has been sent with instructions to reset the password";
+            restService.postPromise("UserSetupWebApi", "SendActivationEmail", params).then(function () {
+                alertService.success("An email has been sent with instructions to reset the password");
+            });
         }
     }
 
     angular.module('sw_layout')
-        .controller('UserStatisticsController', ['$scope', "restService","alertService", "formatService", userStatisticsController]);
+        .controller('UserStatisticsController', ['$scope', "restService", "alertService", "formatService", userStatisticsController]);
 
 })(angular);
 
