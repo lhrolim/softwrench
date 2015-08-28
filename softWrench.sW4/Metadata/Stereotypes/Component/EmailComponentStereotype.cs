5622365452a6845ede31c569f33a5ebf56371093
Ken <kpederson@controltechnologysolutions.com>
2015-08-28 11:44:12 -0700

    Begin adding stereotype for components
    Begin adding component stereo types

---
+++ softWrench.sW4/Metadata/Stereotypes/Component/EmailComponentStereotype.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace softWrench.sW4.Metadata.Stereotypes.Component {
+    class EmailComponentStereotype : AComponentStereotype {
+
+        private static EmailComponentStereotype _instance;
+
+        public static string NewItemValidation = "newItemValidation";
+
+        public static EmailComponentStereotype GetInstance() {
+            return _instance ?? (_instance = new EmailComponentStereotype());
+        }
+
+        private EmailComponentStereotype() { }
+
+        protected override IDictionary<string, string> DefaultValues() {
+            var values = new Dictionary<string, string>();
+            values[NewItemValidation] = "emailService.validateEmail";
+            return values;
+        }
+    }
+}
---
+++ softwrench.sw4.Shared2/Metadata/Applications/Schema/ComponentStereotype.cs
@@ -0,0 +1,5 @@
+﻿namespace softwrench.sW4.Shared2.Metadata.Applications.Schema {
+    public enum ComponentStereotype {
+        Email
+    }
+}
