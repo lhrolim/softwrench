9af2c35d86d7131d57e2244da4e817c913c0a504
lhrolim <luizhenrique.rolim@gmail.com>
2015-09-04 22:07:05 -0300

    SWOFF-115

--- softwrench.sW4.WebMobile/www/Content/Mobile/scripts/services/login/securityservice.js
+++ softwrench.sW4.WebMobile/www/Content/Mobile/scripts/services/login/securityservice.js
@@ -51,6 +51,9 @@
             var loginUrl = routeService.loginURL();
             return $http.post(loginUrl, { username: username, password: password })
                 .then(function (response) {
+                    //cleaning history so that back button does not return user to login page
+                    $ionicHistory.clearCache();
+
                     var userdata = response.data;
                     if (userdata.Found) {
                         loginLocal(userdata.UserName);
