6764b8aae666d642bad2460c888b36fc8c60038a
lhrolim <luizhenrique.rolim@gmail.com>
2015-09-11 12:31:33 -0300

    quick refactoring moving code to specific class

--- softWrench.sW4/Metadata/Menu/XmlMenuMetadataParser.cs
+++ softWrench.sW4/Metadata/Menu/XmlMenuMetadataParser.cs
@@ -36,7 +36,7 @@ namespace softWrench.sW4.Metadata.Menu {
             var indexItem = menuElement.Attribute(XmlMenuMetadataSchema.MenuIndexItemAttribute).ValueOrDefault((string)null);
             var leafs = XmlMenuParserCommons.BuildLeafs(menuElement, indexItem, modules);
             if (catalog != null) {
-                leafs = MergeWithCatalog(catalog, leafs);
+                leafs = MenuCatalogMerger.MergeWithCatalog(catalog, leafs);
             }
             var menuDefinition = new MenuDefinition(leafs, displacement, indexItem);
             modules.Sort();
@@ -44,51 +44,6 @@ namespace softWrench.sW4.Metadata.Menu {
             return menuDefinition;
         }
 
-        private List<MenuBaseDefinition> MergeWithCatalog(MenuTemplateCatalog catalog, List<MenuBaseDefinition> leafs) {
-            var mergedMenu = new List<MenuBaseDefinition>();
-            foreach (var leaf in leafs) {
-                if (leaf is ReferenceMenuItemDefinition) {
-                    mergedMenu.Add(GetMergedDefinition((ReferenceMenuItemDefinition)leaf, catalog));
-                } else {
-                    mergedMenu.Add(leaf);
-                }
-            }
-
-            return mergedMenu;
-        }
-
-        private MenuBaseDefinition GetMergedDefinition(ReferenceMenuItemDefinition referenceLeaf, MenuTemplateCatalog catalog) {
-            var item = catalog.Leafs.FirstOrDefault(f => f.Id.EqualsIc(referenceLeaf.Id));
-            if (item == null) {
-                throw MenuMetadataException.MissingReferenceException(referenceLeaf.Id);
-            }
-
-            var clonedItem = MergeOwnProperties(referenceLeaf, item);
-
-            if (CollectionUtil.NullOrEmpty(referenceLeaf.Leafs)) {
-                return clonedItem;
-            }
-
-            return MergeLeafNodes(clonedItem);
-        }
-
-        private static MenuBaseDefinition MergeLeafNodes(MenuBaseDefinition clonedItem) {
-            //TODO: finish merge applying positions to leafts, etc
-            return clonedItem;
-        }
-
-        private static MenuBaseDefinition MergeOwnProperties(ReferenceMenuItemDefinition referenceLeaf, MenuBaseDefinition item) {
-            var clonedItem = item.ShallowCopy();
-            clonedItem.Title = referenceLeaf.Title ?? clonedItem.Title;
-            clonedItem.Role = referenceLeaf.Role ?? clonedItem.Role;
-            clonedItem.Icon = referenceLeaf.Icon ?? clonedItem.Icon;
-            clonedItem.Tooltip = referenceLeaf.Tooltip ?? clonedItem.Tooltip;
-            if (clonedItem is MenuContainerDefinition) {
-                var container = clonedItem as MenuContainerDefinition;
-                container.Action = referenceLeaf.Action ?? container.Action;
-                container.Controller = referenceLeaf.Action ?? container.Controller;
-            }
-            return clonedItem;
-        }
+      
     }
 }
