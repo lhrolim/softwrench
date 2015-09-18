2705f369002cebb65b07308015283e4f3f44f5ff
lhrolim <luizhenrique.rolim@gmail.com>
2015-09-18 00:32:29 -0300

    SWWEB-1685

--- softWrench.sW4.Web/Formatting/JsonDateTimeConverter.cs
+++ softWrench.sW4.Web/Formatting/JsonDateTimeConverter.cs
@@ -12,22 +12,16 @@ namespace softWrench.sW4.Web.Formatting {
                 writer.WriteValue((string)null);
                 return;
             }
-            string datetime;
             var user = SecurityFacade.CurrentUser();
             // Chrome converts times without offsets as if they are UTC. Instead of sending the time 
             // without an offset, send the time with the users current off set so no conversion occurs.
             var userOffsetVal = 0;
-            var dateConverted = ((DateTime)value).FromMaximoToUser(user);
-            if (userOffsetVal == 0) {
-                datetime = dateConverted.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFFK");
-            } else {
-                if (user.TimezoneOffset != null) {
-                    userOffsetVal = user.TimezoneOffset.Value;
-                }
-                var userOffset = TimeSpan.FromMinutes(userOffsetVal * -1);
-                datetime =
-                    new DateTimeOffset(dateConverted, userOffset).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFFK");
+            if (user.TimezoneOffset != null) {
+                userOffsetVal = user.TimezoneOffset.Value;
             }
+            var userOffset = TimeSpan.FromMinutes(userOffsetVal * -1);
+            var datetime = new DateTimeOffset(((DateTime)value).FromMaximoToUser(user), userOffset)
+                .ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFFK");
             writer.WriteValue(datetime);
         }
 
