0135f4b396c5f549f63c3dbf4b626491b674f4ab
Scott A. Matthews <scott@designsbysm.com>
2015-10-06 11:46:15 -0700

    SWWEB-1638 start notification message mockups

---
+++ softWrench.sW4.Web/Content/Scripts/client/directives/notifications.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('sw_layout');
+
+app.directive('notifications', function (contextService, $log) {
+    var log = $log.getInstance('sw4.notifications');
+
+    return {
+        templateUrl: contextService.getResourceUrl('/Content/Templates/notifications.html'),
+        controller: function ($scope) {
+           
+        }
+    }
+});
