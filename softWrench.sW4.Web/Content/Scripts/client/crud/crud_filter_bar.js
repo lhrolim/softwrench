64facc3f700ae5c9e8829f435fb44ee90407f0c8
rodrigobotti <rodrigo.botti@gmail.com>
2015-10-15 17:41:09 -0300

    [SWWEB-1153]:documentation

--- softWrench.sW4.Web/Content/Scripts/client/crud/crud_filter_bar.js
+++ softWrench.sW4.Web/Content/Scripts/client/crud/crud_filter_bar.js
@@ -6,21 +6,28 @@
             restrict: "E",
             templateUrl: contextService.getResourceUrl("/Content/Templates/crud/crud_filter_bar.html"),
             scope: {
-                schema: "=",
-                datamap: "=",
-                searchData: "=",
-                searchOperator: "=",
-                selectAll: "=",
-                advancedFilterMode: "=",
-                filterApplied: "&"
+                schema: "=", // model's current schema
+                datamap: "=", // model's current datamap
+                searchData: "=", // shared dictionary [column : current filter search value]
+                searchOperator: "=", // shared dictionary [column : current filter operator object]
+                selectAll: "=", // shared boolean flag indicating if multiple select in filters is selected
+                advancedFilterMode: "=", // shared boolean flag indicating if advanced filter mode is activated
+                filterApplied: "&" // callback executed when the filters are applied
             },
             controller: ["$scope", "$injector", "i18NService", "fieldService", "commandService", "formatService", "expressionService", "searchService",
                 function ($scope, $injector, i18NService, fieldService, commandService, formatService, expressionService, searchService) {
 
                     var config = {
+                        /** 'don't filter' operator: helper to clear current filter */
                         noopoperator: { id: "", symbol: "", begin: "", end: "", title: "No Filter" }
                     };
 
+                    /**
+                     * Sets the operator in $scope.searchOperator[columnName]
+                     * 
+                     * @param String columnName 
+                     * @param {} operator 
+                     */
                     $scope.selectOperator = function (columnName, operator) {
                         $scope.searchOperator[columnName] = operator;
                         var searchData = $scope.searchData;
@@ -34,11 +41,22 @@
                         }
                     };
 
+                    /**
+                     * Clears the filter associated to columnName 
+                     * and invokes the 'filterApplied' callback.
+                     * 
+                     * @param String columnName 
+                     */
                     $scope.clearFilter = function(columnName) {
                         $scope.selectOperator(columnName, config.noopoperator);
                         $scope.filterApplied();
                     };
 
+                    /**
+                     * Immediately applies the filter associated to columnName.
+                     * 
+                     * @param String columnName 
+                     */
                     $scope.filterSearch = function (columnName) {
                         if (!$scope.searchOperator[columnName] || $scope.searchOperator[columnName].symbol === "") {
                             $scope.searchOperator[columnName] = searchService.defaultSearchOperation();
@@ -51,6 +69,11 @@
                         $scope.filterApplied();
                     };
 
+                    /**
+                     * Marks the $scope.datamap's values's fields as checked ("_#selected" key)
+                     * 
+                     * @param Boolean checked 
+                     */
                     $scope.toggleSelectAll = function (checked) {
                         angular.forEach($scope.datamap, function (value) {
                             value.fields["_#selected"] = checked;
