e9fb21a2f481b6ad44ae1f5114475ca6111ba1f1
Scott A. Matthews <scott@designsbysm.com>
2015-11-12 16:39:28 -0700

    Renamed panasonic to pesco

--- softwrench.sw4.pesco/Properties/AssemblyInfo.cs
+++ softwrench.sw4.pesco/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("softwrench.sw4.panasonic")]
+[assembly: AssemblyTitle("softwrench.sw4.pesco")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("softwrench.sw4.panasonic")]
+[assembly: AssemblyProduct("softwrench.sw4.pesco")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
