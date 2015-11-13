c5bc3f50aab795b9c7b4a7b12c59fa9cd80f63a5
Ken <kpederson@controltechnologysolutions.com>
2015-11-13 10:35:09 -0700

    Remove extra using statements
    Remove extra using statements

--- softWrench.sW4/Data/Persistence/Dataset/Commons/BaseMultiassetlocciDataSet.cs
+++ softWrench.sW4/Data/Persistence/Dataset/Commons/BaseMultiassetlocciDataSet.cs
@@ -1,17 +1,5 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Newtonsoft.Json.Linq;
-using softwrench.sw4.Shared2.Data.Association;
-using softWrench.sW4.Data.API.Composition;
-using softWrench.sW4.Data.Entities;
-using softWrench.sW4.Data.Persistence.Dataset.Commons.Ticket.Commlog;
-using softWrench.sW4.Data.Persistence.SWDB;
 using softWrench.sW4.Data.Search;
-using softWrench.sW4.Metadata.Applications;
-using softWrench.sW4.Metadata.Applications.DataSet;
 using softWrench.sW4.Metadata.Applications.DataSet.Filter;
-using softWrench.sW4.Security.Services;
 
 namespace softWrench.sW4.Data.Persistence.Dataset.Commons {
 
