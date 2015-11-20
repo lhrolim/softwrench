fa02050ca7d31f8217bb946a4b2c89afc925f9a6
Scott A. Matthews <scott@designsbysm.com>
2015-11-19 17:53:22 -0700

    add grunt sass processor

---
+++ softWrench.sW4.Web/Gruntfile.js
@@ -0,0 +1,44 @@
+/// <binding AfterBuild='sass' ProjectOpened='watch' />
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            files: '**/*.scss',
+            tasks: ['sass']
+        },
+        sass: {
+            options: {
+                sourceMap: true,
+            },
+            dev: {
+                files: [{
+                    expand: true,
+                    cwd: 'Content/Customers/',
+                    dest: 'Content/Customers/',
+                    src: ['**/*.scss'],
+                    ext: '.css'
+                },
+                {
+                    expand: true,
+                    cwd: 'Content/Shared/',
+                    dest: 'Content/Shared/',
+                    src: ['**/*.scss'],
+                    ext: '.css'
+                },
+                {
+                    expand: true,
+                    cwd: 'Content/styles/',
+                    dest: 'Content/styles/',
+                    src: ['**/*.scss'],
+                    ext: '.css'
+                }]
+            }
+        }
+    });
+
+    // load npm tasks
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // define default task
+    grunt.registerTask('default', ['sass', 'watch']);
+};
