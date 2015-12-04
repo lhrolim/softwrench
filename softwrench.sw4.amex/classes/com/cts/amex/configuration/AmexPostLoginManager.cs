339bbaaa7e7f8c3f4f8df580a5c3bb6a71136fde
rodrigobotti <rodrigo.botti@gmail.com>
2015-12-04 08:40:30 -0700

    [supervised training] 1st use case

---
+++ softwrench.sw4.amex/classes/com/cts/amex/configuration/AmexPostLoginManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cts.commons.simpleinjector;
+using cts.commons.simpleinjector.Events;
+using softWrench.sW4.Configuration.Services.Api;
+using softWrench.sW4.Data.Persistence;
+using softWrench.sW4.Security.Services;
+using softWrench.sW4.Util;
+
+namespace softwrench.sw4.amex.classes.com.cts.amex.configuration {
+    public class AmexPostLoginManager : ISWEventListener<UserLoginEvent> {
+
+        private readonly MaximoHibernateDAO _dao;
+
+        public AmexPostLoginManager(MaximoHibernateDAO dao) {
+            _dao = dao;
+        }
+
+
+        public void HandleEvent(UserLoginEvent userEvent) {
+
+            if (ApplicationConfiguration.ClientName != "amex") {
+                return;
+            }
+
+            var user = userEvent.InMemoryUser;
+            if (user.MaximoPersonId != null) {
+                var dciSite = _dao.FindSingleByNativeQuery<string>("select DCI_SITE from person where personid = ?",
+                    user.MaximoPersonId);
+                user.Genericproperties["DCI_SITE"] = dciSite;
+            } else {
+                //this would cause all of the grids to show no data
+                user.Genericproperties["DCI_SITE"] = "#$null";
+            }
+
+        }
+    }
+}
