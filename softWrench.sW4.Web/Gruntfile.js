bdabd7eba4a7ec350b12ee69dd55a569aa4170b8
rodrigobotti <rodrigo.botti@gmail.com>
2015-12-10 15:30:58 -0700

    run grunt from Nant

--- softWrench.sW4.Web/Gruntfile.js
+++ softWrench.sW4.Web/Gruntfile.js
@@ -3,45 +3,55 @@ module.exports = function (grunt) {
     grunt.initConfig({
         watch: {
             files: [
-                'Content/Customers/**/*.scss',
-                'Content/Shared/**/*.scss',
-                'Content/styles/**/*.scss'],
-            tasks: ['sass']
+                "Content/Customers/**/*.scss",
+                "Content/Shared/**/*.scss",
+                "Content/styles/**/*.scss"],
+            tasks: ["sass"]
         },
         sass: {
-            options: {
-                sourceMap: true,
-            },
-            dev: {
+            config: {
                 files: [{
                     expand: true,
-                    cwd: 'Content/Customers/',
-                    dest: 'Content/Customers/',
-                    src: ['**/*.scss'],
-                    ext: '.css'
+                    cwd: "Content/Customers/",
+                    dest: "Content/Customers/",
+                    src: ["**/*.scss"],
+                    ext: ".css"
                 },
                 {
                     expand: true,
-                    cwd: 'Content/Shared/',
-                    dest: 'Content/Shared/',
-                    src: ['**/*.scss'],
-                    ext: '.css'
+                    cwd: "Content/Shared/",
+                    dest: "Content/Shared/",
+                    src: ["**/*.scss"],
+                    ext: ".css"
                 },
                 {
                     expand: true,
-                    cwd: 'Content/styles/',
-                    dest: 'Content/styles/',
-                    src: ['**/*.scss'],
-                    ext: '.css'
+                    cwd: "Content/styles/",
+                    dest: "Content/styles/",
+                    src: ["**/*.scss"],
+                    ext: ".css"
                 }]
+            },
+            dev: {
+                options: {
+                    sourceMap: true
+                },
+                files: "<%= sass.config.files %>"
+            },
+            prod: {
+                options: {
+                    sourceMap: false
+                },
+                files: "<%= sass.config.files %>"
             }
+
         }
     });
 
     // load npm tasks
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks("grunt-sass");
+    grunt.loadNpmTasks("grunt-contrib-watch");
 
     // define default task
-    grunt.registerTask('default', ['sass', 'watch']);
+    grunt.registerTask("default", ["sass", "watch"]);
 };
