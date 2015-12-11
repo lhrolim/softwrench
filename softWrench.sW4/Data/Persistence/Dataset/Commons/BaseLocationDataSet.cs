c613be779be55e2e0c9e52d7abbb6e74e38f0cf2
Ken <kpederson@controltechnologysolutions.com>
2015-12-11 13:29:44 -0700

    Add method for getting classstructure hierarchypath
    Add a method for getting classstructure.hierarchypath in the
    locationdataset, and fill the value on the locationspec_ compositions.

--- softWrench.sW4/Data/Persistence/Dataset/Commons/BaseLocationDataSet.cs
+++ softWrench.sW4/Data/Persistence/Dataset/Commons/BaseLocationDataSet.cs
@@ -1,16 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Newtonsoft.Json.Linq;
+using Quartz.Util;
+using softWrench.sW4.Data.API.Composition;
+using softWrench.sW4.Data.API.Response;
+using softWrench.sW4.Data.Pagination;
 using softWrench.sW4.Data.Search;
+using softWrench.sW4.Metadata.Applications;
 using softWrench.sW4.Metadata.Applications.DataSet.Filter;
 
 namespace softWrench.sW4.Data.Persistence.Dataset.Commons {
 
     public class BaseLocationDataSet : MaximoApplicationDataSet {
+        private const string CteTemplate = @"classificationhierarchy{1} ( classstructureid, classificationid, parent, level )
+                                             AS
+                                             (
+                                             select	cs.classstructureid, c.classificationid, cs.parent, 0 AS Level
+                                             from	classstructure cs
+                                             inner join classification c on cs.classificationid = c.classificationid
+                                             where cs.classificationid = '{0}'
+                                             UNION ALL
+                                             select	cs.classstructureid, c.classificationid, cs.parent, Level + 1
+                                             from	classstructure cs
+                                             inner join classification c on cs.classificationid = c.classificationid
+                                             inner join classificationhierarchy{1} ch on cs.classstructureid = ch.parent
+                                             )
+                                             ";
+        private const string SelectTemplate = @"select distinct '{0}' as id, 
+	                                            SUBSTRING(
+		                                          (
+			                                        select ' / '+ch1.classificationid as [text()]
+			                                        from classificationhierarchy{1} ch1
+			                                        order by ch1.level desc
+			                                        For XML PATH ('')
+		                                          ), 3, 1000) [hierarchypath]
+                                                from classificationhierarchy{1} ch2";
         public SearchRequestDto BuildAssettransWhereClause(CompositionPreFilterFunctionParameters parameter) {
             var originalEntity = parameter.OriginalEntity;
             var location = originalEntity.GetAttribute("location");
             var siteid = originalEntity.GetAttribute("siteid");
             var orgid = originalEntity.GetAttribute("orgid");
-            
+
             var sb = new StringBuilder();
             //base section
             sb.AppendFormat(@"(assettrans.fromloc = '{0}' AND assettrans.siteid = '{1}' AND assettrans.orgid = '{2}')
@@ -21,6 +53,50 @@ namespace softWrench.sW4.Data.Persistence.Dataset.Commons {
             parameter.BASEDto.AppendWhereClause(sb.ToString());
 
             return parameter.BASEDto;
+        }
+
+        public override CompositionFetchResult GetCompositionData(ApplicationMetadata application, CompositionFetchRequest request,
+            JObject currentData) {
+
+            var result = base.GetCompositionData(application, request, currentData);
+            var locationspecList = result.ResultObject["locationspec_"].ResultList;
+            if (locationspecList.Any())
+            {
+                var classificationids =
+                    locationspecList.Select(value => value["classstructure_.classificationid"].ToString()).ToList();
+                var classhierarchys = GetClassstructureHierarchyPath(classificationids);
+                foreach (var locationspec in locationspecList)
+                {
+                    var classificationid = locationspec["classstructure_.classificationid"].ToString();
+                    var path = classhierarchys.Single(p => p["id"] == classificationid)["hierarchypath"];
+                    locationspec.Add("classstructure_.hierarchypath", path);
+                }
+            }
+            return result;
+        }
+
+        private List<Dictionary<string, string>> GetClassstructureHierarchyPath(List<string> classificationids) {
+            var queryString = GetCteQuery(classificationids);
+            var result = MaximoHibernateDAO.GetInstance().FindByNativeQuery(queryString);
+            return result;
+        }
+        private string GetCteQuery(List<string> classificationids) {
+            var cteQuery = "WITH ";
+            var selectQuery = "";
+            var queryGroup = 0;
+            foreach (var classificationid in classificationids) {
+                if (cteQuery != "WITH ") {
+                    cteQuery += ",\r\n";
+                }
+                cteQuery += CteTemplate.FormatInvariant(classificationid, queryGroup);
+                if (selectQuery != "") {
+                    selectQuery += "\r\nUNION\r\n";
+                }
+                selectQuery += SelectTemplate.FormatInvariant(classificationid, queryGroup);
+                queryGroup++;
+            }
+            var completeQuery = cteQuery + "\r\n" + selectQuery;
+            return completeQuery;
         }
 
         public override string ApplicationName() {
