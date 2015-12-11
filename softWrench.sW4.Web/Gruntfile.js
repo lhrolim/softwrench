e4863af2c9350178ef47a6efbe4929010260f660
rodrigobotti <rodrigo.botti@gmail.com>
2015-12-11 08:45:59 -0700

    bugfix: path in dev environment

--- softWrench.sW4.Web/Gruntfile.js
+++ softWrench.sW4.Web/Gruntfile.js
@@ -3,12 +3,14 @@ module.exports = function (grunt) {
 
     var path = grunt.option("path") || "";
     var customer = grunt.option("customer") || "";
+    var customerPath = (!!customer ? customer + "/" : "");
+    var fullPath = !!path ? path + "/" : "";
 
     var filesToCompile = [
         {
             expand: true,
-            cwd: "Content/Customers/" + (!!customer ? customer + "/" : ""),
-            dest: "Content/Customers/" + (!!customer ? customer + "/" : ""),
+            cwd: "Content/Customers/" + customerPath,
+            dest: "Content/Customers/" + customerPath,
             src: ["**/*.scss"],
             ext: ".css"
         },
@@ -27,8 +29,8 @@ module.exports = function (grunt) {
             ext: ".css"
         }
     ].map(function (file) {
-        file.cwd = path + "/" + file.cwd;
-        file.dest = path + "/" + file.dest;
+        file.cwd = (fullPath) + file.cwd;
+        file.dest = (fullPath) + file.dest;
         return file;
     });
 
