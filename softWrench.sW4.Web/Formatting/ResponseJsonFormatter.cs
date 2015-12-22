8ed1c1e865b8971e87e017c0ce19212734bc1918
rodrigobotti <rodrigo.botti@gmail.com>
2015-12-22 11:01:43 -0200

    [SWWEB-1935]: not serializing null fields to json

--- softWrench.sW4.Web/Formatting/ResponseJsonFormatter.cs
+++ softWrench.sW4.Web/Formatting/ResponseJsonFormatter.cs
@@ -1,6 +1,7 @@
 ﻿using cts.commons.web.Formatting;
 using Newtonsoft.Json.Serialization;
 using System.Net.Http.Formatting;
+using Newtonsoft.Json;
 
 namespace softWrench.sW4.Web.Formatting {
     public class ResponseJsonFormatter : JsonMediaTypeFormatter, ISWJsonFormatter {
@@ -8,6 +9,7 @@ namespace softWrench.sW4.Web.Formatting {
         public ResponseJsonFormatter() {
             SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             SerializerSettings.Converters.Add(new JsonDateTimeConverter());
+            SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
         }
     }
 }
