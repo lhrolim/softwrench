2cb07111220198fc69bce00d8a93b88b06500160
lhrolim <luizhenrique.rolim@gmail.com>
2016-01-05 11:53:24 -0200

    fix jstest promise

--- softWrench.sW4.Web/Content/Scripts/client/crud/crud_list.js
+++ softWrench.sW4.Web/Content/Scripts/client/crud/crud_list.js
@@ -505,7 +505,7 @@ app.directive('crudList', ["contextService", "$timeout", function (contextServic
                             printMode: false,
                             metadataid: $scope.metadataid
                         });
-                        searchPromise.success(function (data) {
+                        searchPromise.then(function (data) {
                             // Set the scroll position to the top of the new page
                             contextService.insertIntoContext("scrollto", { 'applicationName': $scope.applicationName, 'scrollTop': 0 });
                             $scope.gridRefreshed(data.data, $scope.panelid);
