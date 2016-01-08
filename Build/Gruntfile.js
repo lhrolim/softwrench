31aad8caf8060d3edf8b6530fab5e90c7a32e103
rodrigobotti <rodrigo.botti@gmail.com>
2016-01-08 12:21:28 -0200

    [SWWEB-1608]: vendor scripts through bower

--- Build/Gruntfile.js
+++ Build/Gruntfile.js
@@ -53,26 +53,31 @@ module.exports = function (grunt) {
 
         clean: {
             options: {
-                force: true
+                force: true // required to clean outside current folder
             },
             vendor: [
-                "<%= app.vendor %>" + "/css/*",
-                "<%= app.vendor %>" + "/scripts/*",
-                "<%= app.vendor %>" + "/fonts/*"
+                "<%= app.vendor %>/css/*",
+                "<%= app.vendor %>/scripts/*",
+                "<%= app.vendor %>/fonts/*"
             ],
-            tmp: ["<%= app.tmp %>"],
-            dist: ["<%= app.dist %>"]
+            tmp: [
+                "<%= app.tmp %>"
+            ],
+            dist: [
+                "<%= app.dist %>"
+            ]
         },
 
         bowercopy: {
             css: {
                 options: {
-                    destPrefix: "<%= app.vendor %>" + "/css"
+                    destPrefix: "<%= app.vendor %>/css"
                 },
                 files: {
                     "bootstrap.css": "bootstrap/dist/css/bootstrap.min.css",
                     "bootstrap-theme.css": "bootstrap/dist/css/bootstrap-theme.min.css",
                     "bootstrap-datetimepicker.css": "eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css",
+                    "textAngular.css": "textAngular/dist/textAngular.css",
                     "font-awesome.css": "font-awesome/css/font-awesome.min.css",
                     "angular-ui-select.css": "ui-select/dist/select.min.css",
                     "selectize.css": "selectize/dist/css/selectize.bootstrap3.css",
@@ -80,7 +85,7 @@ module.exports = function (grunt) {
             },
             fonts: {
                 options: {
-                    destPrefix: "<%= app.vendor %>" + "/fonts"
+                    destPrefix: "<%= app.vendor %>/fonts"
                 },
                 files: {
                     ".": [
@@ -91,17 +96,63 @@ module.exports = function (grunt) {
             },
             dev: {
                 options: {
-                    destPrefix: "<%= app.vendor %>" + "/scripts"
+                    destPrefix: "<%= app.vendor %>/scripts"
                 },
                 files: {
+                    "jquery.js": "jquery/dist/jquery.js",
+                    "jquery-ui.js": "jquery-ui/ui/jquery-ui.js",
+                    "jquery-file-style.js": "jquery.filestyle/jquery.filestyle.js",
+                    "jquery-file-download.js": "jquery-file-download/src/Scripts/jquery.fileDownload.js",
+                    "jquery-file-upload.js": "blueimp-file-upload/js/jquery.fileupload.js",
+                    "spin.js": "spin.js/spin.js",
+                    "angular-file-upload.js": "angular-file-upload/angular-file-upload.js",
+                    "lz-string.js": "lz-string/libs/lz-string.js",
+                    "modernizr.js": "modernizr-min/dist/modernizr.min.js",
+                    "angular.js": "angular/angular.js",
+                    "angular-sanitize.js": "angular-sanitize/angular-sanitize.js",
+                    "angular-strap.js": "angular-strap/dist/angular-strap.js",
+                    "angular-bindonce.js": "angular-bindonce/bindonce.js",
+                    "angular-animate.js": "angular-animate/angular-animate.js",
+                    "angular-xeditable.js": "angular-xeditable/dist/js/xeditable.js",
+                    "moment.js": "moment/src/moment.js",
+                    "moment-locale-de.js": "moment/locale/de.js",
+                    "moment-locale-es.js": "moment/locale/es.js",
+                    "bootstrap.js": "bootstrap/dist/js/bootstrap.js",
+                    "bootstrap-combobox.js": "bootstrap-combobox/js/bootstrap-combobox.js",
+                    "bootstrap-datetimepicker.js": "eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js",
+                    "bootstrap-multiselect.js": "bootstrap-multiselect/dist/js/bootstrap-multiselect.js",
+                    "bootbox.js": "bootbox.js/bootbox.js",
                 }
             },
             prod: {
                 options: {
                     clean: true,
-                    destPrefix: "<%= app.vendor %>" + "/scripts"
+                    destPrefix: "<%= app.vendor %>/scripts"
                 },
                 files: {
+                    "jquery.js": "jquery/dist/jquery.min.js",
+                    "jquery-ui.js": "jquery-ui/ui/minified/jquery-ui.min.js",
+                    "jquery-file-style.js": "jquery.filestyle/jquery.filestyle.js",
+                    "jquery-file-download.js": "jquery-file-download/src/Scripts/jquery.fileDownload.js",
+                    "jquery-file-upload.js": "blueimp-file-upload/js/jquery.fileupload.js",
+                    "spin.js": "spin.js/spin.min.js",
+                    "angular-file-upload.js": "angular-file-upload/angular-file-upload.min.js",
+                    "lz-string.js": "lz-string/libs/lz-string.min.js",
+                    "modernizr.js": "modernizr-min/dist/modernizr.min.js",
+                    "angular.js": "angular/angular.min.js",
+                    "angular-sanitize.js": "angular-sanitize/angular-sanitize.min.js",
+                    "angular-strap.js": "angular-strap/dist/angular-strap.min.js",
+                    "angular-bindonce.js": "angular-bindonce/bindonce.min.js",
+                    "angular-animate.js": "angular-animate/angular-animate.min.js",
+                    "angular-xeditable.js": "angular-xeditable/dist/js/xeditable.min.js",
+                    "moment.js": "moment/min/moment.min.js",
+                    "moment-locale-de.js": "moment/locale/de.js",
+                    "moment-locale-es.js": "moment/locale/es.js",
+                    "bootstrap.js": "bootstrap/dist/js/bootstrap.min.js",
+                    "bootstrap-combobox.js": "bootstrap-combobox/js/bootstrap-combobox.js",
+                    "bootstrap-datetimepicker.js": "eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js",
+                    "bootstrap-multiselect.js": "bootstrap-multiselect/dist/js/bootstrap-multiselect.js",
+                    "bootbox.js": "bootbox.js/bootbox.js",
                 }
             }
         }
@@ -109,11 +160,12 @@ module.exports = function (grunt) {
 
     // load npm tasks
     grunt.loadNpmTasks("grunt-sass");
-    grunt.loadNpmTasks("grunt-bowercopy");
-    grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-contrib-clean");
-    grunt.loadNpmTasks("grunt-karma");
+    grunt.loadNpmTasks("grunt-bowercopy");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-karma"); 
 
     // define default tasks
     grunt.registerTask("default", ["sass"]);
--- softWrench.sW4.Web/App_Start/WebBundleConfig.cs
+++ softWrench.sW4.Web/App_Start/WebBundleConfig.cs
@@ -21,6 +21,7 @@ namespace softWrench.sW4.Web {
                "~/Content/customVendor/css/bootstrap-multiselect.css"
                ));
 
+            // from bower fonts
             bundles.Add(new StyleBundle("~/Content/fonts").Include(
                 "~/Content/fonts/font.css"
                 ));
@@ -28,71 +29,15 @@ namespace softWrench.sW4.Web {
 
         public void PopulateScriptBundles(BundleCollection bundles) {
 
-            // from bower
+            // from bower scripts
             bundles.Add(new ScriptBundle("~/Content/vendor/scripts").Include(
                 "~/Content/vendor/scripts/*.js"
             ));
 
-            bundles.Add(new ScriptBundle("~/Content/Scripts/jquery/jquery").Include(
-                 "~/Content/Scripts/vendor/jquery/jquery-2.0.3-max.js",
-                 "~/Content/Scripts/vendor/jquery/jquery-ui-1.10.3.js",
-                 "~/Content/Scripts/vendor/jquery/jquery-file-style.js",
-                 "~/Content/Scripts/vendor/jquery/jquery-filedownload-1.2.0.js",
-                 "~/Content/Scripts/vendor/jquery/zjquery-fileupload-5.40.1.js",
-                 "~/Content/Scripts/vendor/other/spin-min.js"
-                 ));
-
-            bundles.Add(new ScriptBundle("~/Content/Scripts/thirdparty").Include(
-                "~/Content/Scripts/vendor/other/textAngular-sanitize.js",
-                "~/Content/Scripts/vendor/other/textAngular-min.js",
-                "~/Content/Scripts/vendor/other/textAngular-setup.js",
-                "~/Content/Scripts/vendor/other/jquery.scannerdetection.js",
-                "~/Content/Scripts/vendor/other/angular-fileUpload.js",
-                "~/Content/Scripts/vendor/other/selectize/selectize.js",
-                "~/Content/Scripts/vendor/other/selectize/angular-selectize.js",
-                "~/Content/Scripts/vendor/other/angular-file-dnd.js",
-                "~/Content/Scripts/vendor/other/lz-string.js")
-                //"~/Content/Scripts/vendor/other/angular-ui-select.js")
-                );
-
-            bundles.Add(new ScriptBundle("~/Content/Scripts/jqueryui").Include(
-                "~/Content/Scripts/vendor/jquery/jquery-ui-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/Content/Scripts/jqueryval").Include(
-                "~/Content/Scripts/vendor/jquery/jquery.unobtrusive*",
-                "~/Content/Scripts/vendor/jquery/query.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/Content/Scripts/modernizr").Include(
-                "~/Content/Scripts/vendor/other/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/Content/Scripts/angular/angular").Include(
-                "~/Content/Scripts/vendor/angular/angular.js",
-                "~/Content/Scripts/vendor/angular/angular-strap.js",
-                "~/Content/Scripts/vendor/angular/angular-sanitize.js",
-                "~/Content/Scripts/vendor/angular/bindonce.js",
-                "~/Content/Scripts/vendor/angular/components/*.js"
-                ));
-
-            bundles.Add(new ScriptBundle("~/Content/Scripts/twitter-bootstrap").Include(
-                "~/Content/Scripts/vendor/other/moment.js",
-                "~/Content/Scripts/vendor/bootstrap/bootstrap.max.js",
-                "~/Content/Scripts/vendor/bootstrap/bootstrap-datepicker.js",
-                "~/Content/Scripts/vendor/bootstrap/bootstrap-combobox.js",
-                "~/Content/Scripts/vendor/bootstrap/bootstrap-datetimepicker.js",
-                "~/Content/Scripts/vendor/bootstrap/bootstrap-collapse.js",
-                "~/Content/Scripts/vendor/bootstrap/bootstrap-richtext.js",
-                "~/Content/Scripts/vendor/other/bootbox.js",
-                "~/Content/Scripts/vendor/other/typeahead.js",
-                "~/Content/Scripts/vendor/moment/locales/de.js",
-                "~/Content/Scripts/vendor/moment/locales/es.js",
-                "~/Content/Scripts/vendor/bootstrap/modal.js",
-                "~/Content/Scripts/vendor/bootstrap/bootstrap-multiselect.js"
-                ));
-
-            bundles.Add(new ScriptBundle("~/Content/Scripts/ace/ace").Include(
-                "~/Content/Scripts/vendor/ace/ace.js"));
+            // customized vendor scripts
+            bundles.Add(new ScriptBundle("~/Content/vendor/scripts").Include(
+                "~/Content/customVendor/scripts/*.js"
+            ));
 
             bundles.Add(new ScriptBundle("~/Content/Scripts/client/signin").Include(
                 "~/Content/Scripts/client/signin/signin.js"
@@ -108,9 +53,7 @@ namespace softWrench.sW4.Web {
                 "~/Content/Scripts/client/directives/menu/*.js")
                 .IncludeDirectory("~/Content/Templates/commands", "*.js", true)
                 .IncludeDirectory("~/Content/modules", "*.js", true));
-
-            bundles.Add(new ScriptBundle("~/Content/Scripts/thirdparty/graphics").Include(
-                "~/Content/Scripts/vendor/other/tableau/tableau-2.0.0-min.js"));
+            
         }
 
         public static void ClearBundles() {
