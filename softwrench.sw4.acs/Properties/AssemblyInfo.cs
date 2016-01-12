f40da1600c048c4ffc05ac44e32329b79235e50b
Scott A. Matthews <scott@designsbysm.com>
2016-01-12 14:08:12 -0700

    change xerox clientkey to acs

--- softwrench.sw4.acs/Properties/AssemblyInfo.cs
+++ softwrench.sw4.acs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("softwrench.sw4.xerox")]
+[assembly: AssemblyTitle("softwrench.sw4.acs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("softwrench.sw4.xerox")]
+[assembly: AssemblyProduct("softwrench.sw4.acs")]
 [assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
