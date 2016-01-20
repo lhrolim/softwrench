e4b9e55fc0d79e2661c9a24d5fcb3f2790030596
rodrigobotti <rodrigo.botti@gmail.com>
2016-01-20 19:10:57 -0200

    [SWWEB-2007]: support for number filter -> angular not throwing number format error

---
+++ softWrench.sW4.Web/Content/Scripts/client/directives/stringtonumber.js
@@ -0,0 +1,21 @@
+﻿(function (angular) {
+    "use strict";
+
+    angular.module("sw_layout")
+    .directive("stringToNumber", function () {
+        return {
+            restrict: "A",
+            require: "ngModel",
+            link: function (scope, element, attrs, ngModel) {
+                ngModel.$parsers.push(function (value) {
+                    if (!angular.isNumber(value)) return value;
+                    return String(value);
+                });
+                ngModel.$formatters.push(function (value) {
+                    return parseInt(value, 10);
+                });
+            }
+        };
+    });
+
+})(angular);
