68e2e24bd83236eaaf013efe96f04aa50ceffd4f
Ken <pederson.it@gmail.com>
2016-01-21 11:55:29 -0700

    Add more tabs
    Add the rest of the tabs except for Safety

---
+++ softwrench.sw4.webcommons/web_content/webcommons/scripts/softwrench/util/metadata_builder.js
@@ -0,0 +1,29 @@
+﻿(function (angular) {
+    "use strict";
+
+    function entityBuilderController($scope, $http, i18NService) {
+
+        $scope.generateData = function () {
+
+            $scope.hasdata = false;
+            $scope.HasError = false;
+
+            $http.get(url("/api/generic/EntityMetadata/Build?tablename=" + $scope.table))
+                .success(function (data) {
+                    $scope.hasdata = true;
+                    $scope.metadata = data.metadata;
+                })
+                .error(function (data) {
+                    $scope.error = data.error;
+                    $scope.hasdata = true;
+                });
+        };
+
+        $scope.i18N = function (key, defaultValue, paramArray) {
+            return i18NService.get18nValue(key, defaultValue, paramArray);
+        };
+    }
+
+    angular.module('sw_layout').controller("EntityBuilderController", ["$scope", "$http", "i18NService", entityBuilderController]);
+
+})(angular);
