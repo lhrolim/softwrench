db19793a51e89c8fb4a14975d3e8e3562ca9fcef
Ken <pederson.it@gmail.com>
2016-02-05 14:32:07 -0700

    Remove unnecessary parser
    Remove unnecessary parser and add comments

--- softWrench.sW4.Web/Content/Scripts/client/crud/aaa_layout.js
+++ softWrench.sW4.Web/Content/Scripts/client/crud/aaa_layout.js
@@ -138,9 +138,6 @@ angular.module('sw_prelogin', []);
         return {
             require: 'ngModel',
             link: function (scope, element, attrs, ngModel) {
-                ngModel.$parsers.push(function (value) {
-                    return parseFloat(value, 10);
-                });
                 ngModel.$formatters.push(function (value) {
                     return '' + value;
                 });
