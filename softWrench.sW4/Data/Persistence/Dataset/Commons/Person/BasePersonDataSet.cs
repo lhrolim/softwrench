f896d64fcf773d25368b8f9e6ce8bf05120d5218
Ken <pederson.it@gmail.com>
2016-03-08 11:44:05 -0700

    Update user cache after changing current user sig
    When a user updates their user information, update the inmemoryuser
    cache with the new user info.

--- softWrench.sW4/Data/Persistence/Dataset/Commons/Person/BasePersonDataSet.cs
+++ softWrench.sW4/Data/Persistence/Dataset/Commons/Person/BasePersonDataSet.cs
@@ -152,6 +152,11 @@ namespace softWrench.sW4.Data.Persistence.Dataset.Commons.Person {
             user.UserPreferences.Signature = signature;
             user.Profiles = LoadProfiles(json);
             UserManager.SaveUser(user);
+            // Upate the in memory user if the change is for the currently logged in user
+            var currentUser = SecurityFacade.CurrentUser();
+            if (user.UserName.EqualsIc(currentUser.Login)) {
+                SecurityFacade.UpdateUserCache(user, currentUser.TimezoneOffset.ToString());
+            }
             var targetResult = Engine().Execute(operationWrapper);
             if (isCreation && isactive) {
                 _userSetupEmailService.SendActivationEmail(user, primaryEmail, passwordString);
