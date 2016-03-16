a926282856d99f276c1af4c3a907686283f1c4a1
rodrigobotti <rodrigo.botti@gmail.com>
2016-03-16 16:21:50 -0300

    [SWWEB-2099]: addressing PR comments

--- softwrench.sw4.kongsberg/classes/com/cts/kongsberg/dataset/KongsbergWorklogDataSet.cs
+++ softwrench.sw4.kongsberg/classes/com/cts/kongsberg/dataset/KongsbergWorklogDataSet.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
 using softWrench.sW4.Data.API;
 using softWrench.sW4.Data.API.Response;
 using softWrench.sW4.Data.Persistence;
@@ -9,24 +11,32 @@ using softWrench.sW4.Metadata.Security;
 namespace softwrench.sw4.kongsberg.classes.com.cts.kongsberg.dataset {
     public class KongsbergWorklogDataSet : MaximoApplicationDataSet {
 
-        private static string TOP_ATTACHMENT_URL_BY_WORKLOGID = @"select top(1) I.urlname from docinfo I
+        private static string TOP_ATTACHMENT_URL_BY_WORKLOGID = @"select I.urlname from docinfo I
 	                                                                inner join doclinks L
 		                                                                on I.docinfoid = L.docinfoid
-	                                                                where L.ownertable='WORKLOG' and L.ownerid=?
-	                                                                order by L.createdate desc";
+	                                                                where L.ownertable='WORKLOG' and L.ownerid=:OWNERID";
 
         public override ApplicationDetailResult GetApplicationDetail(ApplicationMetadata application, InMemoryUser user, DetailRequest request) {
             var result = base.GetApplicationDetail(application, user, request);
             var datamap = result.ResultObject.Fields;
 
-            var docinfourllist = MaximoHibernateDAO.GetInstance().FindByNativeQuery(TOP_ATTACHMENT_URL_BY_WORKLOGID, datamap[application.IdFieldName]);
-            if (!docinfourllist.Any()) return result;
+            var docinfourl = WorklogAttachmentUrl(datamap[application.IdFieldName]);
 
-            var docinfourl = docinfourllist.First()["urlname"];
+            if (docinfourl == null) return result;
+
             var fileUrl = AttachmentHandler.GetFileUrl(docinfourl);
             datamap.Add("attachment_url", fileUrl);
 
             return result;
+        }
+
+        private string WorklogAttachmentUrl(object worklogid) {
+            var parameters = new ExpandoObject();
+            var parameterCollection = (ICollection<KeyValuePair<string, object>>)parameters;
+            parameterCollection.Add(new KeyValuePair<string, object>("OWNERID", worklogid));
+
+            var docinfourllist = MaxDAO.FindByNativeQuery(TOP_ATTACHMENT_URL_BY_WORKLOGID, parameters, new PaginationData(1, 1, "L.createdate desc"));
+            return docinfourllist.Any() ? docinfourllist.First().urlname : null;
         }
 
         public override string ApplicationName() {
