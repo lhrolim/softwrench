f61725546f9d9a7d5aa2ce617a47c1fd0009cd3e
Ken <pederson.it@gmail.com>
2016-04-05 04:20:43 -0700

    SWWEB-2291
    * Remove hidedescription use
    
    Remove hide description use to show descriptions.
    Update class structure type provider for umc so that it includes the
    description for the child class.
    
    * Update Web.config

--- softwrench.sw4.umc/classes/com/cts/umc/dataset/UmcWorkorderDataSet.cs
+++ softwrench.sw4.umc/classes/com/cts/umc/dataset/UmcWorkorderDataSet.cs
@@ -1,63 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using cts.commons.persistence;
 using softwrench.sw4.Shared2.Data.Association;
-using softWrench.sW4.Data.Search;
 using softWrench.sW4.Metadata.Applications.DataSet;
-using softWrench.sW4.Metadata.Applications.DataSet.Filter;
 using softWrench.sW4.Util;
+using softWrench.sW4.Data.Persistence.Dataset.Commons.Ticket;
 
-namespace softWrench.sW4.Data.Persistence.Dataset.Commons.Ticket {
-    public class BaseTicketDataSet : MaximoApplicationDataSet {
+namespace softWrench.sW4.umc.classes.com.cts.umc.dataset {
+    public class UmcWorkorderDataSet : BaseWorkorderDataSet {
 
-
-        /*private const string Base = "(( DOCLINKS.ownerid = '{0}' ) AND ( UPPER(COALESCE(DOCLINKS.ownertable,'')) = '{0}' )  ";*/
-
-        //WAPPR -> Pode mudar para todos os outros. Sem restrições de edição na info da WO.
-        //APPR -> Pode mudar para todos os outros. Sem restrições de edição na info da WO (por enquanto).
-        //COMP -> Pode apenas mudar para CLOSE. Sem restrições de edição na info da WO.
-        //CLOSE-> Não dá para editar mais nenhuma info da WO.
-        //CAN -> Não dá para editar mais nenhuma info da WO.
-        public IEnumerable<IAssociationOption> FilterAvailableStatus(AssociationPostFilterFunctionParameters postFilter) {
-            var metadataParameters = ContextLookuper.LookupContext().MetadataParameters;
-            string currentStatus = null;
-
-            if (postFilter.OriginalEntity.ContainsAttribute("status")) {
-                currentStatus = postFilter.OriginalEntity.GetAttribute("status").ToString();
-            } else if (metadataParameters.ContainsKey("currentstatus")) {
-                currentStatus = metadataParameters["currentstatus"].ToString();
-            }
-            var filterAvailableStatus = postFilter.Options;
-            if (currentStatus == null) {
-                return new List<IAssociationOption> { filterAvailableStatus.First(l => l.Value == "OPEN") };
-            }
-            var currentOption = filterAvailableStatus.FirstOrDefault(l => l.Value == currentStatus);
-            if (currentOption == null) {
-                return filterAvailableStatus;
-            }
-
-            if (currentStatus == "APPR" || currentStatus == "WAPPR") {
-                return filterAvailableStatus;
-            }
-            if (currentStatus == "COMP") {
-                return new List<IAssociationOption> { currentOption, filterAvailableStatus.First(l => l.Value == "CLOSE") };
-            }
-            return new List<IAssociationOption> { currentOption };
+        public UmcWorkorderDataSet(ISWDBHibernateDAO swdbDao) : base(swdbDao) {
+            
         }
 
-
-        public SearchRequestDto FilterAssets(AssociationPreFilterFunctionParameters parameters) {
-            var filter = parameters.BASEDto;
-            var location = (string)parameters.OriginalEntity.GetAttribute("location");
-            if (location == null) {
-                return filter;
-            }
-            filter.AppendSearchEntry("asset.location", location.ToUpper());
-            return filter;
-        }
-
-        protected virtual IEnumerable<IAssociationOption> GetClassStructureType(OptionFieldProviderParameters parameters, string ticketclass, string searchString = null) {
+        protected override IEnumerable<IAssociationOption> GetClassStructureType(OptionFieldProviderParameters parameters, string ticketclass, string searchString = null) {
 
             // TODO: Change the design to use a tree view component
             var query = BuildQuery(parameters, ticketclass, searchString);
@@ -70,19 +26,20 @@ namespace softWrench.sW4.Data.Persistence.Dataset.Commons.Ticket {
                         record["CLASS_4"] == null ? "" : record["CLASS_4"] + "/",
                         record["CLASS_3"] == null ? "" : record["CLASS_3"] + "/",
                         record["CLASS_2"] == null ? "" : record["CLASS_2"] + "/",
-                        record["CLASS_1"] ?? "");
+                        record["CLASS_1"] == null ? "" : record["CLASS_1"] + " (" + record["DESCRIPTION_1"] + ")");
                     return new AssociationOption(record["ID"], label);
                 });
         }
 
-        protected virtual string BuildQuery(OptionFieldProviderParameters parameters, string ticketclass, string searchString = null) {
+        protected override string BuildQuery(OptionFieldProviderParameters parameters, string ticketclass, string searchString = null) {
             
             var classStructureQuery = string.Format(@"SELECT  c.classstructureid AS ID, 
                                                                p3.classificationid AS CLASS_5, 
                                                                p2.classificationid AS CLASS_4,  
                                                                p1.classificationid AS CLASS_3, 
                                                                p.classificationid  AS CLASS_2, 
-                                                               c.classificationid  AS CLASS_1
+                                                               c.classificationid  AS CLASS_1,
+                                                               c.description AS DESCRIPTION_1
                                                        FROM classstructure {3} c
                                                        left join classstructure {3} p on p.classstructureid = c.parent
                                                        left join classstructure {3} p1 on p1.classstructureid = p.parent
@@ -112,6 +69,13 @@ namespace softWrench.sW4.Data.Persistence.Dataset.Commons.Ticket {
             return classStructureQuery;
         }
 
+        public override string ApplicationName() {
+            return "workorder";
+        }
+
+        public override string ClientFilter() {
+            return "umc";
+        }
 
     }
 }
