20499b8f651ed29c056dd185f3f4b4729b3f7690
rodrigobotti <rodrigo.botti@gmail.com>
2016-04-08 16:13:47 -0300

    migration bugfix: sw_user2.maximopersonid=username if maximopersonid is null

--- softwrench.sw4.user/Migration/MigrationSWWEB2356.cs
+++ softwrench.sw4.user/Migration/MigrationSWWEB2356.cs
@@ -5,14 +5,15 @@ namespace softwrench.sw4.user.Migration {
     [Migration(201604061830)]
     public class MigrationSwweb2356 : FluentMigrator.Migration {
 
-        public override void Up()
-        {
+        public override void Up() {
             Create.Column("systemuser").OnTable("SW_USER2").AsBoolean().WithDefaultValue(false);
             
             Update.Table("SW_USER2").Set(new { maximopersonid = "swadmin" }).Where(new { username = "swadmin" });
             Update.Table("SW_USER2").Set(new { maximopersonid = "swjobuser" }).Where(new { username = "swjobuser" });
             Update.Table("SW_USER2").Set(new {systemuser = true}).Where(new { username ="swadmin"});
             Update.Table("SW_USER2").Set(new {systemuser = true}).Where(new { username ="swjobuser"});
+
+            Execute.Sql("update sw_user2 set maximopersonid=username where maximopersonid is null");
 
             Create.UniqueConstraint("SW_MAXPERSON_UQ").OnTable("SW_USER2").Column("MAXIMOPERSONID");
         }
