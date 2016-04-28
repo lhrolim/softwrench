bb1b5ee034b1a62ee1a5ecf25681425180a9e2e8
lhrolim <luizhenrique.rolim@gmail.com>
2016-04-28 16:30:06 -0300

    SWWEB-2431 fix

--- softwrench.sw4.firstsolar/web_content/firstsolar/scripts/advancedSearchService.js
+++ softwrench.sw4.firstsolar/web_content/firstsolar/scripts/advancedSearchService.js
@@ -185,6 +185,7 @@
                         }
 
                         deferred.resolve(workorderInitialDataMap);
+                        modalService.hide();
                     }
                 });
 
