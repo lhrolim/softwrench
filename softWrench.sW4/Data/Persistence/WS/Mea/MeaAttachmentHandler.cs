5bcd1d148dac86fce7974e402c4aaeff58987bca
lhrolim <luizhenrique.rolim@gmail.com>
2016-05-03 22:17:36 -0300

    fixing compilation issue

--- softWrench.sW4/Data/Persistence/WS/Mea/MeaAttachmentHandler.cs
+++ softWrench.sW4/Data/Persistence/WS/Mea/MeaAttachmentHandler.cs
@@ -4,11 +4,13 @@ using softWrench.sW4.Data.Persistence.WS.Internal;
 using softWrench.sW4.Metadata;
 using softWrench.sW4.Util;
 using System.IO;
+using softWrench.sW4.Data.Persistence.Dataset.Commons;
 using softWrench.sW4.Data.Persistence.WS.Applications.Compositions;
 
 namespace softWrench.sW4.Data.Persistence.WS.Mea {
     public class MeaAttachmentHandler : AttachmentHandler {
-        public MeaAttachmentHandler(MaximoHibernateDAO maxDAO) : base(maxDAO) {
+        public MeaAttachmentHandler(MaximoHibernateDAO maxDAO, DataSetProvider dataSetProvider) : base(maxDAO, dataSetProvider)
+        {
         }
 
         /// <summary>
