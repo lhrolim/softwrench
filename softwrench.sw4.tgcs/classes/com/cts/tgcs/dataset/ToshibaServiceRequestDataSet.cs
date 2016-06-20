6fa32f1ceca11393d5063ea856698072a60983ee
Ken <pederson.it@gmail.com>
2016-06-20 09:10:59 -0700

    SWWEB-2520 (#1266)
    * Add custom quick SR to toshiba
    
    * Add thirdparty ticket to tgcs SR
    
    * Add thirdparty composition to tgcs quick sr
    
    * Finish thirdparty tgcs qsr composition

---
+++ softwrench.sw4.tgcs/classes/com/cts/tgcs/dataset/ToshibaServiceRequestDataSet.cs
@@ -0,0 +1,26 @@
+﻿using cts.commons.persistence;
+using softWrench.sW4.Data.Persistence.Dataset.Commons.Ticket;
+using softWrench.sW4.Data.Search;
+using softWrench.sW4.Metadata.Applications.DataSet.Filter;
+
+namespace softwrench.sw4.tgcs.classes.com.cts.tgcs.dataset {
+    class ToshibaServiceRequestDataSet : BaseServiceRequestDataSet {
+        private readonly IMaximoHibernateDAO _maximoDao;
+        public ToshibaServiceRequestDataSet(ISWDBHibernateDAO swdbDao, IMaximoHibernateDAO maximoDao) : base(swdbDao) {
+            _maximoDao = maximoDao;
+        }
+
+        public SearchRequestDto FilterQSRWorklogs(CompositionPreFilterFunctionParameters parameter) {
+            parameter.BASEDto.AppendWhereClause(" clientviewable = 1 ");
+            return parameter.BASEDto;
+        }
+
+        public override string ApplicationName() {
+            return "servicerequest,quickservicerequest";
+        }
+
+        public override string ClientFilter() {
+            return "tgcs";
+        }
+    }
+}
---
+++ softwrench.sw4.tgcs/web_content/tgcs/scripts/toshibasr_service.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    angular
+      .module('sw_layout')
+      .factory('toshibasrService', ['$q', 'searchService', chicagosrService]);
+
+    function chicagosrService($q, searchService) {
+        var service = {
+            afterChangeAffectedPerson: afterChangeAffectedPerson
+        };
+
+        function afterChangeAffectedPerson(event) {
+            var datamap = event.fields;
+            var searchData = {
+                personid: datamap['affectedperson'],
+                isprimary: '1'
+            };
+            var operation = searchService.getSearchOperationById("EQ");
+            var extraparams = {
+                searchOperators: {
+                    "personid": operation,
+                    "isprimary": operation
+                }
+            }
+            return $q.all([
+                searchService.searchWithData("email", searchData, "list", extraparams),
+                searchService.searchWithData("phone", searchData, "list", extraparams)
+            ]).then(function (result) {
+                var emailResult = result[0].data.resultObject[0];
+                datamap['affectedemail'] = emailResult ? emailResult.fields['emailaddress'] : '';
+                var phoneResult = result[1].data.resultObject[0];
+                datamap['affectedphone'] = phoneResult ? phoneResult.fields['phonenum'] : '';
+            });
+        };
+
+        return service;
+    }
+})();
