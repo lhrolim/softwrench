618422373e128e0827696f9489efe18ca49bd4f2
rodrigobotti <rodrigo.botti@gmail.com>
2016-07-01 14:54:20 -0500

    bugfix: create new dashboard as swadmin

--- softwrench.sw4.dashboard/classes/controller/DashboardDataSet.cs
+++ softwrench.sw4.dashboard/classes/controller/DashboardDataSet.cs
@@ -2,7 +2,9 @@
 using System.Linq;
 using softwrench.sw4.dashboard.classes.model.entities;
 using softwrench.sw4.Shared2.Data.Association;
+using softwrench.sW4.Shared2.Metadata.Applications;
 using softWrench.sW4.Data.Persistence.Dataset.Commons;
+using softWrench.sW4.Metadata;
 using softWrench.sW4.Metadata.Applications.DataSet;
 using softWrench.sW4.Security.Services;
 
@@ -13,9 +15,12 @@ namespace softwrench.sw4.dashboard.classes.controller {
         public IEnumerable<IAssociationOption> GetExistingDashboards(OptionFieldProviderParameters parameters) {
             var user = SecurityFacade.CurrentUser();
             var profiles = user.Profiles;
-            var applications = user.MergedUserProfile.Permissions.Where(p => !p.HasNoPermissions).Select(p => p.ApplicationName).ToArray();
+            var applications = user.IsSwAdmin()
+               // swadmin.MergedUserProfile has empty Permissions
+               ? MetadataProvider.FetchSecuredTopLevelApps(ClientPlatform.Web, user).Select(a => a.ApplicationName)
+               : user.MergedUserProfile.Permissions.Where(p => !p.HasNoPermissions).Select(p => p.ApplicationName);
 
-            var list = SWDAO.FindByQuery<Dashboard>(Dashboard.ByUserAndApplications(profiles.Select(s => s.Id), true), user.UserId, applications);
+            var list = SWDAO.FindByQuery<Dashboard>(Dashboard.ByUserAndApplications(profiles.Select(s => s.Id), true), user.UserId, applications.ToList());
             var options = new List<MultiValueAssociationOption>();
             foreach (var dashboard in list) {
                 var label = dashboard.Active ? dashboard.Title : dashboard.Title + " (INACTIVE)";
