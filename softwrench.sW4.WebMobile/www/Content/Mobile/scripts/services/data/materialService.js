31f9b0b447be1e52c1b13e8f0348488474e6bbff
rodrigobotti <rodrigo.botti@gmail.com>
2016-07-01 14:41:04 -0500

    [SWOFF-187]+[SWOFF-177]: metadata+initial client-side

---
+++ softwrench.sW4.WebMobile/www/Content/Mobile/scripts/services/data/materialService.js
@@ -0,0 +1,52 @@
+﻿(function (angular, _) {
+    "use strict";
+
+    function materialService(dao, associationConstants, offlineSchemaService) {
+        //#region Utils
+
+        //#endregion
+
+        //#region Public methods
+
+        function itemSelected(event) {
+            const schema = event.schema;
+            const datamap = event.datamap;
+            const value = event.newValue;
+            datamap["description"] = datamap["item_.description"];
+            dao.findByQuery("AssociationData", `application='offlineinvbalances' and datamap like '%"itemnum":"${value}"%'`)
+                .then(r => {
+                    // set options for storeroom
+                    const options = _.chain(r)
+                        .map(e => e.datamap)
+                        .groupBy("location")
+                        .pairs()
+                        .map(kv => {
+                            const [storeloc, invbals] = kv;
+                            const bins = invbals.map(i => _.omit(i, ["location"]));
+                            return new associationConstants.Option(storeloc, storeloc, { bins });
+                        })
+                        .value();
+
+                    const storeRoomField = offlineSchemaService.getFieldByAttribute(schema, "storeloc");
+                    storeRoomField.options = options;
+                });
+        }
+
+        //#endregion
+
+        //#region Service Instance
+        const service = {
+            itemSelected,
+        };
+        return service;
+        //#endregion
+    }
+
+    //#region Service registration
+
+    angular.module("sw_mobile_services")
+        .factory("materialService", ["swdbDAO", "associationConstants", "offlineSchemaService", materialService]);
+
+    //#endregion
+
+})(angular, _);
