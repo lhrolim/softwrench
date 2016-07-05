8d5554679607a52dc7c5dd879453ebb8931f5be7
nitinmanjukogt <nitin.manju@kogt.kongsberg.com>
2016-07-05 22:54:05 +0530

    Swweb 2604 (#1307)
    * increasing the column capacity holding the metadata info in the metadataeditor table.
    
    * handling the Sqlserver migration.

---
+++ softWrench.sW4.Web/DB_Migration/5.9.1/20160629_SWWEB_2604.cs
@@ -0,0 +1,17 @@
+﻿using cts.commons.persistence.Util;
+using FluentMigrator;
+
+namespace softWrench.sW4.Web.DB_Migration._5._9._0 {
+    [Migration(201606291631)]
+    public class MigrationSwweb2604 : Migration {
+        public override void Up() {
+            IfDatabase("MySql").Execute.Sql("ALTER TABLE sw_metadataeditor MODIFY COLUMN metadata MEDIUMBLOB");
+
+            IfDatabase("SqlServer").Execute.Sql("ALTER TABLE sw_metadataeditor ALTER COLUMN metadata varbinary(max)");
+        }
+
+        public override void Down() {
+            
+        }
+    }
+}
