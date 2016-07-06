342186fadb6b7474d3495213633250cc68539247
rodrigobotti <rodrigo.botti@gmail.com>
2016-07-06 12:37:00 -0500

    bugfix: application state problem count

--- softwrench.sW4.WebMobile/www/Content/Mobile/scripts/services/application/applicationstateservice.js
+++ softwrench.sW4.WebMobile/www/Content/Mobile/scripts/services/application/applicationstateservice.js
@@ -9,24 +9,13 @@
         
         const countPending = app => swdbDAO.countByQuery("DataEntry", `application='${app}' and pending = 1`);
 
-        const dirtyAndProblematicCount = app => 
-            swdbDAO.findByQuery("DataEntry", `application='${app}' and isDirty=1`)
-                // all dirty entries
-                .then( entries => {
-                    // no dirty entries means no problematic entries
-                    if (!entries || entries.length <= 0) {
-                        return { dirty: 0, problematic: 0 };
-                    }
+        const countDirty = app => swdbDAO.countByQuery("DataEntry", `application='${app}' and isDirty=1`);
 
-                    const ids = entries.map(entry => `'${entry.id}'`);
-
-                    return swdbDAO.countByQuery("BatchItem", `dataentry in (${ids}) and problem is not null`)
-                        // count of problematic batches related to the entries
-                        .then(problematic => {
-                            const justDirty = entries.length - problematic;
-                            return { dirty: justDirty, problematic: problematic };
-                        });
-                });
+        const countProblematic = app =>
+            swdbDAO.executeStatement(`select count(d.id) from DataEntry d where d.application=? and d.isDirty=1 and
+                                    (select count(b.id) from BatchItem b where b.dataentry=d.id and problem is not null) > 0`,
+                                    [app])
+                .then(r => !r ? 0 : r[0]["count(d.id)"]);
         
 
         //#endregion
@@ -40,7 +29,7 @@
          *  application: String, // application's name 
          *  all: Integer, // count of all entities
          *  pending: Integer, // count of pending entities 
-         *  dirty: Integer, // count of all dirty entities
+         *  dirty: Integer, // count of all dirty entities (just dirty, not including problematic)
          *  problematic: Integer, // count of problematic entities 
          * }
          * 
@@ -52,24 +41,21 @@
                     const apps = results.map(app => app.application);
 
                     const promises = apps.map(app => {
-                        const countPromises = [];
+                        const countPromises = [
+                            countAll(app), // all
+                            countPending(app), // pending
+                            countDirty(app), // dirty
+                            countProblematic(app) // problematic
+                        ];
 
-                        //TODO: verify (then fix) why using swdbdao made the first 2 queries be overriden by "application='<app>' and isDirty=1"
-                        //var basequery = "application='{0}'".format(app);
-                        //countPromises.push(swdbDAO.countByQuery("DataEntry", basequery)); // all
-                        //countPromises.push(swdbDAO.countByQuery("DataEntry", basequery + " and isPending=1")); // pending
-
-                        countPromises.push(countAll(app)); // all
-                        countPromises.push(countPending(app)); // pending
-                        countPromises.push(dirtyAndProblematicCount(app)); // dirty + problematic -> because problematic=dirty+batchitem.problem!==null
-
-                        return $q.all(countPromises).then(counts => ({
-                            application: app,
-                            all: counts[0],
-                            pending: counts[1],
-                            dirty: counts[2].dirty,
-                            problematic: counts[2].problematic
-                        }));
+                        return $q.all(countPromises)
+                            .spread((all, pending, dirty, problematic) => ({
+                                application: app,
+                                all,
+                                pending,
+                                dirty: dirty - problematic, // just dirty
+                                problematic
+                            }));
                     });
                     return $q.all(promises);
                 });
@@ -83,17 +69,12 @@
         function getAppConfig() {
             const serverConfigPromise = configurationService.getConfig("serverconfig");
             const clientVersionPromise = isRippleEmulator() ? $q.when("Ripple") : $cordovaAppVersion.getVersionNumber();
-            return $q.all([serverConfigPromise, clientVersionPromise]).then(results => {
-                const serverConfig = results[0];
-                const appVersion = results[1];
-                const config = {
+            return $q.all([serverConfigPromise, clientVersionPromise])
+                .spread((serverConfig, appVersion) => ({
                     'server': serverConfig,
-                    'client': {
-                        'version': appVersion
-                    }
-                };
-                return config;
-            });
+                    'client': { 'version': appVersion }
+                })
+            );
         }
 
         //#endregion
