5e33a3cd1e86bbbffd1a0addf79f3ec6376ef7c8
rodrigobotti <rodrigo.botti@gmail.com>
2016-07-11 14:40:33 -0700

    [SWOFF-175]: working materials.ITEM tab

--- softwrench.sW4.WebMobile/www/Content/Mobile/scripts/constants/associationconstants.js
+++ softwrench.sW4.WebMobile/www/Content/Mobile/scripts/constants/associationconstants.js
@@ -2,15 +2,13 @@
     "use strict";
 
     angular.module("sw_mobile_services").constant("associationConstants", {
-
         Option: class {
           constructor (value, label, extra) {
               this.value = value;
-              this.label = label;
+              this.label = label || this.value;
               this.extrafields = extra;
           }  
-        },
-
+        }
     });
 
 })(angular);
--- softwrench.sW4.WebMobile/www/Content/Mobile/scripts/services/data/materialService.js
+++ softwrench.sW4.WebMobile/www/Content/Mobile/scripts/services/data/materialService.js
@@ -1,20 +1,10 @@
 ﻿(function (angular, _) {
     "use strict";
 
-    function materialService(dao, associationConstants, offlineSchemaService, offlineAssociationService) {
+    function materialService(dao, associationConstants, offlineSchemaService, offlineAssociationService, swAlertPopup) {
         //#region Utils
 
-        function provideOptionList(schema, datamap, list, parentKey, target, extraChildrenFieldName) {
-            const options = _.chain(list)
-                        .groupBy(parentKey) // grouping by parent key (denormalized list, has repetitions of locations and binnums)
-                        .pairs()
-                        .map(kv => {
-                            const [key, children] = kv;
-                            const childrenList = children.map(i => _.omit(i, [parentKey]));
-                            return new associationConstants.Option(key, key, { [extraChildrenFieldName]: childrenList });
-                        })
-                        .value();
-
+        function setOptionList(schema, datamap, target, options) {
             const targetField = offlineSchemaService.getFieldByAttribute(schema, target);
             targetField.options = options; // providing options
 
@@ -25,34 +15,15 @@
             }
         }
 
-        const numberValue = value => !value ? 0 : parseFloat(value.replace(",", "."));
-
-        const setItemValues = (datamap, values) => {
-            datamap["curbal"] = numberValue(values["curbal"]);
-            datamap["physcnt"] = numberValue(values["physcnt"]);
-            datamap["itemsetid"] = values["itemsetid"];
-            datamap["conditioncode"] = values["conditioncode"];
-        };
-
-        const cleanItemData = (datamap, itemSelected) => {
-            const numberValue = itemSelected ? 0 : null;
-            datamap["storeloc"] = "null$ignorewatch";
-            datamap["binnum"] = "null$ignorewatch";
-            datamap["lotnum"] = "null$ignorewatch";
-            datamap["storeloc_.#bins"] = null;
-            datamap["binnum_.#lots"] = null;
-            datamap["lotnum_.#itemvalues"] = null;
-            datamap["curbal"] = numberValue;
-            datamap["physcnt"] = numberValue;
-            datamap["itemsetid"] = null;
-            datamap["conditioncode"] = null;
+        const cleanItemData = (datamap) => {
             datamap["item_.description"] = null;
+            datamap["#description"] = null;
         }
 
         const cleanMaterialData = (datamap, materialSelected) => {
-            const numberValue = materialSelected ? 0 : null;
+            const nullNumberValue = materialSelected ? 0 : null;
             datamap["description"] = null;
-            datamap["unitcost"] = numberValue;
+            datamap["unitcost"] = nullNumberValue;
         }
 
         //#endregion
@@ -62,7 +33,7 @@
         function lineSelected(event) {
             const datamap = event.datamap;
             const linetype = datamap.linetype;
-            cleanItemData(datamap, linetype === "ITEM");
+            cleanItemData(datamap);
             cleanMaterialData(datamap, linetype === "MATERIAL");
         }
 
@@ -71,50 +42,17 @@
             const datamap = event.datamap;
             const value = event.newValue;
             datamap["#description"] = datamap["item_.description"];
-            
-            // cleanup
-            cleanItemData(datamap, true);
 
             if (!value) return;
 
-            // provide option list of storerooms for the selected item
-            dao.findByQuery("AssociationData", `application='offlineinvbalances' and datamap like '%"itemnum":"${value}"%'`)
-                .then(r => 
-                    provideOptionList(schema, datamap, r.map(e => e.datamap), "location", "storeloc", "#bins"));
-        }
-
-        function storeRoomSelected(event) {
-            const value = event.newValue;
-            if (!value) return;
-            const schema = event.schema;
-            const datamap = event.datamap;
-            const bins = datamap["storeloc_.#bins"];
-            if(!bins) return;
-            // provide option list of bins
-            provideOptionList(schema, datamap, bins, "binnum", "binnum", "#lots");
-        }
-
-        function binSelected(event) {
-            const schema = event.schema;
-            const datamap = event.datamap;
-            const lots = datamap["binnum_.#lots"];
-            if (!lots) return;
-            // provide list of lots
-            const lotnumField = offlineSchemaService.getFieldByAttribute(schema, "lotnum");
-            const options = lots.map(l => new associationConstants.Option(l.lotnum, l.lotnum, { "#itemvalues": l }));
-            if (options.length === 1) { // selecting single option
-                const selectedOption = options[0];
-                offlineAssociationService.updateExtraProjectionsForOptionField(selectedOption, lotnumField.associationKey);
-                setItemValues(datamap, selectedOption.extrafields["#itemvalues"]);
-                datamap["lotnum"] = `${selectedOption.value}$ignorewatch`;
-            }
-        }
-
-        function lotSelected(event) {
-            const datamap = event.datamap;
-            const itemvalues = datamap["lotnum_.#itemvalues"];
-            if (!itemvalues) return;
-            setItemValues(datamap, itemvalues);
+            return dao.findByQuery("AssociationData", `application='offlineinventory' and datamap like '%"itemnum":"${value}"%'`)
+                .then(r => {
+                    if (!r || r.length <= 0) {
+                        return swAlertPopup.show({ title: "Item not available", template: "This item is not available in the inventory.<br>Please select a different one" }, 3000);
+                    }
+                    const options = r.map(a => new associationConstants.Option(a.datamap["location"]));
+                    setOptionList(schema, datamap, "storeloc", options);
+                });
         }
 
         //#endregion
@@ -122,10 +60,7 @@
         //#region Service Instance
         const service = {
             lineSelected,
-            itemSelected,
-            storeRoomSelected,
-            binSelected,
-            lotSelected
+            itemSelected
         };
         return service;
         //#endregion
@@ -134,7 +69,7 @@
     //#region Service registration
 
     angular.module("sw_mobile_services")
-        .factory("materialService", ["swdbDAO", "associationConstants", "offlineSchemaService", "offlineAssociationService", materialService]);
+        .factory("materialService", ["swdbDAO", "associationConstants", "offlineSchemaService", "offlineAssociationService", "swAlertPopup", materialService]);
 
     //#endregion
 
