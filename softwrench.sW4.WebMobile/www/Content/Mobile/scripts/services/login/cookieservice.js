c5072244ac813809ab0c75b0f5ca14c5b4200148
rodrigobotti <rodrigo.botti@gmail.com>
2016-07-25 16:01:17 -0700

    [SWOFF-206]: initial cordova-master-cookie test
    (cherry picked from commit 76662601b211ef6ef796fc82af09376383b25a14)

---
+++ softwrench.sW4.WebMobile/www/Content/Mobile/scripts/services/login/cookieservice.js
@@ -0,0 +1,70 @@
+﻿(function (angular) {
+    "use strict";
+
+    function cookieService($q, dao, settingsService) {
+        //#region Utils
+
+        //#region cordova-cookie-master plugin adapter promisefying methods
+        class CordovaCookieMasterAdapter {
+            constructor(cookieMaster, q) {
+                this.cookieMaster = cookieMaster;
+                this.$q = q;
+            }
+            getCookieValue(url, name) {
+                const deferred = this.$q.defer();
+                this.cookieMaster.getCookieValue(url, name, cookie => deferred.resolve(cookie.cookieValue), error => deferred.reject(error));
+                return deferred.promise;
+            }
+            setCookieValue(url, name, value) {
+                const deferred = this.$q.defer();
+                this.cookieMaster.setCookieValue(url, name, value, () => deferred.resolve(), error => deferred.reject(error));
+                return deferred.promise;
+            }
+            clearCookies() {
+                const deferred = this.$q.defer();
+                this.cookieMaster.clearCookies(() => deferred.resolve(), error => deferred.reject(error));
+                return deferred.promise;
+            }
+        }
+        //#endregion
+
+        const $cookie = new CordovaCookieMasterAdapter(window.cookieMaster, $q);
+        const config = {
+            cookieName: "swcookie"
+        }
+
+        //#endregion
+
+        //#region Public methods
+
+        function getWebCookie() {
+            return settingsService.getServerUrl(url => $cookie.getCookieValue(url, config.cookieName));
+        }
+
+        function setWebCookie(value) {
+            return settingsService.getServerUrl(url => $cookie.setCookieValue(url, config.cookieName, value));
+        }
+
+        function clearCookies() {
+            return $cookie.clearCookies();
+        }
+
+        //#endregion
+
+        //#region Service Instance
+        const service = {
+            getWebCookie,
+            setWebCookie,
+            clearCookies
+        };
+        return service;
+        //#endregion
+    }
+
+    //#region Service registration
+
+    angular.module("sw_mobile_services").factory("cookieService", ["$q", "swdbDAO", "settingsService", cookieService]);
+
+    //#endregion
+
+})(angular);
