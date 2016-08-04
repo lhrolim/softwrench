15f45c1222623fd4eabf25a3ffc06caf00e4e294
Rodrigo Botti <rodrigo.botti@gmail.com>
2016-08-03 23:52:25 -0300

    [SWOFF-212] FS Materials Requirements (#1362)
    * [SWOFF-212]: FS material requirements (except warranty)
    
    * [SWOFF-212]: metadata for matusetrans.warranty

--- softwrench.sW4.WebMobile/www/Content/Mobile/scripts/services/data/materialService.js
+++ softwrench.sW4.WebMobile/www/Content/Mobile/scripts/services/data/materialService.js
@@ -1,23 +1,13 @@
 ﻿(function (angular, _) {
     "use strict";
 
-    function materialService(dao, associationConstants, offlineSchemaService, offlineAssociationService, swAlertPopup) {
+    function materialService() {
         //#region Utils
 
-        function setOptionList(schema, datamap, target, options) {
-            const targetField = offlineSchemaService.getFieldByAttribute(schema, target);
-            targetField.options = options; // providing options
-
-            if (options.length === 1) { // only a single option: select it -> expose extrafields and set value 
-                const selectedOption = options[0];
-                offlineAssociationService.updateExtraProjectionsForOptionField(selectedOption, targetField.associationKey);
-                datamap[target] = selectedOption.value;
-            }
-        }
-
         const cleanItemData = (datamap) => {
-            datamap["item_.description"] = null;
+            datamap["offlineitem_.description"] = null;
             datamap["#description"] = null;
+            datamap["description"] = null;
         }
 
         const cleanMaterialData = (datamap, materialSelected) => {
@@ -30,45 +20,60 @@
 
         //#region Public methods
 
+        /**
+         * @returns {String} whereclause that filters locations that are storerooms 
+         */
+        const getStoreRoomWhereClause = () =>
+            "textindex02='STOREROOM' and textindex03='OPERATING'";
+
+        /**
+         * @returns {String} whereclause that filters items that are stocked in the selected storeroom 
+         */
+        const getAvailableItemsWhereClause = () =>
+            "textindex01 in (select textindex01 from AssociationData where application='offlineinventory' and textindex02=@storeloc)";
+        
+        /**
+         * Clears datamap.
+         * 
+         * @param {events.afterchange} event 
+         */
         function lineSelected(event) {
             const datamap = event.datamap;
             const linetype = datamap.linetype;
             cleanItemData(datamap);
+            datamap["storeloc"] = "null$ignorewatch";
+            datamap["itemnum"] = "null$ignorewatch";
             cleanMaterialData(datamap, linetype === "MATERIAL");
+            datamap["qtyrequested"] = 1;
         }
 
+        /**
+         * Clear item data.
+         * 
+         * @param {events.afterchange} event 
+         */
+        function storeRoomSelected(event) {
+            const datamap = event.datamap;
+            if (!datamap["itemnum"]) return;
+            datamap["itemnum"] = "null$ignorewatch";
+            cleanItemData(datamap);
+        }
+
+        /**
+         * Sets description from selected item.
+         * 
+         * @param {events.afterchange} event 
+         */
         function itemSelected(event) {
-            const schema = event.schema;
             const datamap = event.datamap;
             const value = event.newValue;
-
             if (!value) {
                 cleanItemData(datamap);
                 return;
             }
-
-            datamap["#description"] = datamap["item_.description"];
-            return dao.findByQuery("AssociationData", `application='offlineinventory' and datamap like '%"itemnum":"${value}"%'`)
-                .then(r => {
-                    if (!r || r.length <= 0) {
-                        // should not happen: server whereclauses should only bring items that have inventory entries associated with it
-                        return swAlertPopup.show({
-                            title: "Item not available",
-                            template: "This item is not available in the inventory.<br>Please select a different one"
-                        }, 3000)
-                        .then(() => {
-                            cleanItemData(datamap);
-                            datamap["itemnum"] = "null$ignorewatch";
-                        });
-                    }
-                    const options = _.chain(r)
-                        .map(a => a.datamap["location"])
-                        .uniq()
-                        .map(l => new associationConstants.Option(l))
-                        .value();
-
-                    setOptionList(schema, datamap, "storeloc", options);
-                });
+            const description = datamap["offlineitem_.description"];
+            datamap["#description"] = description;
+            datamap["description"] = description;
         }
 
         //#endregion
@@ -76,7 +81,10 @@
         //#region Service Instance
         const service = {
             lineSelected,
-            itemSelected
+            itemSelected,
+            storeRoomSelected,
+            getStoreRoomWhereClause,
+            getAvailableItemsWhereClause
         };
         return service;
         //#endregion
@@ -85,7 +93,7 @@
     //#region Service registration
 
     angular.module("sw_mobile_services")
-        .factory("materialService", ["swdbDAO", "associationConstants", "offlineSchemaService", "offlineAssociationService", "swAlertPopup", materialService]);
+        .factory("materialService", [materialService]);
 
     //#endregion
 
