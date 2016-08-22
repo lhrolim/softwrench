c8c760d877dea541e4583f7f405f9cdac7eb8e4e
rodrigobotti <rodrigo.botti@gmail.com>
2016-08-22 18:33:13 -0300

    simple injector disregarding other customers dlls

--- softWrench.sW4/Util/AssemblyLocator.cs
+++ softWrench.sW4/Util/AssemblyLocator.cs
@@ -24,32 +24,42 @@ namespace softWrench.sW4.Util {
             return AllAssemblies;
         }
 
-        private static ICollection GetListOfAssemblies()
-        {
-            if (ApplicationConfiguration.IsUnitTest) {
-                var enumerable = Directory.GetFiles(
-                    AppDomain.CurrentDomain.BaseDirectory)
-                    .Where(file => Path.GetExtension(file) == ".dll");
-                return enumerable
-                    .Select(file => Assembly.LoadFrom(file)).ToList();
-            }
-            return BuildManager.GetReferencedAssemblies();
+        private static ICollection GetListOfAssemblies() {
+            return ApplicationConfiguration.IsUnitTest 
+                ? Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory)
+                    .Where(file => Path.GetExtension(file) == ".dll")
+                    .Select(Assembly.LoadFrom).ToList()
+                : BuildManager.GetReferencedAssemblies();
+        }
+
+        public static bool IsSWAssembly(Assembly assembly) {
+            var fullName = assembly.FullName;
+            return fullName.StartsWith("softWrench", StringComparison.InvariantCultureIgnoreCase) || fullName.StartsWith("cts", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public static bool IsCustomerAssembly(Assembly assembly) {
+            return assembly.GetManifestResourceNames().Any(n => n.EndsWith("customer.placeholder"));
+        }
+
+        public static bool IsCurrentCustomerAssembly(Assembly assembly) {
+            return assembly.FullName.StartsWith("softwrench.sw4.{0}".Fmt(ApplicationConfiguration.ClientName), StringComparison.CurrentCultureIgnoreCase);
         }
 
         public static IEnumerable<Assembly> GetSWAssemblies() {
-            return
-                GetAssemblies()
-                    .Where(r => r.FullName.StartsWith("softWrench", StringComparison.InvariantCultureIgnoreCase) || r.FullName.StartsWith("cts", StringComparison.InvariantCultureIgnoreCase));
+            return GetAssemblies().Where(a => {
+                var isSw = IsSWAssembly(a);
+                var isCustomer = IsCustomerAssembly(a);
+                var isCurrentCustomer = IsCurrentCustomerAssembly(a);
+                return (isSw && !isCustomer) || (isSw && isCurrentCustomer);
+            });
         }
 
-       
-
         public static bool CustomerAssemblyExists() {
-            return GetAssemblies().Any(r => r.FullName.StartsWith("softwrench.sw4.{0}".Fmt(ApplicationConfiguration.ClientName), StringComparison.CurrentCultureIgnoreCase));
+            return GetAssemblies().Any(IsCurrentCustomerAssembly);
         }
 
         public static Assembly GetCustomerAssembly() {
-            return GetAssemblies().FirstOrDefault(r => r.FullName.StartsWith("softwrench.sw4.{0}".Fmt(ApplicationConfiguration.ClientName), StringComparison.CurrentCultureIgnoreCase));
+            return GetAssemblies().FirstOrDefault(IsCurrentCustomerAssembly);
         }
 
         public static Assembly GetAssembly(string assemblyName) {
