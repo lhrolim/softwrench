dfbdd8819a0eb24121d74e9472321c774186cea6
lhrolim <luiz.rolim@amlabs.com.br>
2016-11-03 14:28:26 -0200

    SWWEb-2831 (#1525)
    * SWWEb-2831
    
    * fixing test

---
+++ softwrench.sw4.pesco/web_content/pesco/scripts/pescocommunication_service.js
@@ -0,0 +1,33 @@
+﻿
+(function (angular) {
+    'use strict';
+
+  
+    function pescoCommunicationService(crudContextHolderService) {
+       
+
+        function getSubjectDefaultExpression (datamap, schema, displayable) {
+
+            var parentData = crudContextHolderService.rootDataMap();
+            if (datamap['ownertable'].equalIc("SR")) {
+                return "SR ##" + parentData['ticketid'];
+            }
+            if (datamap['ownertable'].equalIc("WORKORDER")) {
+                return "WO ##" + parentData['wonum'];
+            }
+            return "";
+        };
+
+        const service = {
+            getSubjectDefaultExpression: getSubjectDefaultExpression
+        };
+
+        return service;
+    }
+
+    angular
+    .module('pesco')
+    .clientfactory('pescoCommunicationService', ['crudContextHolderService', pescoCommunicationService]);
+
+
+})(angular);
