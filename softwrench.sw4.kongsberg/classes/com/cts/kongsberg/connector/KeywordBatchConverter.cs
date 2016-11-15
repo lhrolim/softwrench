681f5df15b9c6a72124bd94045e8c838739240b3
Edson Mesquita <emesquita@controltechnologysolutions.com>
2016-11-15 15:57:18 -0200

    SWWEB-2730 Keywords in Solution (#1539)

---
+++ softwrench.sw4.kongsberg/classes/com/cts/kongsberg/connector/KeywordBatchConverter.cs
@@ -0,0 +1,25 @@
+﻿using softWrench.sW4.Data.Batches;
+
+namespace softwrench.sw4.kongsberg.classes.com.cts.kongsberg.connector {
+
+    public class KeywordBatchConverter : ABatchSubmissionConverter {
+
+        public override string BatchProperty {
+            get { return "#keywordlist_"; }
+        }
+
+        public override string ApplicationName() {
+            return "keyword";
+        }
+
+        public override string ClientFilter() {
+            return null;
+        }
+
+        public override string SchemaId() {
+            return null;
+        }
+
+
+    }
+}
