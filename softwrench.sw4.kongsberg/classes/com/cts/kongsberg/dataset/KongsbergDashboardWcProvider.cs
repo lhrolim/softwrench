a044a8b097af38b177900a52566b666931504860
emfmesquita <emesquita@controltechnologysolutions.com>
2017-02-20 23:11:16 -0300

    Filter anomalous sr on all grids

--- softwrench.sw4.kongsberg/classes/com/cts/kongsberg/dataset/KongsbergDashboardWcProvider.cs
+++ softwrench.sw4.kongsberg/classes/com/cts/kongsberg/dataset/KongsbergDashboardWcProvider.cs
@@ -6,10 +6,6 @@ namespace softwrench.sw4.kongsberg.classes.com.cts.kongsberg.dataset {
 
         public readonly Dictionary<string, Dictionary<string, string>> DashBoardWhereClauses = new Dictionary<string, Dictionary<string, string>>() {
             {"servicerequest", new Dictionary<string, string>() {
-
-                // applied to all sr grid dashboards - excludes anomalous sr with changedates way in the future
-                {"", "SR.ticketid not in ('20587', '20588', '20590', '21582', '21583', '21585', '21588', '21591', '21592', '21593', '21594', '21595')" },
-
                 {"closed", "sr.status in ('RESOLVED','CLOSED')" },
                 {"open", "(SR.status = 'INPROG' or SR.status = 'SLAHOLD' or SR.status = 'NEW' or SR.status = 'PENDING' or SR.status = 'QUEUED')" }
             }}
