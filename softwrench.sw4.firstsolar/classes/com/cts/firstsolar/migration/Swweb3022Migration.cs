a71bd4633d33bbcf0da9fd28a7625e5cc515bc8e
lhrolim <luizhenrique.rolim@gmail.com>
2017-06-20 19:59:03 -0300

    missing migration file

--- softwrench.sw4.firstsolar/classes/com/cts/firstsolar/migration/Swweb3022Migration.cs
+++ softwrench.sw4.firstsolar/classes/com/cts/firstsolar/migration/Swweb3022Migration.cs
@@ -13,7 +13,7 @@ namespace softwrench.sw4.firstsolar.classes.com.cts.firstsolar.migration {
     public class Swweb3022Migration : Migration {
 
         public override void Up() {
-            Create.Column("cc").OnTable("OPT_MAINTENANCE_ENG").AsString(MigrationUtil.StringLarge);
+            Create.Column("cc").OnTable("OPT_MAINTENANCE_ENG").AsString(MigrationUtil.StringLarge).Nullable();
         }
 
         public override void Down() {
