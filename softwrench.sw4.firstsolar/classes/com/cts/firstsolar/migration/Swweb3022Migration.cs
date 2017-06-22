1b258e5043f557051c35ad869c9f127708f566b9
emfmesquita <emesquita@controltechnologysolutions.com>
2017-06-22 18:13:37 -0300

    migration fix

--- softwrench.sw4.firstsolar/classes/com/cts/firstsolar/migration/Swweb3022Migration.cs
+++ softwrench.sw4.firstsolar/classes/com/cts/firstsolar/migration/Swweb3022Migration.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using cts.commons.persistence.Util;
+﻿using cts.commons.persistence.Util;
 using FluentMigrator;
 
 namespace softwrench.sw4.firstsolar.classes.com.cts.firstsolar.migration {
     [Migration(201706201103)]
-    public class Swweb3022_2Migration : Migration {
+    public class Swweb3022Migration : Migration {
 
         public override void Up() {
             Create.Column("cc").OnTable("OPT_MAINTENANCE_ENG").AsString(MigrationUtil.StringLarge).Nullable();
