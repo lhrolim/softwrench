620032ffca311a1f9348f8760e6d3568d486d7e3
lhrolim <luizhenrique.rolim@gmail.com>
2017-07-04 13:24:33 -0300

    changing the customized way to allow the app

--- softwrench.sw4.firstsolar/classes/com/cts/firstsolar/configuration/FSUserMainSecurityApplier.cs
+++ softwrench.sw4.firstsolar/classes/com/cts/firstsolar/configuration/FSUserMainSecurityApplier.cs
@@ -7,8 +7,10 @@ using cts.commons.portable.Util;
 using cts.commons.simpleinjector;
 using softwrench.sw4.user.classes.entities;
 using softwrench.sw4.user.classes.entities.security;
+using softWrench.sW4.Data.API;
 using softWrench.sW4.Metadata.Applications;
 using softWrench.sW4.Metadata.Applications.Security;
+using softWrench.sW4.Metadata.Security;
 
 namespace softwrench.sw4.firstsolar.classes.com.cts.firstsolar.configuration {
 
@@ -16,19 +18,20 @@ namespace softwrench.sw4.firstsolar.classes.com.cts.firstsolar.configuration {
     [OverridingComponent(ClientFilters = "firstsolar")]
     public class FSUserMainSecurityApplier : UserMainSecurityApplier {
 
-        protected override ApplicationPermission LookupPermission(ApplicationMetadata application, MergedUserProfile profile) {
-            var basePermission = base.LookupPermission(application, profile);
+        public override InMemoryUserExtensions.SecurityModeCheckResult VerifyMainSecurityMode(InMemoryUser user, ApplicationMetadata application,
+            DataRequestAdapter request){
+            var securityResult = base.VerifyMainSecurityMode(user, application, request);
+            var profile = user.MergedUserProfile;
             var wpPermission = profile.GetPermissionByApplication("_workpackage");
             if (wpPermission == null || wpPermission.HasNoPermissions) {
-                return basePermission;
+                return securityResult;
             }
 
             if (application.Name.EqualsIc("workorder")) {
                 //if the user has access to workpackage, it should be given access to workorders schemas as well, otherwise some lookups would fail
-                basePermission.AllowUpdate = true;
+                return InMemoryUserExtensions.SecurityModeCheckResult.Allow;
             }
-            return basePermission;
-
+            return securityResult;
         }
     }
 }
