0d6418fdf2a643f3b6af0c30a3d45f70752c86d0
lhrolim <luizhenrique.rolim@gmail.com>
2017-07-16 10:22:23 -0300

    fixing security

--- softWrench.sW4.Web/Controllers/HomeController.cs
+++ softWrench.sW4.Web/Controllers/HomeController.cs
@@ -104,7 +104,7 @@ namespace softWrench.sW4.Web.Controllers {
                 actionURL = String.Format("api/generic/{0}/{1}", controllerToRedirect, actionToRedirect);
             }
             var unescapedQs = WebAPIUtil.GetUnescapedQs(queryString);
-//            ValidateSecurity(unescapedQs);
+            ValidateSecurity(unescapedQs);
 
             var redirectURL = String.Format("{0}?{1}", actionURL, unescapedQs);
 
