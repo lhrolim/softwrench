42a582d061489a8e82e6dcb75e61d95ef535b015
Edson Mesquita <emesquita@controltechnologysolutions.com>
2017-07-20 18:18:47 -0300

    SWWEB-3056 First solar attachment - Field length and description (#1740)

--- softwrench.sw4.webcommons/web_content/webcommons/scripts/softwrench/util/aa_utils.js
+++ softwrench.sw4.webcommons/web_content/webcommons/scripts/softwrench/util/aa_utils.js
@@ -671,6 +671,19 @@ function removeDuplicatesOnArray(array, keyProp) {
     return withoutDuplicates;
 }
 
+/**
+ * Generates a globally unique identifier (GUID)
+ */
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
+
 /* Base64 encode / decode http://www.webtoolkit.info/ */
 var Base64 = {
     // private property
