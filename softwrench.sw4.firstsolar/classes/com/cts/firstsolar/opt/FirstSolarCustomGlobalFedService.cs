f1a48ade7ed15030e98f47d783a71fbddb717963
emfmesquita <emesquita@controltechnologysolutions.com>
2017-08-01 15:35:38 -0300

    gfed email fix

--- softwrench.sw4.firstsolar/classes/com/cts/firstsolar/opt/FirstSolarCustomGlobalFedService.cs
+++ softwrench.sw4.firstsolar/classes/com/cts/firstsolar/opt/FirstSolarCustomGlobalFedService.cs
@@ -124,7 +124,7 @@ namespace softwrench.sw4.firstsolar.classes.com.cts.firstsolar.opt {
 
         public async Task<string> BuildToFromGfed(DailyOutageMeeting dom, WorkPackage package) {
             var toList = new List<string>();
-            var qryResult = await _maxDao.FindByNativeQueryAsync(GFedQuery, package.WorkorderId);
+            var qryResult = await _maxDao.FindByNativeQueryAsync(GFedEmailQuery, package.WorkorderId);
             if (qryResult != null && qryResult.Any()) {
                 var row = qryResult.First();
                 AddEmail(row, toList, PlannerEmailColumn);
