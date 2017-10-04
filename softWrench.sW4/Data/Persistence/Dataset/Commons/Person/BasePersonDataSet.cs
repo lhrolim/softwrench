8a2ebf408ea541d3c310322ee3e7ebd1a25a3b47
emfmesquita <emesquita@controltechnologysolutions.com>
2017-10-04 17:13:14 -0300

    isactive sw not showing on users page

--- softWrench.sW4/Data/Persistence/Dataset/Commons/Person/BasePersonDataSet.cs
+++ softWrench.sW4/Data/Persistence/Dataset/Commons/Person/BasePersonDataSet.cs
@@ -101,7 +101,7 @@ namespace softWrench.sW4.Data.Persistence.Dataset.Commons.Person {
             }
             var swusers = UserManager.GetUserByPersonIds(personIds);
             foreach (var record in result.ResultObject) {
-                var swuser = swusers.SingleOrDefault(user => user.UserName.EqualsIc(record.GetAttribute("personid").ToString()));
+                var swuser = swusers.SingleOrDefault(user => user.MaximoPersonId.EqualsIc(record.GetAttribute("personid").ToString()));
                 if (swuser == null) {
                     continue;
                 }
