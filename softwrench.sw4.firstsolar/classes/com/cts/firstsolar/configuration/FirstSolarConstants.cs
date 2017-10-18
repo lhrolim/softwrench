b5e99851e20755035480a28361439f39256b28fd
Edson Mesquita <emesquita@controltechnologysolutions.com>
2017-10-17 23:14:36 -0200

    SWWEB-3229 add emails to new WP notification (#1829)

--- softwrench.sw4.firstsolar/classes/com/cts/firstsolar/configuration/FirstSolarConstants.cs
+++ softwrench.sw4.firstsolar/classes/com/cts/firstsolar/configuration/FirstSolarConstants.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace softwrench.sw4.firstsolar.classes.com.cts.firstsolar.configuration {
-    public class FirstSolarConstants
-    {
-
+﻿namespace softwrench.sw4.firstsolar.classes.com.cts.firstsolar.configuration {
+    public class FirstSolarConstants {
         public const string FacilitiesProp = "sync.facilities";
         public const string AvailableFacilitiesProp = "sync.availablefacilities";
-
-
+        public const string TierOneCcEmails = "Troy.Lauterbach@firstsolar.com, TMoeller@firstsolar.com, Frank.Kelly@firstsolar.com, Kirby.Hunt@firstsolar.com, James.Kieley@firstsolar.com, William.Byrd@firstsolar.com";
     }
 }
