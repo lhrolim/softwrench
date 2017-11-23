924cdb27c8630440c39a2f8ef0e5a2b818d6c400
luiz rolim <luizhenrique.rolim@gmail.com>
2017-11-23 12:23:20 -0200

    SWWEB-3279

---
+++ softwrench.sw4.problem/DB_Migration/MigrationSwweb3279.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using cts.commons.persistence.Util;
+using FluentMigrator;
+
+namespace softWrench.sw4.problem.DB_Migration {
+
+    [Migration(201711231152)]
+    public class MigrationSwweb3279 : Migration {
+
+        public override void Up() {
+            Execute.Sql("alter table prob_problem alter column data VARBINARY(max)");
+
+        }
+
+        public override void Down() {
+
+        }
+    }
+}
