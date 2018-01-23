a60a30ebf878f1c16e123e6b3fd1e1091917bfed
luiz rolim <luizhenrique.rolim@gmail.com>
2018-01-23 09:45:26 -0200

    HAP-1171

---
+++ softwrench.sw4.Hapag/Data/Scheduler/Jobs/Helper/R104ExtractorHelper.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Text;
+using softwrench.sw4.Hapag.Data.DataSet.Helper;
+using softWrench.sW4.Data.Search;
+using softWrench.sW4.Metadata;
+using softWrench.sW4.Util;
+
+namespace softwrench.sw4.Hapag.Data.Scheduler.Jobs.Helper {
+
+    /// <summary>
+    /// Fix precondition:
+    /// ASSET.STATUS = 120 Active
+    /// Extract all Assets where “Computer_name”* is starting with h,c,n,t,a,b,k (small and capital letters).
+    /// 
+    /// </summary>
+
+    public class R104ExtractorHelper
+    {
+
+        public const string ComputerNameQuery ="CASE WHEN LOCATE('//',asset.Description) > 0 THEN LTRIM(RTRIM(SUBSTR(asset.Description,1,LOCATE('//',asset.Description)-1))) ELSE LTRIM(RTRIM(asset.Description)) END";
+
+        internal static SearchRequestDto BuildDTO() {
+
+            var dto = new SearchRequestDto {
+                SearchSort = "computername",
+                SearchAscending = true,
+                QueryAlias = "rI104"
+            };
+            //h,c,n,t,a,b,k 
+            char[] chars = { 'h', 'c', 'n', 't', 'a', 'b', 'k'};
+            dto.AppendWhereClause("upper(STATUS) in ('{0}') ".Fmt(AssetConstants.Active.ToUpper()));
+            var sb = new StringBuilder(" ( ");
+
+            for (var index = 0;index < chars.Length;index++) {
+                var c = chars[index];
+                sb.AppendFormat(" {0} like ('{1}%') ",ComputerNameQuery, c);
+                if (index != chars.Length - 1) {
+                    sb.AppendFormat(" or ");
+                }
+
+            }
+            sb.Append(")");
+
+            dto.AppendWhereClause(sb.ToString());
+
+            
+
+            //            var configFilePath = MetadataProvider.GlobalProperty("RI104Path");
+            //            if (!File.Exists(configFilePath)) {
+            //                return dto;
+            //            }
+            //            var fileLines = File.ReadAllLines(configFilePath);
+            //
+            //            var sb = ClassStructureConfigFileReader.DoBuildQuery(fileLines);
+            //            dto.AppendWhereClause(sb);
+            return dto;
+
+        }
+
+
+
+    }
+}
---
+++ softwrench.sw4.Hapag/Data/Scheduler/Jobs/RI104ExtractorJob.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using softwrench.sw4.Hapag.Data.Scheduler.Jobs.Helper;
+using softwrench.sW4.Shared2.Metadata.Applications.Schema;
+using softWrench.sW4.Data.Persistence.Relational;
+using softWrench.sW4.Metadata;
+using softWrench.sW4.Metadata.Applications;
+using softWrench.sW4.Scheduler;
+using softWrench.sW4.Security.Services;
+using softWrench.sW4.Util;
+
+namespace softwrench.sw4.Hapag.Data.Scheduler.Jobs {
+    // ReSharper disable once InconsistentNaming
+    public class RI104ExtractorJob : ASwJob {
+
+        private const string FileName = "Active_Assets.csv";
+        private const string FileBaseDirectoryPath = "E:\\Client List\\";
+
+
+        private readonly EntityRepository _entityRepository;
+
+        private readonly ExcelUtil _excelUtil;
+
+        public RI104ExtractorJob(EntityRepository entityRepository, ExcelUtil excelUtil) {
+            _entityRepository = entityRepository;
+            _excelUtil = excelUtil;
+        }
+
+
+        public override string Name() {
+            return "RI104 Client Extractor";
+        }
+
+        public override string Description() {
+            return "Extract all Active Assets from Hapag as described at HAP-1171 ";
+        }
+
+        public override string Cron() {
+            return "0 0 1 * * ?";
+        }
+
+        public override void ExecuteJob() {
+
+            var user = SecurityFacade.CurrentUser();
+            var app = MetadataProvider.Application("asset").ApplyPoliciesWeb(new ApplicationMetadataSchemaKey("RI104Export"));
+
+            var sliced = MetadataProvider.SlicedEntityMetadata(app);
+            var dto = R104ExtractorHelper.BuildDTO();
+            var rows = _entityRepository.Get(sliced, dto);
+
+            var csvBytes = _excelUtil.ConvertGridToCsv(user, app.Schema, rows);
+            var outputPath = FileBaseDirectoryPath + FileName;
+            if (ApplicationConfiguration.IsLocal()) {
+                outputPath = "c:\\softwrench\\hapag\\Active_Assets.csv";
+            }
+            File.WriteAllBytes(outputPath, csvBytes);
+
+        }
+
+
+
+        public override bool IsScheduled {
+            get; set;
+        }
+        public override bool RunAtStartup() {
+            return ApplicationConfiguration.IsLocal();
+        }
+    }
+}
