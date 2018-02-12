4c3e7941d373ed1aa9afa4e3382abbf21879150b
luiz rolim <luizhenrique.rolim@gmail.com>
2018-02-12 16:27:25 -0200

    log ++

--- softWrench.sW4/Data/Persistence/WS/Applications/Compositions/AttachmentHandler.cs
+++ softWrench.sW4/Data/Persistence/WS/Applications/Compositions/AttachmentHandler.cs
@@ -350,7 +350,7 @@ namespace softWrench.sW4.Data.Persistence.WS.Applications.Compositions {
                     }
                     return Tuple.Create(fileBytes, fileName);
                 } catch (Exception exception) {
-                    Log.ErrorFormat("Error Attachment Handler: {0} - {1}", exception.Message, exception.InnerException == null ? "No Internal Error Message" : exception.InnerException.Message, exception);
+                    Log.Error("Error Attachment Handler: {0} - {1}".Fmt(exception.Message, exception.InnerException?.Message ?? "No Internal Error Message"), exception);
                     return null;
                 }
             }
