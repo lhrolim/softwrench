09398215d0e2280b4a3e9c3ad1d9e181540f0d8d
luiz rolim <luizhenrique.rolim@gmail.com>
2018-02-19 14:52:23 -0300

    fixing deferred issue

--- softwrench.sw4.webcommons/web_content/webcommons/scripts/softwrench/util/angular/deferredupdate.js
+++ softwrench.sw4.webcommons/web_content/webcommons/scripts/softwrench/util/angular/deferredupdate.js
@@ -1,182 +1,183 @@
-﻿(function( ng ){
+﻿(function (ng) {
 
-	//taken from here:https://github.com/bennadel/DeferredWithUpdate.js/blob/master/examples/update-response.htm
-	//with slight modifications
+    //taken from here:https://github.com/bennadel/DeferredWithUpdate.js/blob/master/examples/update-response.htm
+    //with slight modifications
 
-	// Define the module for this code.
-	var app = ng.module( "DeferredWithUpdate", [] );
+    // Define the module for this code.
+    var app = ng.module("DeferredWithUpdate", []);
 
 
-	// When defining the Deferred object, we need to have two things be true:
-	//
-	// 1. We have access to AngularJS' $q class.
-	// 2. We return an object that can be instantiated.
-	//
-	// As such, we can define it as a service object and inject the $q class.
-	app.service(
-		"DeferredWithUpdate",
-		function( $q ) {
+    // When defining the Deferred object, we need to have two things be true:
+    //
+    // 1. We have access to AngularJS' $q class.
+    // 2. We return an object that can be instantiated.
+    //
+    // As such, we can define it as a service object and inject the $q class.
+    app.service(
+        "DeferredWithUpdate",
+        function ($q) {
 
 
-			// I just pass-through to the core $q class.
-			this.all = function() {
+            // I just pass-through to the core $q class.
+            this.all = function () {
 
-				return(
-					$q.all.apply( $q, arguments )
-				);
+                return (
+                    $q.all.apply($q, arguments)
+                );
 
-			};
+            };
 
 
-			// I return a new deferred object with the decorated promise.
-			this.defer = function() {
+            // I return a new deferred object with the decorated promise.
+            this.defer = function () {
 
-				// Get the core deferred class and its promise.
-				var deferred = $q.defer();
-				var promise = deferred.promise;
+                // Get the core deferred class and its promise.
+                var deferred = $q.defer();
+                var promise = deferred.promise;
 
-				// Store the original methods that we are going to decorate.
-				var core = {
-					resolve: deferred.resolve,
-					reject: deferred.reject,
-					then: promise.then
-				};
-				
-				// Flag that the deferred value has not yet been resolved or rejected. Under
-				// the hood, AngularJS does something rather odd with the promise - it calls 
-				// resolve() for both resolved AND rejected deferred values. As such, we need 
-				// to track the states slightly independently. 
-				var isResolved = false;
-				var isRejected = false;
-				
-				// Keep a collection of update and mistake callbacks.
-				var updateCallbacks = [];
-				var mistakeCallbacks = [];
+                // Store the original methods that we are going to decorate.
+                var core = {
+                    resolve: deferred.resolve,
+                    reject: deferred.reject,
+                    then: promise.then
+                };
 
+                // Flag that the deferred value has not yet been resolved or rejected. Under
+                // the hood, AngularJS does something rather odd with the promise - it calls 
+                // resolve() for both resolved AND rejected deferred values. As such, we need 
+                // to track the states slightly independently. 
+                var isResolved = false;
+                var isRejected = false;
 
-				// I decorate tht resolve function, setting the internal pending flag.
-				deferred.resolve = function() {
+                // Keep a collection of update and mistake callbacks.
+                var updateCallbacks = [];
+                var mistakeCallbacks = [];
 
-					// If the resolve() has been called more than once, alert the update 
-					// callbacks. When doing this, we need to check the rejected flag since, 
-					// internally, the $q class uses the resolve to announce the reject() 
-					// event - odd.
-					if (!isRejected ) {
 
-						// Invoke all the update callbacks.
-						for ( var i = 0, length = updateCallbacks.length ; i < length ; i++ ) {
+                // I decorate tht resolve function, setting the internal pending flag.
+                deferred.resolve = function () {
 
-							updateCallbacks[ i ].apply( {}, arguments );
+                    // If the resolve() has been called more than once, alert the update 
+                    // callbacks. When doing this, we need to check the rejected flag since, 
+                    // internally, the $q class uses the resolve to announce the reject() 
+                    // event - odd.
+                    const upCallbackLength = updateCallbacks.length;
 
-						}
+                    if (!isRejected && upCallbackLength !== 0) {
 
-					} else {
+                        // Invoke all the update callbacks.
+                        for (var i = 0, length = upCallbackLength; i < length; i++) {
 
-						isResolved = true;
+                            updateCallbacks[i].apply({}, arguments);
 
-						// Invoke the underlying resolve function.
-						core.resolve.apply( deferred, arguments )
+                        }
 
-					}
+                    }
 
-				};
+                    isResolved = true;
 
+                    // Invoke the underlying resolve function.
+                    core.resolve.apply(deferred, arguments);
 
-				// I decorate the reject function, setting the internal pending flag.
-				deferred.reject = function() {
 
-					// If the reject method has been called after the deferred has been resolved, 
-					// then we'll want to alert the mistake callbacks.
-					if ( isResolved ) {
+                };
 
-						// Invoke all the mistake callbacks.
-						for ( var i = 0, length = mistakeCallbacks.length ; i < length ; i++ ) {
 
-							mistakeCallbacks[ i ].apply( {}, arguments );
+                // I decorate the reject function, setting the internal pending flag.
+                deferred.reject = function () {
 
-						}
+                    // If the reject method has been called after the deferred has been resolved, 
+                    // then we'll want to alert the mistake callbacks.
+                    if (isResolved) {
 
-					} else {
+                        // Invoke all the mistake callbacks.
+                        for (var i = 0, length = mistakeCallbacks.length; i < length; i++) {
 
-						isRejected = true;
+                            mistakeCallbacks[i].apply({}, arguments);
 
-						// Invoke the underlying reject function.
-						core.reject.apply( deferred, arguments )
+                        }
 
-					}
+                    } else {
 
-				};
+                        isRejected = true;
 
+                        // Invoke the underlying reject function.
+                        core.reject.apply(deferred, arguments)
 
-				// I decorate the then() method, adding the update() method to the wrapped promise.
-				promise.then = function() {
+                    }
 
-					// Call the underlying then() method - this will return a new promise. We'll
-					// then have to udpate that promise with its own update() method.
-					var newPromise = core.then.apply( promise, arguments );
+                };
 
-					// Copy the decorated methods.
-					newPromise.update = promise.update;
-					newPromise.mistake = promise.mistake;
 
-					// Return the newly decorated promsie.
-					return( newPromise );
+                // I decorate the then() method, adding the update() method to the wrapped promise.
+                promise.then = function () {
 
-				};
+                    // Call the underlying then() method - this will return a new promise. We'll
+                    // then have to udpate that promise with its own update() method.
+                    var newPromise = core.then.apply(promise, arguments);
 
+                    // Copy the decorated methods.
+                    newPromise.update = promise.update;
+                    newPromise.mistake = promise.mistake;
 
-				// I add the update() callback registration on the promise.
-				promise.update = function( callback ) {
+                    // Return the newly decorated promsie.
+                    return (newPromise);
 
-					// Keep track of the callback.
-					updateCallbacks.push( callback );
+                };
 
-					// Return the existing promise.
-					return( promise );
 
-				};
+                // I add the update() callback registration on the promise.
+                promise.update = function (callback) {
 
+                    // Keep track of the callback.
+                    updateCallbacks.push(callback);
 
-				// I add the mistake() callback registration on the promise.
-				promise.mistake = function( callback ) {
+                    // Return the existing promise.
+                    return (promise);
 
-					// Keep track of the callback.
-					mistakeCallbacks.push( callback );
+                };
 
-					// Return the existing promise.
-					return( promise );
 
-				};
+                // I add the mistake() callback registration on the promise.
+                promise.mistake = function (callback) {
 
+                    // Keep track of the callback.
+                    mistakeCallbacks.push(callback);
 
-				// Return the decorated deferred object.
-				return( deferred );
+                    // Return the existing promise.
+                    return (promise);
 
-			};
+                };
 
 
-			// I just pass-through to the core $q class.
-			this.reject = function(){
+                // Return the decorated deferred object.
+                return (deferred);
 
-				return(
-					$q.reject.apply( $q, arguments )
-				);
+            };
 
-			};
 
+            // I just pass-through to the core $q class.
+            this.reject = function () {
 
-			// I just pass-through to the core $q class.
-			this.when = function() {
+                return (
+                    $q.reject.apply($q, arguments)
+                );
 
-				return(
-					$q.when.apply( $q, arguments )
-				);
+            };
 
-			};
 
+            // I just pass-through to the core $q class.
+            this.when = function () {
 
-		}
-	);
+                return (
+                    $q.when.apply($q, arguments)
+                );
 
+            };
 
-})( angular );
+
+        }
+    );
+
+
+})(angular);
